f9574c099a2e80fc9e40f1ac4cdbac4836baf1fd
Christian Hoff <christian.hoff@bmw-carit.de>
2014-03-21 15:23:41 +0100

    build: bounceproxy.war is compliant to open source regulations
    * using maven-resrouces-plugin to copy NOTICE and LICENSE file to war
    * added DISCLAIMER
    * added license files of dependency libs
    * auto formatter
    
    Change-Id: If5f69def0209719aea958fb88fec5750b4acb315

--- java/messaging/bounceproxy/controlled-bounceproxy/src/main/java/io/joynr/messaging/bounceproxy/filter/CharacterEncodingFilter.java
+++ java/messaging/bounceproxy/controlled-bounceproxy/src/main/java/io/joynr/messaging/bounceproxy/filter/CharacterEncodingFilter.java
@@ -39,19 +39,19 @@ import com.google.inject.Singleton;
 @Singleton
 public class CharacterEncodingFilter implements Filter {
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
-		response.setCharacterEncoding("UTF-8");
-		chain.doFilter(request, response);
-	}
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+                                                                                             ServletException {
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
 
-	@Override
-	public void destroy() {
-	}
+    @Override
+    public void destroy() {
+    }
 
 }
