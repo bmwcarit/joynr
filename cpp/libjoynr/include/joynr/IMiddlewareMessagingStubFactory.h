ff5eece09eceedf7027aada6b0202db4b1007e93
Michael Knapp <michael.knapp@bmw-carit.de>
2014-04-09 15:00:21 +0200

    cpp: Implement dbus messaging stub factory
    Change-Id: I850c2df5a97d6283421df513934a0e08a6de328d

---
+++ cpp/libjoynr/include/joynr/IMiddlewareMessagingStubFactory.h
@@ -0,0 +1,40 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2014 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#ifndef IMIDDLEWAREMESSAGINGSTUBFACTORY_H
+#define IMIDDLEWAREMESSAGINGSTUBFACTORY_H
+
+#include <QString>
+#include <QSharedPointer>
+
+namespace joynr {
+
+namespace system { class Address; }
+class IMessaging;
+
+class IMiddlewareMessagingStubFactory {
+public:
+    virtual ~IMiddlewareMessagingStubFactory() {}
+    virtual QSharedPointer<IMessaging> create(const joynr::system::Address& destAddress) = 0;
+    virtual bool canCreate(const joynr::system::Address& destAddress) = 0;
+};
+
+
+} // namespace joynr
+
+#endif // IMIDDLEWAREMESSAGINGSTUBFACTORY_H
