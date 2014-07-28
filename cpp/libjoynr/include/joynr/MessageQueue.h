bf845c38788be6075b477331fab5e5810a73cf84
Michael Knapp <michael.knapp@bmw-carit.de>
2014-07-28 13:52:57 +0200

    cpp: Add MessageQueue class
    This class implements the message queue for the message router.
    
    Change-Id: Ie554b32ddf121ac5b49ef868ee04d946f1efa68b

---
+++ cpp/libjoynr/include/joynr/MessageQueue.h
@@ -0,0 +1,52 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2014 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#ifndef MESSAGEQUEUE_H
+#define MESSAGEQUEUE_H
+
+#include "joynr/PrivateCopyAssign.h"
+#include "joynr/JoynrExport.h"
+
+#include "joynr/JoynrMessage.h"
+#include "joynr/MessagingQos.h"
+#include "joynr/ContentWithDecayTime.h"
+
+#include <QMutex>
+
+namespace joynr {
+
+
+class JOYNR_EXPORT MessageQueue {
+public:
+    MessageQueue();
+
+    ~MessageQueue();
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(MessageQueue);
+
+    typedef ContentWithDecayTime<QPair<JoynrMessage, MessagingQos>> MessageQueueItem;
+
+    QMap<QString, MessageQueueItem*>* queue;
+    mutable QMutex queueMutex;
+
+};
+
+}
+
+#endif // MESSAGEQUEUE_H
