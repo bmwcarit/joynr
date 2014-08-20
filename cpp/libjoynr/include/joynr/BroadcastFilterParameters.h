4a44a147738535679fb3274c8202a4b145839bb1
Daniel Mohr <daniel.mohr@bmw-carit.de>
2014-08-20 15:16:31 +0200

    [cpp] Introduce BroadcastFilterParameters class
    Change-Id: If05fbe6613a061329d9d6475fd664232ab4c2104

---
+++ cpp/libjoynr/include/joynr/BroadcastFilterParameters.h
@@ -0,0 +1,65 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2013 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#ifndef BROADCASTFILTERPARAMETERS_H
+#define BROADCASTFILTERPARAMETERS_H
+
+#include <QMap>
+#include <QString>
+#include <QObject>
+#include <QVariant>
+#include <QSharedPointer>
+#include <QMetaType>
+
+#include "joynr/JoynrCommonExport.h"
+
+namespace joynr {
+
+/**
+ * \class BroadcastFilterParameters
+ * \brief The BroadcastFilterParameters class represents generic filter parameters
+ * for selective broadcasts
+ */
+class JOYNRCOMMON_EXPORT BroadcastFilterParameters : public QObject {
+    Q_OBJECT
+
+    Q_PROPERTY(QMap filterParameters READ getFilterParameters WRITE setFilterParameters)
+
+public:
+    BroadcastFilterParameters();
+    BroadcastFilterParameters(const BroadcastFilterParameters& filterParameters);
+    BroadcastFilterParameters& operator=(const BroadcastFilterParameters& filterParameters);
+    ~BroadcastFilterParameters();
+    bool operator==(const BroadcastFilterParameters& filterParameters) const;
+
+    void setFilterParameter(QString parameter, QString value);
+
+    virtual bool equals(const QObject& other) const;
+
+    QMap<QString, QVariant> getFilterParameters() const;
+    void setFilterParameters(const QMap<QString, QVariant> &value);
+
+private:
+    QMap<QString, QVariant> filterParameters;
+};
+
+} // namespace joynr
+
+Q_DECLARE_METATYPE(joynr::BroadcastFilterParameters)
+Q_DECLARE_METATYPE(QSharedPointer<joynr::BroadcastFilterParameters>)
+#endif // BROADCASTFILTERPARAMETERS_H
---
+++ cpp/libjoynr/subscription/BroadcastFilterParameters.cpp
@@ -0,0 +1,75 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2013 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#include "joynr/BroadcastFilterParameters.h"
+
+namespace joynr {
+
+BroadcastFilterParameters::BroadcastFilterParameters() :
+    filterParameters(QMap<QString, QVariant>()) {
+}
+
+BroadcastFilterParameters::BroadcastFilterParameters(const BroadcastFilterParameters &filterParameters) :
+    QObject(),
+    filterParameters(filterParameters.getFilterParameters()) {
+}
+
+BroadcastFilterParameters &BroadcastFilterParameters::operator=(const BroadcastFilterParameters &filterParameters){
+    this->filterParameters = filterParameters.getFilterParameters();
+    return *this;
+}
+
+BroadcastFilterParameters::~BroadcastFilterParameters() {
+}
+
+bool BroadcastFilterParameters::operator==(const BroadcastFilterParameters &filterParameters) const {
+    bool equal = this->filterParameters.keys() == filterParameters.getFilterParameters().keys();
+
+    QList<QVariant> values1 = this->filterParameters.values();
+    QList<QVariant> values2 = this->filterParameters.values();
+
+    if (equal){
+        for (int i=0; i<values1.size(); i++){
+            if (! (equal = equal && (values1[i].toString()) == values2[i].toString()))
+                break;
+        }
+    }
+
+    return equal;
+}
+
+void BroadcastFilterParameters::setFilterParameter(QString parameter, QString value) {
+    filterParameters.insert(parameter, value);
+}
+
+QMap<QString, QVariant> BroadcastFilterParameters::getFilterParameters() const {
+    return filterParameters;
+}
+
+void BroadcastFilterParameters::setFilterParameters(const QMap<QString, QVariant> &value){
+    filterParameters = value;
+}
+
+bool BroadcastFilterParameters::equals(const QObject& other) const {
+    int typeThis = QMetaType::type(this->metaObject()->className());
+    int typeOther = QMetaType::type(other.metaObject()->className());
+    auto newOther = dynamic_cast<const BroadcastFilterParameters*>(&other);
+    return typeThis == typeOther && *this == *newOther;
+}
+
+} // namespace joynr
