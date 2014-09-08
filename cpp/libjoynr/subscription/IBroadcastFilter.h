48e7cf7b6379c8a43f4141d9143e113c799b7cf0
Daniel Mohr <daniel.mohr@bmw-carit.de>
2014-09-08 16:42:34 +0200

    [cpp] Add interface for broadcast filters
    Change-Id: I18e95163ab7b3c6eca5820e22747497968d22ba1

---
+++ cpp/libjoynr/subscription/IBroadcastFilter.h
@@ -0,0 +1,47 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2014 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#ifndef IBROADCASTFILTER_H
+#define IBROADCASTFILTER_H
+
+#include "joynr/JoynrExport.h"
+
+template<typename K, typename V>
+class QMap;
+
+class QVariant;
+class QString;
+
+typedef QMap<QString, QVariant> QVariantMap;
+
+class BroadcastFilterParamters;
+
+
+namespace joynr {
+
+class JOYNR_EXPORT IBroadcastFilter
+{
+public:
+
+    virtual bool filter(QVariantMap eventValues, BroadcastFilterParamters filterParamters) = 0;
+
+};
+
+} // namespace joynr
+
+#endif // IBROADCASTFILTER_H
--- cpp/libjoynr/subscription/PublicationManager.cpp
+++ cpp/libjoynr/subscription/PublicationManager.cpp
@@ -30,6 +30,7 @@
 #include "joynr/BroadcastSubscriptionRequest.h"
 #include "joynr/BroadcastFilterParameters.h"
 #include "joynr/Util.h"
+#include "libjoynr/subscription/IBroadcastFilter.h"
 #include "libjoynr/subscription/SubscriptionRequestInformation.h"
 #include "libjoynr/subscription/BroadcastSubscriptionRequestInformation.h"
 #include "libjoynr/subscription/SubscriptionAttributeListener.h"
