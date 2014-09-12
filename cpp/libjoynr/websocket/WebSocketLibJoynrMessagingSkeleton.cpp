6463a49c6a6a879e7a585ede60269679e605991d
Christian Hoff <christian.hoff@bmw-carit.de>
2014-09-12 16:55:20 +0200

    [C++] created WebSocket messaging skeleton on libjoynr side
    Change-Id: I9f2c61f1296291cd1c2f47e513f67662dfc3faa5

---
+++ cpp/libjoynr/websocket/WebSocketLibJoynrMessagingSkeleton.cpp
@@ -0,0 +1,68 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2014 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#include "WebSocketLibJoynrMessagingSkeleton.h"
+
+#include <QtWebSockets/QWebSocket>
+
+#include "joynr/JsonSerializer.h"
+#include "joynr/system/WebSocketClientAddress.h"
+
+namespace joynr {
+
+joynr_logging::Logger* WebSocketLibJoynrMessagingSkeleton::logger =
+        joynr_logging::Logging::getInstance()->getLogger("MSG", "WebSocketLibJoynrMessagingSkeleton");
+
+WebSocketLibJoynrMessagingSkeleton::WebSocketLibJoynrMessagingSkeleton(
+        MessageRouter& messageRouter
+) :
+    messageRouter(messageRouter)
+{
+}
+
+WebSocketLibJoynrMessagingSkeleton::~WebSocketLibJoynrMessagingSkeleton()
+{
+}
+
+void WebSocketLibJoynrMessagingSkeleton::transmit(JoynrMessage &message, const MessagingQos &qos)
+{
+    messageRouter.route(message, qos);
+}
+
+void WebSocketLibJoynrMessagingSkeleton::onConnected()
+{
+    QWebSocket* server = qobject_cast<QWebSocket*>(sender());
+
+    connect(
+            server, &QWebSocket::textMessageReceived,
+            this, &WebSocketLibJoynrMessagingSkeleton::onTextMessageReceived
+    );
+}
+
+void WebSocketLibJoynrMessagingSkeleton::onTextMessageReceived(const QString &message)
+{
+    // deserialize message and transmit
+    joynr::JoynrMessage* joynrMsg =
+            JsonSerializer::deserialize<joynr::JoynrMessage>(message.toUtf8());
+    // message router copies joynr message when scheduling thread that handles
+    // message delivery
+    transmit(*joynrMsg, MessagingQos());
+    delete joynrMsg;
+}
+
+} // namespace joynr
