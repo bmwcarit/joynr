d6a1395fe0093ac5def75d9bf2d5493917e2c7c9
Christian Hoff <christian.hoff@bmw-carit.de>
2014-09-15 08:21:37 +0200

    [C++] create LibJoynrWebSocketRuntime
    * split JoynrRuntime into JoynrDbusRuntime and JoynrWsRuntime
    * JoynrDbusRuntime is only build when USE_DBUS_COMMONAPI_COMMUNICATION
      is set
    
    Change-Id: I2a4f54b83753007e2a91a20f3d3484af469e37c7

---
+++ cpp/runtimes/libjoynr-runtime/websocket/LibJoynrWebSocketRuntime.cpp
@@ -0,0 +1,65 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2014 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#include "runtimes/libjoynr-runtime/websocket/LibJoynrWebSocketRuntime.h"
+
+#include "libjoynr/websocket/WebSocketMessagingStubFactory.h"
+#include "joynr/system/WebSocketClientAddress.h"
+#include "libjoynr/websocket/WebSocketLibJoynrMessagingSkeleton.h"
+#include "joynr/Util.h"
+
+namespace joynr {
+
+LibJoynrWebSocketRuntime::LibJoynrWebSocketRuntime(QSettings* settings):
+    LibJoynrRuntime(settings),
+    wsLibJoynrMessagingSkeleton(Q_NULLPTR)
+{
+    QString messagingUuid = Util::createUuid().replace("-", "");
+    QString libjoynrMessagingId("libjoynr.messaging.participantid_" + messagingUuid);
+    QSharedPointer<joynr::system::Address> libjoynrMessagingAddress(
+                new system::WebSocketClientAddress(
+                    libjoynrMessagingId
+                )
+    );
+
+    // create connection to parent routing service
+    QSharedPointer<joynr::system::Address> ccMessagingAddress(
+                new system::WebSocketAddress(
+                    // TODO: read CC address from settings file
+                    system::WebSocketProtocol::WS,  // protocol
+                    QString("localhost"),           // host
+                    4242,                           // port
+                    QString()                       // path
+                )
+    );
+
+    LibJoynrRuntime::init(new WebSocketMessagingStubFactory(), libjoynrMessagingAddress, ccMessagingAddress);
+}
+
+LibJoynrWebSocketRuntime::~LibJoynrWebSocketRuntime() {
+    delete wsLibJoynrMessagingSkeleton;
+    wsLibJoynrMessagingSkeleton = Q_NULLPTR;
+}
+
+void LibJoynrWebSocketRuntime::startLibJoynrMessagingSkeleton(MessageRouter &messageRouter)
+{
+    // create messaging skeleton using uuid
+    wsLibJoynrMessagingSkeleton = new WebSocketLibJoynrMessagingSkeleton(messageRouter);
+}
+
+} // namespace joynr
