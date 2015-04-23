6e98f33a9a560c479a0a20f3b572ee7806177a12
David Katz <David.Katz@bmw-carit.de>
2015-04-23 09:16:42 +0200

    java: correct format of license header in PublicationTimersTest
    Change-Id: I6686a3860d892b9afa8f8e227e77c4c672800bbd

--- java/core/libjoynr/src/test/java/io/joynr/pubsub/publication/PublicationTimersTest.java
+++ java/core/libjoynr/src/test/java/io/joynr/pubsub/publication/PublicationTimersTest.java
@@ -8,9 +8,9 @@ package io.joynr.pubsub.publication;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -74,8 +74,8 @@ public class PublicationTimersTest {
 
     @Test(timeout = 4000)
     public void publicationsSentUntilExpiryDate() throws InterruptedException, JoynrSendBufferFullException,
-                                                      JoynrMessageNotSentException, JsonGenerationException,
-                                                      JsonMappingException, IOException {
+                                                 JoynrMessageNotSentException, JsonGenerationException,
+                                                 JsonMappingException, IOException {
         LOG.debug("Starting PublicationTimersTest.timerIsStoppedWhenEnddateIsReached test");
         int period = 500;
         int subscriptionLength = 1100;
