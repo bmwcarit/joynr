be235b98481f9d6594446ef6cbefbeece3c7c1c6
David Katz <David.Katz@bmw-carit.de>
2015-07-29 10:32:22 +0200

    [build] prevent race condition with CI deployed artefacts in radio
    There was a race condition with the continuous CI servers whereby
    the CI servers were deploying versions of the generator that
    were not compatible with a given verify build, but were being
    downloaded by maven after the local install had occurred.
    
    The fix is to handle the whole generate step in one reactor
    
    Change-Id: I64b2971bf9d67fe10bdcbcc9f95cda682ef278ad

--- docker/joynr-cpp/scripts/build/cpp-radio-app.sh
+++ docker/joynr-cpp/scripts/build/cpp-radio-app.sh
@@ -29,18 +29,16 @@ env
 
 # fail on first error
 set -e
-(
-     # radio currently also generating for Java, so install the java-generator too
-     # until we can exclude the Java build of radio using @executionId (see below)
-    cd /data/src/java/java-generator
-    mvn clean install -P no-license-and-notice,no-java-formatter,no-checkstyle -DskipTests
-)
 
-cd /data/src/examples/radio-app
-
-# once updated to maven 3.3.1, execute the radio-app generation explicitly without
-# java using @executionId. See http://jira.codehaus.org/browse/MNG-5768
-mvn -P no-license-and-notice generate-sources
+# radio currently also generating for Java, so install the java-generator too
+# until we can exclude the Java build of radio using @executionId (see below)
+cd /data/src/
+mvn clean install -P no-license-and-notice,no-java-formatter,no-checkstyle -DskipTests \
+--projects \
+io.joynr.tools.generator:generator-framework,\
+io.joynr.java:java-generator,\
+io.joynr.cpp:cpp-generator,\
+io.joynr.examples:radio-app
 
 rm -rf /data/build/radio
 mkdir /data/build/radio
