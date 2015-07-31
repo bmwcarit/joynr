39eddadc24decfeddc6b5ab0f77c2e8bfc6e07f3
David Katz <David.Katz@bmw-carit.de>
2015-07-31 10:48:42 +0200

    [build] add install of android sdk 21 to docker build
    avoids having to install the sdks jars to a local nexus etc.
    
    Change-Id: I40695feb89e846b95e6a67369de93f5ae688595d

--- joynr-setup-android-sdk.sh
+++ joynr-setup-android-sdk.sh
@@ -19,20 +19,20 @@ if [ -z "$ANDROID_HOME" ] || [ ! -d "$ANDROID_HOME" ]
 then
 	echo '$ANDROID_HOME should be set to the Android SDK directory'
 	exit 1
-fi 
+fi
 
 # fail on first error
 set -e
 
 (
 	log "CLONE SDK DEPLOYER"
+	rm -rf maven-android-sdk-deployer
 	git clone --depth 1 https://github.com/simpligility/maven-android-sdk-deployer
 )
 (
 	log "INSTALL LOLLIPOP SDK TO MAVEN REPOSITORY"
 	cd maven-android-sdk-deployer
-	mvn install -P 5.0
-	cd -
+	mvn clean install --projects com.simpligility.android.sdk-deployer:android-21 --also-make
 )
 (
 	log "CLEANUP"
