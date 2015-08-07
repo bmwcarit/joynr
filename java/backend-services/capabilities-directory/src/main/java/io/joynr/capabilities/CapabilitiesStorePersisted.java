ab89c0f84de65dcd504aa74c7c5db010e629b7f0
David Katz <David.Katz@bmw-carit.de>
2015-08-07 22:52:41 +0200

    [Java] don't rollback unless transaction is active
    otherwise an IllegalStateException is thrown
    
    Change-Id: I0b2dfff3b10f435ee1ea5a8cb4a67280f69cee7d

--- java/backend-services/capabilities-directory/src/main/java/io/joynr/capabilities/CapabilitiesStorePersisted.java
+++ java/backend-services/capabilities-directory/src/main/java/io/joynr/capabilities/CapabilitiesStorePersisted.java
@@ -89,7 +89,9 @@ public class CapabilitiesStorePersisted implements CapabilitiesStore {
             entityManager.merge(capabilityEntry);
             transaction.commit();
         } catch (Exception e) {
-            transaction.rollback();
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
             logger.error("unable to add capabilityEntry: {}, reason: {}", capabilityEntry, e.getMessage());
         } finally {
         }
@@ -137,7 +139,9 @@ public class CapabilitiesStorePersisted implements CapabilitiesStore {
             entityManager.remove(capabilityEntry);
             transaction.commit();
         } catch (Exception e) {
-            transaction.rollback();
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
             logger.error("unable to remove capability: {} reason: {}", participantId, e.getMessage());
             return false;
         } finally {
