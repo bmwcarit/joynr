d6fb29e7ad37d77cc2a5fdbcc07d9dcedb44386f
Christoph Ainhauser <christoph.ainhauser@bmw.de>
2015-08-14 13:19:23 +0200

    [JS] update docker scripts
    Change-Id: I535a9e9fdee98e3b77f0764a55ff95089c0488f6

--- docker/joynr-javascript/scripts/build/javascript-clean-build.sh
+++ docker/joynr-javascript/scripts/build/javascript-clean-build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+source /data/scripts/global.sh
+
+# fail on first error
 set -e
-(
-  cd /data/src/javascript
-  mvn clean install \
-  -Djslint.failOnError=true \
-  -Dskip.copy-notice-file=true \
-  -Dskip.unpack-license-info=true
-)
+
+log "INSTALL JOYNR BASE MODEL, TOOLS AND INFRASTRUCTURE SERVICES"
+cd /data/src
+mvn clean install -P no-license-and-notice,no-java-formatter,no-checkstyle -DskipTests
+
+log "building joynr JavaScript API"
+cd /data/src/javascript
+mvn clean install \
+    -Djslint.failOnError=true \
+    -Dskip.copy-notice-file=true \
+    -Dskip.unpack-license-info=true
