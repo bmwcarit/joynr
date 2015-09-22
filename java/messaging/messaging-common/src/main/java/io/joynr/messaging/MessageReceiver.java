48f289e18e3f7e08025a08995808e081cde0c026
Christoph Ainhauser <Christoph.Ainhauser@bmw-carit.de>
2015-09-22 21:53:34 +0200

    [Java] Get rid of java warnings
    Add javadoc info for parameters in MessageReceiver.start
    
    Change-Id: I6e6fca841229e03d00286dd98ed7ecee008fc5a5

--- java/messaging/messaging-common/src/main/java/io/joynr/messaging/MessageReceiver.java
+++ java/messaging/messaging-common/src/main/java/io/joynr/messaging/MessageReceiver.java
@@ -54,9 +54,10 @@ public interface MessageReceiver {
     void resume();
 
     /**
+     * @param messageArrivedListener the listener to be informed about received messages and errors 
      * @param receiverStatusListeners list of status listeners providing callbacks
      * for successful or failed start
      * @return a future that signals when the receiver is ready to be used.
      */
-    Future<Void> start(MessageArrivedListener messageReceiver, ReceiverStatusListener... receiverStatusListeners);
+    Future<Void> start(MessageArrivedListener messageArrivedListener, ReceiverStatusListener... receiverStatusListeners);
 }
