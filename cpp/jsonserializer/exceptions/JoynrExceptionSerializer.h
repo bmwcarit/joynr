fc44a4024ee56cfefe828b5329326166a761ffdb
Christoph Ainhauser <Christoph.Ainhauser@bmw-carit.de>
2015-11-23 08:59:14 +0100

    [C++] Initial import of JoynrExceptionSerializer
    Change-Id: Ice72a354ae72e00a28786708cd294df0ee0277c3

---
+++ cpp/jsonserializer/exceptions/JoynrExceptionSerializer.h
@@ -0,0 +1,34 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2015 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#ifndef JOYNREXCEPTIONSERIALIZER_H
+#define JOYNREXCEPTIONSERIALIZER_H
+
+#include "joynr/ClassDeserializer.h"
+#include "joynr/ClassSerializer.h"
+#include "joynr/exceptions/JoynrException.h"
+#include "joynr/IDeserializer.h"
+
+#include <sstream>
+
+namespace joynr
+{
+
+
+} /* namespace joynr */
+#endif // JOYNREXCEPTIONSERIALIZER_H
