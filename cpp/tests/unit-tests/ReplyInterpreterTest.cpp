320f9860da7b7325ed5b82d991d74ba52384a3f9
Thomas Reuter <thomas.reuter@de.bertrandt.com>
2015-11-23 15:33:33 +0100

    [C++] add error reporting tests for ReplyInterpreter
    Change-Id: Ice7ca6299cb0c8ec542bdfe53b0f4de21e14e280

--- cpp/tests/unit-tests/ReplyInterpreterTest.cpp
+++ cpp/tests/unit-tests/ReplyInterpreterTest.cpp
@@ -1,7 +1,7 @@
 /*
  * #%L
  * %%
- * Copyright (C) 2011 - 2013 BMW Car IT GmbH
+ * Copyright (C) 2011 - 2015 BMW Car IT GmbH
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,10 @@
 using ::testing::A;
 using ::testing::_;
 
+MATCHER_P(joynrException, other, "") {
+    return arg.getTypeName() == other.getTypeName() && arg.getMessage() == other.getMessage();
+}
+
 using namespace joynr;
 
 class ReplyInterpreterTest : public ::testing::Test {
@@ -52,13 +56,15 @@ TEST_F(ReplyInterpreterTest, execute_calls_caller) {
     int myAltitude = 13;
     EXPECT_CALL(*callback, onSuccess(Property(&types::Localisation::GpsLocation::getAltitude, myAltitude)))
                 .Times(1);
+    EXPECT_CALL(*callback, onError(_,_)).Times(0);
 
     // Create a reply caller
     std::shared_ptr<IReplyCaller> icaller(new ReplyCaller<types::Localisation::GpsLocation>(
             [callback](const RequestStatus& status, const types::Localisation::GpsLocation& location) {
                 callback->onSuccess(location);
             },
-            [](const RequestStatus& status, const exceptions::JoynrException& error){
+            [callback](const RequestStatus& status, const exceptions::JoynrException& error){
+                callback->onError(status, error);
             }));
 
     // Create a reply
@@ -73,6 +79,126 @@ TEST_F(ReplyInterpreterTest, execute_calls_caller) {
     IReplyInterpreter& interpreter = registrar.getReplyInterpreter(Util::getTypeId<types::Localisation::GpsLocation>());
     interpreter.execute(icaller, reply);
 }
+
+TEST_F(ReplyInterpreterTest, execute_calls_caller_void) {
+    // Register metatypes
+    MetaTypeRegistrar& registrar = MetaTypeRegistrar::instance();
+
+    // Create a mock callback
+    std::shared_ptr<MockCallbackWithOnErrorHavingRequestStatus<void>> callback(new MockCallbackWithOnErrorHavingRequestStatus<void>());
+    EXPECT_CALL(*callback, onSuccess())
+                .Times(1);
+    EXPECT_CALL(*callback, onError(_,_)).Times(0);
+
+    // Create a reply caller
+    std::shared_ptr<IReplyCaller> icaller(new ReplyCaller<void>(
+            [callback](const RequestStatus& status) {
+                callback->onSuccess();
+            },
+            [callback](const RequestStatus& status, const exceptions::JoynrException& error){
+                callback->onError(status, error);
+            }));
+
+    // Create a reply
+    Reply reply;
+
+    // Interpret the reply
+    IReplyInterpreter& interpreter = registrar.getReplyInterpreter(Util::getTypeId<void>());
+    interpreter.execute(icaller, reply);
+}
+
+TEST_F(ReplyInterpreterTest, execute_calls_caller_with_error) {
+    // Register metatypes
+    MetaTypeRegistrar& registrar = MetaTypeRegistrar::instance();
+    registrar.registerReplyMetaType<types::Localisation::GpsLocation>();
+
+    // Create a reply
+    exceptions::ProviderRuntimeException error("ReplyInterpreterTestProviderRuntimeExeption");
+    Reply reply;
+    reply.setError(Variant::make<exceptions::ProviderRuntimeException>(error));
+
+    // Create a mock callback
+    std::shared_ptr<MockCallbackWithOnErrorHavingRequestStatus<joynr::types::Localisation::GpsLocation>> callback(new MockCallbackWithOnErrorHavingRequestStatus<joynr::types::Localisation::GpsLocation>());
+    EXPECT_CALL(*callback, onSuccess(_))
+                .Times(0);
+    EXPECT_CALL(*callback, onError(Property(&RequestStatus::getCode, RequestStatusCode::ERROR),joynrException(error)))
+                .Times(1);
+
+    // Create a reply caller
+    std::shared_ptr<IReplyCaller> icaller(new ReplyCaller<types::Localisation::GpsLocation>(
+            [callback](const RequestStatus&, const types::Localisation::GpsLocation& location) {
+                callback->onSuccess(location);
+            },
+            [callback](const RequestStatus& status, const exceptions::JoynrException& error){
+                callback->onError(status, error);
+            }));
+
+    // Interpret the reply
+    IReplyInterpreter& interpreter = registrar.getReplyInterpreter(Util::getTypeId<types::Localisation::GpsLocation>());
+    interpreter.execute(icaller, reply);
+}
+
+TEST_F(ReplyInterpreterTest, execute_calls_caller_void_with_error) {
+    // Register metatypes
+    MetaTypeRegistrar& registrar = MetaTypeRegistrar::instance();
+
+    // Create a reply
+    exceptions::ProviderRuntimeException error("ReplyInterpreterTestProviderRuntimeExeption");
+    Reply reply;
+    reply.setError(Variant::make<exceptions::ProviderRuntimeException>(error));
+
+    // Create a mock callback
+    std::shared_ptr<MockCallbackWithOnErrorHavingRequestStatus<void>> callback(new MockCallbackWithOnErrorHavingRequestStatus<void>());
+    EXPECT_CALL(*callback, onSuccess())
+                .Times(0);
+    EXPECT_CALL(*callback, onError(Property(&RequestStatus::getCode, RequestStatusCode::ERROR),joynrException(error)))
+                .Times(1);
+
+    // Create a reply caller
+    std::shared_ptr<IReplyCaller> icaller(new ReplyCaller<void>(
+            [callback](const RequestStatus& status) {
+                callback->onSuccess();
+            },
+            [callback](const RequestStatus& status, const exceptions::JoynrException& error){
+                callback->onError(status, error);
+            }));
+
+    // Interpret the reply
+    IReplyInterpreter& interpreter = registrar.getReplyInterpreter(Util::getTypeId<void>());
+    interpreter.execute(icaller, reply);
+}
+
+TEST_F(ReplyInterpreterTest, execute_empty_reply) {
+    // Register metatypes
+    MetaTypeRegistrar& registrar = MetaTypeRegistrar::instance();
+    registrar.registerReplyMetaType<types::Localisation::GpsLocation>();
+
+    exceptions::JoynrRuntimeException error("Reply object had no response.");
+
+    // Create a mock callback
+    std::shared_ptr<MockCallbackWithOnErrorHavingRequestStatus<joynr::types::Localisation::GpsLocation>> callback(new MockCallbackWithOnErrorHavingRequestStatus<joynr::types::Localisation::GpsLocation>());
+    EXPECT_CALL(*callback, onSuccess(_))
+                .Times(0);
+    EXPECT_CALL(*callback, onError(Property(&RequestStatus::getCode, RequestStatusCode::ERROR),joynrException(error)))
+                .Times(1);
+
+    // Create a reply caller
+    std::shared_ptr<IReplyCaller> icaller(new ReplyCaller<types::Localisation::GpsLocation>(
+            [callback](const RequestStatus& status, const types::Localisation::GpsLocation& location) {
+                callback->onSuccess(location);
+            },
+            [callback](const RequestStatus& status, const exceptions::JoynrException& error){
+                callback->onError(status, error);
+            }));
+
+    // Create a reply
+    Reply reply;
+
+    // Interpret the reply
+    IReplyInterpreter& interpreter = registrar.getReplyInterpreter(Util::getTypeId<types::Localisation::GpsLocation>());
+    interpreter.execute(icaller, reply);
+}
+
 
 TEST_F(ReplyInterpreterTest, create_createsGpsInterpreterOnlyOnce) {
 
