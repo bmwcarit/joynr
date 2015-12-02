ad59e49db24d146328c2e325e20867de52640c25
rdrcelic <radojko.drcelic@jambit.com>
2015-12-02 08:38:37 +0100

    [C++] added custom c:wqache container class
    to replace QCache in ClientQCache
    
    Change-Id: I385d41520026bdace179cc6c1dd3e882bfa12600

---
+++ cpp/common/include/joynr/Cache.h
@@ -0,0 +1,140 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2015 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#ifndef CACHE_H
+#define CACHE_H
+
+#include <map>
+#include <memory>
+
+namespace joynr
+{
+/**
+ * Class Cache is designed after QCache from Qt framework.
+ * It implements some basic features of QCache needed for
+ * joynr purposes (if used outside joynr as replacement for QCache
+ * consider extending).
+ */
+template <typename Key, typename Value>
+class Cache
+{
+public:
+    /**
+     * @brief Cache with default macCost is 100
+     */
+    Cache() : cacheMap(), maxCost(100)
+    {
+    }
+    /**
+     * @brief Cache with initialy customized maxCost
+     * @param maxCost
+     */
+    Cache(int maxCost) : cacheMap(), maxCost(maxCost)
+    {
+    }
+    /**
+     * @brief contains check if Cache contains given key
+     * @param key
+     * @return
+     */
+    bool contains(const Key& key)
+    {
+        return cacheMap.find(key) != cacheMap.end();
+    }
+    /**
+     * @brief object to lookup object in the cache
+     * @param key retrieve object stored under given key
+     * @return pointer to object, or 0 if no object under given key found
+     */
+    Value* object(const Key& key)
+    {
+        if (cacheMap.find(key) == cacheMap.end()) {
+            return 0;
+        }
+        return cacheMap.find(key)->second.get();
+    }
+    /**
+     * @brief setMaxCost change maxCost
+     * @param maxCost if maxCost is smaller then current Cache size,
+     * cache is going to be truncated by removing elements from the beginning
+     * (oldest are going to be thrown away)
+     */
+    void setMaxCost(int maxCost)
+    {
+        if (maxCost < cacheMap.size()) {
+            removeElementsFromTheBeginning(cacheMap.size() - maxCost);
+        }
+        this->maxCost = maxCost;
+    }
+    /**
+     * @brief insert value under given key. If cache contains value with same key
+     * already, previous value is going to be removed, deleted and finaly replaced with new value
+     * (this feature comes from std::map<Key, std::unique_ptr<Value>> implicitly)
+     * @param key
+     * @param value
+     */
+    void insert(Key key, Value* value)
+    {
+        if (cacheMap.size() == maxCost) {
+            removeElementsFromTheBeginning(1);
+            assert(cacheMap.size() == maxCost - 1);
+        }
+        cacheMap.insert(std::make_pair(key, std::unique_ptr<Value>(value)));
+        assert(cacheMap.size() != 0);
+        assert(cacheMap.size() <= maxCost);
+    }
+    /**
+     * @brief clear removes and destroys all values
+     */
+    void clear()
+    {
+        cacheMap.clear();
+    }
+    /**
+     * @brief size
+     * @return number of key-value pairs stored in cache
+     */
+    int size()
+    {
+        return cacheMap.size();
+    }
+
+private:
+    std::map<Key, std::unique_ptr<Value>> cacheMap;
+    int maxCost;
+
+    /**
+     * @brief removeElementsFromTheBeginning removes given number of elements from the beginning
+     * @param numberOfElements
+     */
+    void removeElementsFromTheBeginning(int numberOfElements)
+    {
+        while (numberOfElements != 0) {
+            auto firstAndOldestElement = cacheMap.begin();
+            std::unique_ptr<Value> olderstValue(std::move(cacheMap.begin()->second));
+            cacheMap.erase(firstAndOldestElement);
+
+            Value* value_ptr = olderstValue.release();
+            delete value_ptr;
+            --numberOfElements;
+        }
+    }
+};
+
+} // namespace joynr
+#endif // CACHE_H
