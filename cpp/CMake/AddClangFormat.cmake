2251e1256196c9943de7f8eb70a2f1d5ff4b6de5
Manuel Schiller <manuel.schiller@bmw-carit.de>
2015-12-14 10:40:47 +0100

    [C++, Build] avoid specifying sources for add_clang_format
    Change-Id: I62515736f7d3b85913fa6500e9a8257aee54158a

--- cpp/CMake/AddClangFormat.cmake
+++ cpp/CMake/AddClangFormat.cmake
@@ -1,18 +1,18 @@
 function(add_clang_format TARGET)
     if(${ENABLE_CLANG_FORMATTER})
+        get_property(TARGET_SOURCES TARGET ${TARGET} PROPERTY SOURCES)
         set(COMMENT "running C++ code formatter: clang-format")
         set(COMMENT "${COMMENT}\nworking directory: ${CMAKE_CURRENT_SOURCE_DIR}")
-	set(COMMENT "${COMMENT}\nprocessing files:\n${ARGN}")
+        set(COMMENT "${COMMENT}\nprocessing files:\n${TARGET_SOURCES}")
 
-	set(FORMAT_TARGET "format-${TARGET}")
-	add_custom_target(
+        set(FORMAT_TARGET "format-${TARGET}")
+        add_custom_target(
             ${FORMAT_TARGET}
-	    COMMAND "${CLANG_FORMAT_PATH}" -i ${ARGN}
-	    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-	    COMMENT "${COMMENT}"
-	    VERBATIM
+            COMMAND "${CLANG_FORMAT_PATH}" -i ${TARGET_SOURCES}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            COMMENT "${COMMENT}"
+            VERBATIM
         )
         add_dependencies(${TARGET} ${FORMAT_TARGET})
     endif(${ENABLE_CLANG_FORMATTER})
 endfunction(add_clang_format)
-
