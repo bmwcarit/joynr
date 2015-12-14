52e4c4ad04514003602f65ef451fd670098d438d
Manuel Schiller <manuel.schiller@bmw-carit.de>
2015-12-14 17:36:16 +0100

    [C++, Build] optionally use clang-tidy
    Change-Id: I883bbb8cea45e96904efec4e10ecf0263e96d2fa

---
+++ cpp/CMake/AddClangTidy.cmake
@@ -0,0 +1,27 @@
+function(filter_out_headers INPUT OUTPUT)
+     set(FILTERED_LIST "")
+     foreach(ENTRY ${${INPUT}})
+       if(NOT ${ENTRY} MATCHES ".+\\.h$")
+           list(APPEND FILTERED_LIST ${ENTRY})
+       endif()
+     endforeach()
+     set(${OUTPUT} ${FILTERED_LIST} PARENT_SCOPE)
+endfunction(filter_out_headers)
+
+function(add_clang_tidy TARGET)
+    if(${ENABLE_CLANG_TIDY})
+
+        get_property(TARGET_SOURCES TARGET ${TARGET} PROPERTY SOURCES)
+        filter_out_headers(TARGET_SOURCES TARGET_SOURCES_WO_HEADERS)
+
+        set(TIDY_TARGET "tidy-${TARGET}")
+
+        add_custom_target(
+             ${TIDY_TARGET}
+             COMMAND ${CLANG_TIDY_PATH} -p ${CMAKE_BINARY_DIR} ${CLANG_TIDY_OPTIONS} ${TARGET_SOURCES_WO_HEADERS}
+             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+             VERBATIM
+        )
+        add_dependencies(${TARGET} ${TIDY_TARGET})
+    endif(${ENABLE_CLANG_TIDY})
+endfunction(add_clang_tidy)
