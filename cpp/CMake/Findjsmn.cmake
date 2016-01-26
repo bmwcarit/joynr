c0f115ee27016f409da28f688c2da1f121124d23
Manuel Schiller <manuel.schiller@bmw-carit.de>
2016-01-26 11:43:11 +0100

    [C++, Build] store result from Findjsmn.cmake in jsmn_FOUND
    Change-Id: I0e142e6e6b004b921bdd5be50a47431ba7b78969

--- cpp/CMake/Findjsmn.cmake
+++ cpp/CMake/Findjsmn.cmake
@@ -4,3 +4,5 @@ find_library(jsmn_LIBRARY libjsmn.a)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(jsmn DEFAULT_MSG
     jsmn_LIBRARY jsmn_INCLUDE_DIR)
+
+set(jsmn_FOUND ${JSMN_FOUND} CACHE BOOL "TRUE if jsmn was found, FALSE otherwise")
