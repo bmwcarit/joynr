bbd7069a86ee73083d53955002e3d06d37d62706
Manuel Schiller <manuel.schiller@bmw-carit.de>
2016-01-26 11:22:16 +0100

    [C++, Build] fix typo in Findjsmn.cmake
    Change-Id: I800c05441e7a2ddd6d05b2d022b5d462ae31b7b4

--- cpp/CMake/Findjsmn.cmake
+++ cpp/CMake/Findjsmn.cmake
@@ -2,5 +2,5 @@ find_path(jsmn_INCLUDE_DIR jsmn.h)
 find_library(jsmn_LIBRARY libjsmn.a)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(jmsn DEFAULT_MSG
+find_package_handle_standard_args(jsmn DEFAULT_MSG
     jsmn_LIBRARY jsmn_INCLUDE_DIR)
