68f4ee7ff2b7e6392d962569df1b97afe9206829
Christoph Ainhauser <Christoph.Ainhauser@bmw-carit.de>
2016-02-04 18:52:23 +0100

    [JS] CapabilitiesStore.checkAge ignores negative max ages
    This way, the age of capabilities can be ignored
    
    Change-Id: I0e4cee35e15fdb2694b6f5a39053c0bc49bfa6b1

--- javascript/libjoynr-js/src/main/js/joynr/capabilities/CapabilitiesStore.js
+++ javascript/libjoynr-js/src/main/js/joynr/capabilities/CapabilitiesStore.js
@@ -227,7 +227,7 @@ define(
                  */
                 var checkAge = function checkAge(discoveryEntry, maxAge) {
                     var registrationTime = registeredCapabilitiesTime[hashCode(discoveryEntry)];
-                    if (registrationTime === undefined || maxAge === undefined) {
+                    if (registrationTime === undefined || maxAge === undefined || maxAge < 0) {
                         return true;
                     }
                     return (Date.now() - registrationTime <= maxAge);
