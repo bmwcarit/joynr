728e8e5ebc00ceda4f44f0080154107c0e37d70b
Christian Hoff <christian.hoff@bmw-carit.de>
2016-02-09 10:38:29 +0100

    [C++, Build] use CURL import targets
    Change-Id: Iabc19c48f9b7f4af5ce4f67597309c8b52331a89

---
+++ cpp/CMake/CheckCurlImportTargets.cmake
@@ -0,0 +1,13 @@
+message(STATUS "variable CURL_FOUND=${CURL_FOUND}")
+message(STATUS "variable CURL_VERSION_STRING=${CURL_VERSION_STRING}")
+message(STATUS "variable CURL_INCLUDE_DIRS=${CURL_INCLUDE_DIRS}")
+message(STATUS "variable CURL_LIBRARIES=${CURL_LIBRARIES}")
+
+if(NOT TARGET cURL::curl)
+    message(STATUS "cURL::curl target not defined. Creating IMPORTED target.")
+    add_library(cURL::curl SHARED IMPORTED)
+    set_target_properties(cURL::curl PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}"
+        IMPORTED_LOCATION "${CURL_LIBRARIES}"
+    )
+endif(NOT TARGET cURL::curl)
