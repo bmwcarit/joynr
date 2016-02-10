2ab479060b9a6f8f99045674eb1e56fcc062d203
Thomas Pal <thomas.pal@bmw-carit.de>
2016-02-10 15:08:40 +0100

    [C++] Removed reference to time_point in Timer::runTimer()
    AddressSanitizer reported heap-use-after-free for the time point.
    
    Change-Id: I33f753637efd0b245d5608887cb19bc715804662

--- cpp/common/concurrency/Timer.cpp
+++ cpp/common/concurrency/Timer.cpp
@@ -146,7 +146,7 @@ void Timer::runTimer()
 
             // Get the first timer in the sorted map and lock until expiry
             std::unique_lock<std::mutex> lock(mutex);
-            const std::chrono::system_clock::time_point& tp = timers.begin()->first;
+            const std::chrono::system_clock::time_point tp = timers.begin()->first;
             if (waitCondition.wait_until(lock, tp) == std::cv_status::timeout) {
 
                 // In case of a race condition we should check if the time
