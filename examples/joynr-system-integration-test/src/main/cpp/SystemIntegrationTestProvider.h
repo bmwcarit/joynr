14528ba04ead17ae563cf09254b99af0b73e8ea7
Donato Sciarra <donato.sciarra@bmw-carit.de>
2016-02-18 11:16:13 +0100

    [C++,Test] Introduce simple application for joynr system int tests
    The consumer and provider here implemented are communicating through
    the SystemIntegrationTest interface via websocket.
    
    The consumer performs a test sequence and exits with EXIT_SUCCESS in
    case the provider replies as expected. As of now, the conumser
    invokes the "add" method and checks if the result is the sum of the two
    in arguments.
    
    The provider exits once all the expected test sequences have been
    performed. As of now, he expects the call to the "add" method.
    
    Change-Id: Ief68f00dc95e4f6c8adc0b13530e7f1fbfca11ce

---
+++ examples/joynr-system-integration-test/src/main/cpp/SystemIntegrationTestProvider.h
@@ -0,0 +1,54 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#ifndef SYSTEM_INTEGRATION_TEST_PROVIDER_H
+#define SYSTEM_INTEGRATION_TEST_PROVIDER_H
+
+#include <cstdint>
+#include <functional>
+
+#include "joynr/test/DefaultSystemIntegrationTestProvider.h"
+
+namespace joynr
+{
+namespace exceptions
+{
+class ProviderRuntimeException;
+} // exceptions
+} // joynr
+
+class SystemIntegrationTestProvider : public joynr::test::DefaultSystemIntegrationTestProvider
+{
+public:
+    explicit SystemIntegrationTestProvider(std::function<void(void)> successCallback);
+    virtual ~SystemIntegrationTestProvider() override = default;
+
+    // Implement interface method
+    virtual void add(const std::int32_t& addendA,
+                     const std::int32_t& addendB,
+                     std::function<void(const std::int32_t& sum)> onSuccess,
+                     std::function<void(const joynr::exceptions::ProviderRuntimeException&)>
+                             onError) override;
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(SystemIntegrationTestProvider);
+
+    std::function<void(void)> successCallback;
+};
+
+#endif // SYSTEM_INTEGRATION_TEST_PROVIDER_H
