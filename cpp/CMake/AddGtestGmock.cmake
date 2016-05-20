7aeed771552408f6cf2a5284a1f4448e39af7a1a
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2016-05-20 10:43:58 +0200

    [C++] Refactored CMake environment for Gtest
    * AddExternalProject now checks if target has been
      already added elsewhere
    
    * Added AddGtestGmock
    
    * Removed AddTest (content now included in AddGtestGmock)
    
    * Robustness and standard cpp tests now include Gtest/Gmock
      using AddGtestGmock
    
    Change-Id: I0af6034433b32d4ee0d2d7ef0dcc573a508e7ce6

--- cpp/CMake/AddGtestGmock.cmake
+++ cpp/CMake/AddGtestGmock.cmake
@@ -5,13 +5,14 @@ option(
 )
 message(STATUS "option USE_PLATFORM_GTEST_GMOCK=" ${USE_PLATFORM_GTEST_GMOCK})
 
+include(AddExternalProject)
+
 if (USE_PLATFORM_GTEST_GMOCK)
 
     find_package(GTest REQUIRED)
     find_package(GMock REQUIRED)
 
 else (USE_PLATFORM_GTEST_GMOCK)
-
     ### clone googletest git repository ###########################################################
 
     # this repository contains both googletest and googlemock
@@ -78,5 +79,34 @@ else (USE_PLATFORM_GTEST_GMOCK)
     message(STATUS "variable googlemock_source_dir=${googlemock_source_dir}")
     set(GMOCK_INCLUDE_DIRS ${googlemock_source_dir}/include)
     set(GMOCK_LIBRARIES ${googlemock_binary_dir}/libgmock.a)
-
 endif(USE_PLATFORM_GTEST_GMOCK)
+
+function(AddTest TARGET)
+    add_executable(
+        ${TARGET}
+        ${ARGN}
+    )
+    set_target_properties(
+        ${TARGET}
+        PROPERTIES
+        COMPILE_FLAGS "-Wno-effc++ -Wno-unused-parameter"
+        AUTOMOC TRUE
+    )
+    if(NOT USE_PLATFORM_GTEST_GMOCK)
+        add_dependencies(${TARGET} googletest)
+        add_dependencies(${TARGET} googlemock)
+    endif(NOT USE_PLATFORM_GTEST_GMOCK)
+
+    if(NOT USE_PLATFORM_MOSQUITTO)
+        add_dependencies(${TARGET} mosquitto::mosquitto)
+    endif(NOT USE_PLATFORM_MOSQUITTO)
+
+    target_include_directories(
+        ${TARGET}
+        SYSTEM
+        PRIVATE
+        ${GTEST_INCLUDE_DIRS}
+        ${GMOCK_INCLUDE_DIRS}
+        ${MOSQUITTO_INCLUDE_DIRS}
+    )
+endfunction(AddTest)
