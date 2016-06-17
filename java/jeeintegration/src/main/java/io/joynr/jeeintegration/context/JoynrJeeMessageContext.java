1f2d7d0e29a90d86e8b059f65f44867034a9c51c
Clive Jevons <clive@jevons-it.net>
2016-06-17 13:27:45 +0200

    [JEE] add custom scope context for joynr messages
    Add a custom scope and context for that scope which are registered via a
    portable CDI extension. This can then be used to hole instances of beans
    valid for the duration of the processing of a joynr message in the CDI
    container.
    
    Change-Id: I1636aa774a61298ce963cff98ffe4d4d1c68748c

---
+++ java/jeeintegration/src/main/java/io/joynr/jeeintegration/context/JoynrJeeMessageContext.java
@@ -0,0 +1,93 @@
+package io.joynr.jeeintegration.context;
+
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.enterprise.context.ContextNotActiveException;
+import javax.enterprise.context.spi.Context;
+import javax.enterprise.context.spi.Contextual;
+import javax.enterprise.context.spi.CreationalContext;
+
+import io.joynr.jeeintegration.api.JoynrJeeMessageScoped;
+
+/**
+ * The {@link Context} which holds {@Link JoynrJeeMessageScoped} {@link Contextual contextual instances} for the
+ * duration of a joynr message being processed.
+ */
+public class JoynrJeeMessageContext implements Context {
+
+    private static JoynrJeeMessageContext instance;
+
+    private ThreadLocal<Map<Contextual<?>, Object>> contextualStore = new ThreadLocal<>();
+
+    @Override
+    public Class<? extends Annotation> getScope() {
+        return JoynrJeeMessageScoped.class;
+    }
+
+    @Override
+    public <T> T get(Contextual<T> contextual, CreationalContext<T> creationalContext) {
+        T instance = get(contextual);
+        if (instance == null) {
+            instance = contextual.create(creationalContext);
+            if (instance != null) {
+                contextualStore.get().put(contextual, instance);
+            }
+        }
+        return instance;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T get(Contextual<T> contextual) {
+        Map<Contextual<?>, Object> store = contextualStore.get();
+        if (store == null) {
+            throw new ContextNotActiveException();
+        }
+        return (T) store.get(contextual);
+    }
+
+    @Override
+    public boolean isActive() {
+        return contextualStore.get() != null;
+    }
+
+    public void activate() {
+        contextualStore.set(new HashMap<>());
+    }
+
+    public void deactivate() {
+        if (contextualStore.get() != null) {
+            contextualStore.get().clear();
+        }
+        contextualStore.remove();
+    }
+
+    public synchronized static JoynrJeeMessageContext getInstance() {
+        if (instance == null) {
+            instance = new JoynrJeeMessageContext();
+        }
+        return instance;
+    }
+
+}
