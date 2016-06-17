d01f7c3ef6177ab99b533fc54a44b42cdae7844c
Clive Jevons <clive@jevons-it.net>
2016-06-17 16:55:05 +0200

    [JEE, Examples] example radio jee app
    An example implementation of the radio app using the JEE integration.
    
    The implementation can also be used as a guide for how to structure
    a joynr app being implemented for JEE.
    
    Note that because jetty will start up on port 8080 by default, and
    Glassfish / Payara also do, copied and changed the joynr backend services
    config from radio-app to start jetty on 8383 instead.
    The jee apps are also configured to connect to that port.
    
    In joynr 0.18 there is a known bug whereby JEE apps will not start up if
    the endpoint registry url is not configured. This is fixed in 0.19 and
    there doesn't need to be specified at all in the configs if the http
    bridge is not being used.
    
    The JEE integration does not currently support subscription / broadcasts,
    yet the radio.fidl includes both. In order to be able to successfully
    deploy the application, the provider needs to at least have the
    SubscriptionPublisherInjection interface, because the joynr runtime expects
    to be able to perform the injection. Added as an empty method with a comment
    pointing out that the JEE integration does not currently support this.
    
    Change-Id: I1b69ecd8f927714900ed37423cbb22803e49741c

---
+++ examples/radio-jee/radio-jee-provider/src/main/java/io/joynr/examples/jee/GeoLocationService.java
@@ -0,0 +1,51 @@
+package io.joynr.examples.jee;
+
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+
+import joynr.vehicle.Country;
+import joynr.vehicle.GeoPosition;
+
+/**
+ * A dummy implementation of a geo-location service which returns lat/long values given a {@link Country}.
+ */
+@Singleton
+public class GeoLocationService {
+
+    private Map<Country, GeoPosition> geoPositions = new HashMap<>();
+
+    @PostConstruct
+    public void initialiseGeoPositions() {
+        geoPositions.put(Country.AUSTRALIA, new GeoPosition(-37.8141070, 144.9632800)); // Melbourne
+        geoPositions.put(Country.ITALY, new GeoPosition(46.4982950, 11.3547580)); // Bolzano
+        geoPositions.put(Country.CANADA, new GeoPosition(53.5443890, -113.4909270)); // Edmonton
+        geoPositions.put(Country.GERMANY, new GeoPosition(48.1351250, 11.5819810)); // Munich
+    }
+
+    public GeoPosition getPositionFor(Country country) {
+        return geoPositions.get(country);
+    }
+
+}
