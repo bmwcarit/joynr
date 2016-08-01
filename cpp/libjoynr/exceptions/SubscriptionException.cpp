769709061429cd334d41d2a0114c0edd706e3a73
Thomas Reuter <thomas.reuter@de.bertrandt.com>
2016-08-01 16:45:40 +0200

    [C++, API] Create new exception type SubscriptionException
    SubscriptionExceptions are used for reporting error conditions when
    creating a subscription (e.g. the provided subscription parameters
    are not correct etc.) that should be transmitted back to consumer side.
    
    Change-Id: I204816baa784c350c078410cf0ab0b47a4af3006

---
+++ cpp/libjoynr/exceptions/SubscriptionException.cpp
@@ -0,0 +1,82 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#include "joynr/exceptions/SubscriptionException.h"
+
+namespace joynr
+{
+
+namespace exceptions
+{
+
+SubscriptionException::SubscriptionException() noexcept : JoynrRuntimeException(), subscriptionId()
+{
+}
+
+SubscriptionException::SubscriptionException(const std::string& message,
+                                             const std::string& subscriptionId) noexcept
+        : JoynrRuntimeException(message),
+          subscriptionId(subscriptionId)
+{
+}
+
+SubscriptionException::SubscriptionException(const std::string& subscriptionId) noexcept
+        : JoynrRuntimeException(subscriptionId),
+          subscriptionId(subscriptionId)
+{
+}
+
+SubscriptionException::SubscriptionException(const SubscriptionException& other)
+        : JoynrRuntimeException(other), subscriptionId(other.subscriptionId)
+{
+}
+
+const std::string& SubscriptionException::getSubscriptionId() const
+{
+    return subscriptionId;
+}
+
+void SubscriptionException::setSubscriptionId(const std::string& subscriptionId)
+{
+    this->subscriptionId = subscriptionId;
+}
+
+const std::string& SubscriptionException::getTypeName() const
+{
+    return SubscriptionException::TYPE_NAME();
+}
+
+SubscriptionException* SubscriptionException::clone() const
+{
+    return new SubscriptionException(const_cast<SubscriptionException&>(*this));
+}
+
+const std::string& SubscriptionException::TYPE_NAME()
+{
+    static const std::string TYPE_NAME = "joynr.exceptions.SubscriptionException";
+    return TYPE_NAME;
+}
+
+bool SubscriptionException::operator==(const SubscriptionException& other) const
+{
+    return message == other.getMessage() && subscriptionId == other.getSubscriptionId();
+}
+
+} // namespace exceptions
+
+} // namespace joynr
