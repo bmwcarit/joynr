4f58e2d5237dcb17b5c8a26b3b769077eab9ac90
Christoph Ainhauser <Christoph.Ainhauser@bmw-carit.de>
2016-08-11 08:07:00 +0200

    [benchmark] enable DLT logger for short circuit test
    Change-Id: I68e63449a2499125286c8f89fa1f7a197847be32

--- tests/performance-test/src/main/cpp/short-circuit/ShortCircuitApplication.cpp
+++ tests/performance-test/src/main/cpp/short-circuit/ShortCircuitApplication.cpp
@@ -18,9 +18,17 @@
  */
 
 #include "ShortCircuitTest.h"
+#include <boost/program_options.hpp>
+#ifdef JOYNR_ENABLE_DLT_LOGGING
+#include <dlt/dlt.h>
+#endif // JOYNR_ENABLE_DLT_LOGGING
 
 int main(int argc, char* argv[])
 {
+#ifdef JOYNR_ENABLE_DLT_LOGGING
+    // Register app at the dlt-daemon for logging
+    DLT_REGISTER_APP("JOYT", argv[0]);
+#endif // JOYNR_ENABLE_DLT_LOGGING
 
     namespace po = boost::program_options;
 
