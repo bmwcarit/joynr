0db07b22a082080a163f4a0efee45391b9658a4e
Christoph Ainhauser <Christoph.Ainhauser@bmw-carit.de>
2016-08-12 10:06:20 +0200

    [benchmark] short-circuit & serialiser tests use printStatistics
    Change-Id: I1be1088e15281f9631d7c7f126ced154251a1403

--- tests/performance-test/src/main/cpp/common/PerformanceTest.h
+++ tests/performance-test/src/main/cpp/common/PerformanceTest.h
@@ -20,10 +20,15 @@
 #ifndef PERFORMANCE_TEST_H
 #define PERFORMANCE_TEST_H
 
+#include <algorithm>
 #include <iomanip>
 #include <iostream>
 #include <chrono>
 #include <utility>
+#include <vector>
+
+using Clock = std::chrono::steady_clock;
+using ClockResolution = std::chrono::microseconds;
 
 struct PerformanceTest
 {
@@ -32,40 +37,73 @@ struct PerformanceTest
      * @returns average duration of a function call in milliseconds
      */
     template <typename Function, typename... Args>
-    double benchmark(std::uint64_t runs, Function&& fun, Args&&... args) const
+    std::vector<ClockResolution> benchmark(std::uint64_t runs, Function&& fun, Args&&... args) const
     {
-        using Clock = std::chrono::steady_clock;
-        using ClockResolution = std::chrono::microseconds;
         using ResultType = decltype(fun(args...));
 
-        const auto start = Clock::now();
-
-        executeBenchmark(runs,
-                         std::forward<Function>(fun),
-                         std::forward<Args>(args)...,
-                         std::is_void<ResultType>{});
-
-        return std::chrono::duration_cast<ClockResolution>(Clock::now() - start).count() /
-               static_cast<double>(runs * 1000);
+        return executeBenchmark(runs,
+                                std::forward<Function>(fun),
+                                std::forward<Args>(args)...,
+                                std::is_void<ResultType>{});
     }
 
     template <typename Function, typename... Args>
-    void executeBenchmark(std::uint64_t runs, Function&& fun, Args&&... args, std::true_type) const
+    std::vector<ClockResolution> executeBenchmark(std::uint64_t runs,
+                                                  Function&& fun,
+                                                  Args&&... args,
+                                                  std::true_type) const
     {
+        std::vector<ClockResolution> durationVector(runs);
         for (std::size_t i = 0; i < runs; ++i) {
+            const auto start = Clock::now();
             fun(std::forward<Args>(args)...);
+            const auto end = Clock::now();
+            durationVector[i] = std::chrono::duration_cast<ClockResolution>(end - start);
         }
+        return durationVector;
     }
 
     template <typename Function, typename... Args>
-    void executeBenchmark(std::uint64_t runs, Function&& fun, Args&&... args, std::false_type) const
+    std::vector<ClockResolution> executeBenchmark(std::uint64_t runs,
+                                                  Function&& fun,
+                                                  Args&&... args,
+                                                  std::false_type) const
     {
+        std::vector<ClockResolution> durationVector(runs);
         using ResultType = decltype(fun(args...));
         // In order to prevent compiler optimization, the result of the function call
         // is stored in a volatile variable.
         for (std::size_t i = 0; i < runs; ++i) {
+            const auto start = Clock::now();
             volatile ResultType result = fun(std::forward<Args>(args)...);
+            const auto end = Clock::now();
+            durationVector[i] = std::chrono::duration_cast<ClockResolution>(end - start);
         }
+        return durationVector;
+    }
+
+    static void printStatistics(std::vector<ClockResolution> durationVector,
+                                ClockResolution totalDuration)
+    {
+        using DoubleMilliSeconds = std::chrono::duration<double, std::milli>;
+        auto maxDelayDuration = std::chrono::duration_cast<DoubleMilliSeconds>(
+                *std::max_element(durationVector.cbegin(), durationVector.cend()));
+        auto minDelayDuration = std::chrono::duration_cast<DoubleMilliSeconds>(
+                *std::min_element(durationVector.cbegin(), durationVector.cend()));
+        auto sumDelayDuration = std::chrono::duration_cast<DoubleMilliSeconds>(std::accumulate(
+                durationVector.cbegin(), durationVector.cend(), ClockResolution(0)));
+        double meanDelay = sumDelayDuration.count() / durationVector.size();
+
+        using DoubleSeconds = std::chrono::duration<double>;
+        auto totalDurationSec = std::chrono::duration_cast<DoubleSeconds>(totalDuration);
+        double msgPerSec = durationVector.size() / totalDurationSec.count();
+
+        std::cerr << "----- statistics -----" << std::endl;
+        std::cerr << "totalDuration:\t" << totalDurationSec.count() << " [s]" << std::endl;
+        std::cerr << "maxDelay:\t\t" << maxDelayDuration.count() << " [ms]" << std::endl;
+        std::cerr << "minDelay:\t\t" << minDelayDuration.count() << " [ms]" << std::endl;
+        std::cerr << "meanDelay:\t\t" << meanDelay << " [ms]" << std::endl;
+        std::cerr << "msg/sec:\t\t" << msgPerSec << std::endl;
     }
 
     template <typename Function, typename... Args>
@@ -74,10 +112,12 @@ struct PerformanceTest
                             Function&& fun,
                             Args&&... args) const
     {
-        double averageDuration =
+        const auto startLoop = Clock::now();
+        std::vector<ClockResolution> durationVector =
                 benchmark(runs, std::forward<Function>(fun), std::forward<Args>(args)...);
-        std::cout << std::setprecision(3) << name << " => average time: " << averageDuration
-                  << " ms => " << (1000 / averageDuration) << " msg/sec" << std::endl;
+        const auto endLoop = Clock::now();
+        printStatistics(
+                durationVector, std::chrono::duration_cast<ClockResolution>(endLoop - startLoop));
     }
 };
 
