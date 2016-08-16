98df7592d9f541fe1ad7ca33e9729a1995544d4f
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2016-08-16 16:23:33 +0200

    [Build, C++] Support --default option for muesli build
    Change-Id: Ie998f09b3dc1c5f7d48dab9be1578cd39b23ccf7

--- docker/joynr-cpp-base/scripts/build/cpp-install-muesli.sh
+++ docker/joynr-cpp-base/scripts/build/cpp-install-muesli.sh
@@ -9,13 +9,17 @@ START=$(date +%s)
 
 function usage
 {
-    echo "usage: cpp-install-muesli.sh [--jobs X]"
+    echo "usage: cpp-install-muesli.sh [--default] [--jobs X]"
 }
+
+DEFAULT="OFF"
 
 while [ "$1" != "" ]; do
     case $1 in
         --jobs )                shift
                                 JOBS=$1
+                                ;;
+        --default )             DEFAULT=""
                                 ;;
         * )                     usage
                                 exit 1
@@ -31,6 +35,12 @@ rm -rf /data/build/rapidjson
 mkdir -p /data/build/rapidjson
 cd /data/build/rapidjson
 
+if [ "$DEFAULT" == "OFF" ]
+then
+    SET_DESTDIR="DESTDIR=/opt/rapidjson"
+else
+    SET_DESTDIR=""
+fi
 cmake -DRAPIDJSON_BUILD_DOC=OFF \
       -DRAPIDJSON_BUILD_EXAMPLES=OFF \
       -DRAPIDJSON_BUILD_TESTS=OFF \
@@ -38,21 +48,29 @@ cmake -DRAPIDJSON_BUILD_DOC=OFF \
       /data/src-rapidjson
 
 make -j $JOBS
-make install DESTDIR=/opt/rapidjson
+make install $SET_DESTDIR
 
 log "INSTALL MUESLI"
 rm -rf /data/build/muesli
 mkdir -p /data/build/muesli
 cd /data/build/muesli
 
+if [ "$DEFAULT" == "OFF" ]
+then
+    SET_CMAKE_PREFIX="-DCMAKE_PREFIX_PATH=/opt/rapidjson/usr/local/lib/cmake/RapidJSON/"
+    SET_DESTDIR="DESTDIR=/opt/muesli"
+else
+    SET_CMAKE_PREFIX=""
+    SET_DESTDIR=""
+fi
 cmake -DENABLE_CLANG_FORMATTER=OFF \
       -DUSE_PLATFORM_RAPIDJSON=ON \
       -DBUILD_MUESLI_TESTS=OFF \
-      -DCMAKE_PREFIX_PATH=/opt/rapidjson/usr/local/lib/cmake/RapidJSON/ \
+      $SET_CMAKE_PREFIX \
       /data/src-muesli
 
 make -j $JOBS
-make install DESTDIR=/opt/muesli
+make install $SET_DESTDIR
 
 END=$(date +%s)
 DIFF=$(( $END - $START ))
