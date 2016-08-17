f875ce909f368691f115152b162015f14e514498
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2016-08-17 13:05:26 +0200

    [Java, JEE] Added inter-language-test-jee-consumer
    Change-Id: I364a260be368aecfb15b90251e5bda98f9ae141d

---
+++ tests/inter-language-test-jee/inter-language-test-jee-consumer/src/main/java/io/joynr/test/interlanguage/jee/IltConsumerTest.java
@@ -0,0 +1,74 @@
+package io.joynr.test.interlanguage.jee;
+
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.joynr.runtime.JoynrRuntime;
+import joynr.interlanguagetest.TestInterfaceSync;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class IltConsumerTest {
+    private static final Logger LOG = LoggerFactory.getLogger(IltConsumerTest.class);
+
+    @Rule
+    public TestName name = new TestName();
+
+    private static String providerDomain;
+
+    // no real proxy, but keep the name anyway for now since it is used in test classes
+    protected static TestInterfaceSync testInterfaceProxy;
+    private static JoynrRuntime consumerRuntime;
+
+    protected static ObjectMapper objectMapper;
+
+    @BeforeClass
+    public static void generalSetUp() throws Exception {
+        LOG.info("generalSetUp: Entering");
+        LOG.info("generalSetUp: Leaving");
+    }
+
+    @Before
+    public void setUp() {
+        LOG.info("setUp: Entering");
+        if (testInterfaceProxy == null) {
+            LOG.info("setUp: testInterfaceProxy == null");
+            testInterfaceProxy = IltConsumerHelper.getServiceLocator().get(TestInterfaceSync.class,
+                                                                           "joynr-inter-language-test-domain");
+        }
+        LOG.info("setUp: Leaving");
+    }
+
+    @AfterClass
+    public static void generalTearDown() throws InterruptedException {
+        LOG.info("generalTearDown: Entering");
+        LOG.info("generalTearDown: Leaving");
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
---
+++ tests/inter-language-test-jee/inter-language-test-jee-consumer/src/main/java/io/joynr/test/interlanguage/jee/JoynrConfigurationProvider.java
@@ -0,0 +1,60 @@
+package io.joynr.test.interlanguage.jee;
+
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.Properties;
+
+import javax.ejb.Singleton;
+import javax.enterprise.inject.Produces;
+
+import io.joynr.jeeintegration.api.JoynrLocalDomain;
+import io.joynr.jeeintegration.api.JoynrProperties;
+import io.joynr.messaging.MessagingPropertyKeys;
+import io.joynr.messaging.mqtt.MqttModule;
+
+@Singleton
+public class JoynrConfigurationProvider {
+
+    @Produces
+    @JoynrProperties
+    public Properties joynrProperties() {
+        Properties joynrProperties = new Properties();
+        joynrProperties.setProperty(MessagingPropertyKeys.PROPERTY_SERVLET_CONTEXT_ROOT,
+                                    "/inter-language-test-jee-consumer/messaging");
+        joynrProperties.setProperty(MessagingPropertyKeys.PROPERTY_SERVLET_HOST_PATH, "http://localhost:8080");
+        joynrProperties.setProperty(MessagingPropertyKeys.CHANNELID, "io.joynr.test.interlanguage.jee.consumer");
+        joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_BROKER_URI, "tcp://localhost:1883");
+        joynrProperties.setProperty(MessagingPropertyKeys.BOUNCE_PROXY_URL, "http://localhost:8383/bounceproxy/");
+        joynrProperties.setProperty(MessagingPropertyKeys.DISCOVERYDIRECTORYURL,
+                                    "http://localhost:8383/discovery/channels/discoverydirectory_channelid/");
+        joynrProperties.setProperty(MessagingPropertyKeys.DOMAINACCESSCONTROLLERURL,
+                                    "http://localhost:8383/discovery/channels/discoverydirectory_channelid/");
+        joynrProperties.setProperty(MessagingPropertyKeys.PERSISTENCE_FILE, "ilt-jee-consumer-joynr.properties");
+
+        return joynrProperties;
+    }
+
+    @Produces
+    @JoynrLocalDomain
+    public String joynrLocalDomain() {
+        return "joynr-inter-language-test-domain";
+    }
+
+}
