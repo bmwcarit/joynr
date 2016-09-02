023e272eca0533e61736396688c19749ed3fb64a
Donato Sciarra <donato.sciarra@bmw-carit.de>
2016-09-02 09:05:07 +0200

    [ILT] Fix build script.
    Change-Id: I3324794a8977b573930ce7a882024d939656f772

--- docker/joynr-ilt-gcc/scripts/build/build_all.sh
+++ docker/joynr-ilt-gcc/scripts/build/build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./java-clean-build skipTests
-./javascript-clean-build skipTests
+./javascript-clean-build --skipTests
 ./cpp-install-muesli.sh --default
 ./cpp-generate.sh
 ./cpp-clean-build.sh --buildtests OFF --enableclangformatter OFF
