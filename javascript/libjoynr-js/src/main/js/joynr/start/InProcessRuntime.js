2fca6c828ed37230c337fe62fae640d670439c35
Christoph Ainhauser <Christoph.Ainhauser@bmw-carit.de>
2016-09-13 16:29:52 +0200

    [JS] Add MessageRouter.shutdown
    Change-Id: I4bf5a33f857757553856e3ee261dbd6ad525de66

--- javascript/libjoynr-js/src/main/js/joynr/start/InProcessRuntime.js
+++ javascript/libjoynr-js/src/main/js/joynr/start/InProcessRuntime.js
@@ -557,6 +557,10 @@ define(
                                 capabilitiesRegistrar.shutdown();
                             }
 
+                            if (messageRouter !== undefined) {
+                                messageRouter.shutdown();
+                            }
+
                             if (requestReplyManager !== undefined) {
                                 requestReplyManager.shutdown();
                             }
