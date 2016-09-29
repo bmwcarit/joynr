2f110c96ad05e4bed543ec32f42eaa1a9b8d6469
Manuel Schiller <manuel.schiller@bmw-carit.de>
2016-09-29 13:47:25 +0200

    [C++] cleaned up includes
    Change-Id: Ifcc8e046078056d0b64ef38f7fb8cf5bca4bec61

--- cpp/cluster-controller/http-communication-manager/LongPollingMessageReceiver.h
+++ cpp/cluster-controller/http-communication-manager/LongPollingMessageReceiver.h
@@ -24,12 +24,9 @@
 #include <mutex>
 #include <string>
 
-#include "joynr/PrivateCopyAssign.h"
-
-#include "joynr/ContentWithDecayTime.h"
 #include "joynr/BrokerUrl.h"
 #include "joynr/Logger.h"
-#include "joynr/Directory.h"
+#include "joynr/PrivateCopyAssign.h"
 #include "joynr/Semaphore.h"
 #include "joynr/Thread.h"
 
