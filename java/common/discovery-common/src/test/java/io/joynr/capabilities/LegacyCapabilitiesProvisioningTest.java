ac25de2e10332d6d89607de93712ddba20ce8ed6
Clive Jevons <clive@jevons-it.net>
2016-10-26 19:29:25 +0200

    [Java] bugfix for legacy MQTT addresses
    We were adding the /low/<participant ID> onto the topic
    for the MQTT address, even though these were later added
    by the MQTT messaging layer, and hence appeared twice in
    the 'to' header field.
    
    Change-Id: I7215d61878d923bd8fbace6af44a604d977b4000

---
+++ java/common/discovery-common/src/test/java/io/joynr/capabilities/LegacyCapabilitiesProvisioningTest.java
@@ -0,0 +1,65 @@
+package io.joynr.capabilities;
+
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Field;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import joynr.infrastructure.GlobalCapabilitiesDirectory;
+import joynr.system.RoutingTypes.Address;
+import joynr.system.RoutingTypes.MqttAddress;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LegacyCapabilitiesProvisioningTest {
+
+    @Before
+    public void setup() throws Exception {
+        Field objectMapperField = CapabilityUtils.class.getDeclaredField("objectMapper");
+        objectMapperField.setAccessible(true);
+        objectMapperField.set(CapabilityUtils.class, new ObjectMapper());
+    }
+
+    @Test
+    public void testMqttAddressGeneratedCorrectly() {
+        LegacyCapabilitiesProvisioning.LegacyProvisioningPropertiesHolder propertiesHolder = new LegacyCapabilitiesProvisioning.LegacyProvisioningPropertiesHolder("tcp://localhost:1883",
+                                                                                                                                                                   null,
+                                                                                                                                                                   "tcp://localhost:1883",
+                                                                                                                                                                   "channel_id",
+                                                                                                                                                                   "discovery_domain",
+                                                                                                                                                                   "capabilities_domain",
+                                                                                                                                                                   "capdir_channel_id",
+                                                                                                                                                                   "domain_access_ctrl_participant_id",
+                                                                                                                                                                   "domain_access_ctrl_channel_id");
+        LegacyCapabilitiesProvisioning subject = new LegacyCapabilitiesProvisioning(propertiesHolder);
+        Address address = subject.getAddressForInterface(GlobalCapabilitiesDirectory.class);
+        assertTrue(address instanceof MqttAddress);
+        MqttAddress globalCapabilitiesAddress = (MqttAddress) address;
+        assertEquals("tcp://localhost:1883", globalCapabilitiesAddress.getBrokerUri());
+        assertEquals("capdir_channel_id", globalCapabilitiesAddress.getTopic());
+    }
+
+}
