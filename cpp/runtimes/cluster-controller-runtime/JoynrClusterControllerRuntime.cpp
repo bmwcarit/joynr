97d1866a382f7db17dff173834089037233bf2a5
Donato Sciarra <donato.sciarra@bmw-carit.de>
2016-11-28 16:22:35 +0100

    [C++] Allow usage of TCP protocol for brokerUrl in setting file
    Change-Id: Ibc50fa30b7cf15f75f7273395b00d9968d7c778f

--- cpp/runtimes/cluster-controller-runtime/JoynrClusterControllerRuntime.cpp
+++ cpp/runtimes/cluster-controller-runtime/JoynrClusterControllerRuntime.cpp
@@ -171,7 +171,9 @@ void JoynrClusterControllerRuntime::initializeAllDependencies()
     std::unique_ptr<IMulticastAddressCalculator> addressCalculator;
 
     if (brokerProtocol == joynr::system::RoutingTypes::MqttProtocol::getLiteral(
-                                  joynr::system::RoutingTypes::MqttProtocol::Enum::MQTT)) {
+                                  joynr::system::RoutingTypes::MqttProtocol::Enum::MQTT) ||
+        brokerProtocol == joynr::system::RoutingTypes::MqttProtocol::getLiteral(
+                                  joynr::system::RoutingTypes::MqttProtocol::Enum::TCP)) {
         JOYNR_LOG_INFO(logger, "MQTT-Messaging");
         auto globalAddress = std::make_shared<const joynr::system::RoutingTypes::MqttAddress>(
                 brokerUrl.toString(), "");
