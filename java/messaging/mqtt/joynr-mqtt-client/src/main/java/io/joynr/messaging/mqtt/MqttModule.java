33376e5b87cc75b877f4c1ceedbbc39609f17a42
Thomas Reuter <thomas.reuter@de.bertrandt.com>
2017-02-13 17:38:25 +0100

    [Java] add property key for optional property mqtt clientId prefix
    Change-Id: Iadca8886ee4678e45f53037ba836371750727ad8

--- java/messaging/mqtt/joynr-mqtt-client/src/main/java/io/joynr/messaging/mqtt/MqttModule.java
+++ java/messaging/mqtt/joynr-mqtt-client/src/main/java/io/joynr/messaging/mqtt/MqttModule.java
@@ -45,6 +45,7 @@ public class MqttModule extends AbstractModule {
     // property key
     public static final String PROPERTY_KEY_MQTT_RECONNECT_SLEEP_MS = "joynr.messaging.mqtt.reconnect.sleepms";
     public static final String PROPERTY_KEY_MQTT_BROKER_URI = "joynr.messaging.mqtt.brokeruri";
+    public static final String PROPERTY_KEY_MQTT_CLIENT_ID_PREFIX = "joynr.messaging.mqtt.clientidprefix";
     public static final String PROPERTY_MQTT_ADDRESS = "property_mqtt_address";
 
     @Provides
