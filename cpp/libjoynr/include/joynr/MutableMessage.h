b97471da7433b1ec73339b4649ac36f768408eeb
Manuel Schiller <manuel.schiller@bmw-carit.de>
2017-05-11 07:29:30 +0200

    [C++] added documentation to MutableMessage
    Change-Id: I43f45d27a542de709d495ecdc80b39d4c43abeb0

--- cpp/libjoynr/include/joynr/MutableMessage.h
+++ cpp/libjoynr/include/joynr/MutableMessage.h
@@ -48,10 +48,18 @@ public:
     MutableMessage(MutableMessage&&) = default;
     MutableMessage& operator=(MutableMessage&&) = default;
 
+    /**
+     * @brief This serializes the contents of this message, then it is optionally signed and
+     * encrypted.
+     * @return an ImmutableMessage representing a frozen state of this message's content
+     */
     std::unique_ptr<ImmutableMessage> getImmutableMessage() const;
 
+    /**
+     * @brief Sets the sender of this message.
+     * @param sender the participant ID of this message's sender
+     */
     void setSender(const std::string& sender);
-
     void setSender(std::string&& sender);
 
     /**
@@ -60,8 +68,11 @@ public:
      */
     const std::string& getSender() const;
 
+    /**
+     * @brief Sets the recipient of this message.
+     * @param recipient the participant ID of this message's recipient
+     */
     void setRecipient(const std::string& recipient);
-
     void setRecipient(std::string&& recipient);
 
     /**
@@ -70,44 +81,116 @@ public:
      */
     const std::string& getRecipient() const;
 
+    /**
+     * @brief Sets the expiry date of the message. If the header is already set,
+     * its value is replaced with the new one.
+     * @param expiryDate the "expiry date" header to be set on the message.
+     */
     void setExpiryDate(JoynrTimePoint expiryDate);
 
+    /**
+     * @brief Gets the expiry date of the message.
+     * @return the "expiry date" header of the message, if the header is set;
+     * A default-constructed value, otherwise.
+     */
     JoynrTimePoint getExpiryDate() const;
 
-    const std::unordered_map<std::string, std::string>& getCustomHeaders() const;
-
-    boost::optional<std::string> getCustomHeader(const std::string& key) const;
-
+    /**
+     * @brief Sets the type of the message. If the type is
+     * already set, its value is replaced with the new one.
+     * @param type the type of this message
+     */
     void setType(const std::string& type);
-
     void setType(std::string&& type);
 
+    /**
+     * @brief Gets the type of this message
+     * @return string containing the message type;
+     * default-constructed string if type has not been set
+     */
     const std::string& getType() const;
 
+    /**
+     * @brief Gets the id of this message
+     * @return string containing the message id
+     */
     const std::string& getId() const;
 
+    /**
+     * @brief Sets the reply address of the message. If the header is
+     * already set, its value is replaced with the new one.
+     * @param replyTo the "reply address" header to be set on the message.
+     * @see Message::HEADER_REPLY_TO()
+     */
     void setReplyTo(const std::string& replyTo);
-
     void setReplyTo(std::string&& replyTo);
 
+    /**
+     * @brief Gets the reply address of the message.
+     * @return an optional containing the "reply address" header of the message; this optional
+     * is not initialized if the header has not been set
+     * @see Message::HEADER_REPLY_TO()
+     */
     const boost::optional<std::string>& getReplyTo() const;
 
+    /**
+     * @brief Sets a custom header for this message.
+     * @param key key of the custom header
+     * @param value value of the custom header
+     */
     void setCustomHeader(const std::string& key, const std::string& value);
-
     void setCustomHeader(std::string&& key, std::string&& value);
 
-    void setEffort(const std::string& effort);
+    /**
+     * @brief Gets all custom headers that have been set for this message
+     * @return a map containing all custom headers of this message
+     */
+    const std::unordered_map<std::string, std::string>& getCustomHeaders() const;
 
+    /**
+     * @brief Gets the value of a custom header.
+     * @param key key of the custom header that shall be retrieved
+     * @return an optional containing the value for the specified key; this optional is not
+     * initialized if a header for this key has not been set
+     */
+    boost::optional<std::string> getCustomHeader(const std::string& key) const;
+
+    /**
+     * @brief Sets the effort to be expent on delivering the message.
+     * If the header is already set, its value is replaced with the new one.
+     * @param effort the "effort" header to be set on the message.
+     * @see Message::HEADER_EFFORT()
+     */
+    void setEffort(const std::string& effort);
     void setEffort(std::string&& effort);
 
+    /**
+     * @brief Gets the effort which should be expent delivering the message.
+     * @return an optional containing the "effort" header of the message; this optional is not
+     * initialized if the header has not been set
+     * @see Message::HEADER_EFFORT()
+     */
     const boost::optional<std::string>& getEffort() const;
 
+    /**
+     * @brief Sets the payload of this message.
+     * If the payload is already set, its value is replaced with the new one.
+     * @param payload payload of this message.
+     */
     void setPayload(const std::string& payload);
-
     void setPayload(std::string&& payload);
 
+    /**
+     * @brief Gets the payload of this message
+     * @return string containing the payload; default-constructed string if payload has not been
+     * set
+     */
     const std::string& getPayload() const;
 
+    /**
+     * @brief  Used to print log messages.
+     * @return a string representing this message used for logging.
+     */
     std::string toLogMessage() const;
 
     /**
