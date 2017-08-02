b14e4de7f5dfcd8d20f1e790a44925b109f4c9f0
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2017-08-02 06:03:15 +0000

    [JS] Performance Tests: Added option to disable different Testtypes e.g. echoStruct
    Change-Id: I9e89520c8a7545e084c8c986f585deee4c66cb20

--- tests/performance-test/src/main/js/consumer.js
+++ tests/performance-test/src/main/js/consumer.js
@@ -22,20 +22,27 @@
 var Promise = require("bluebird").Promise;
 var consumerBase = require("./consumer.base.js");
 
+var whichTests = process.env.testsTypes || "csbk";
+// string explanation: c = complesStruct; s = string; b = byteArray; k = byteArrayWithSizeTimesk;
+
+var runOptional = function (letter, func) {
+    return function(){ return whichTests.indexOf(letter) !== -1 ? func() : Promise.resolve();};
+};
+
 //disable log
 console.log = function() {};
 
-consumerBase.initialize().then(function() {
-    return consumerBase.echoComplexStruct()
-        .then(consumerBase.echoString)
-        .then(consumerBase.echoByteArray)
-        .then(consumerBase.echoByteArrayWithSizeTimesK)
-        .then(consumerBase.shutdown)
-        .then(function() {
-            console.log("SUCCEEDED");
-            process.exit(0);
-        });
-}).catch(function(error) {
-    console.log("Error while performing test: " + error);
-    throw error;
-});
+consumerBase.initialize()
+    .then(runOptional("c", consumerBase.echoComplexStruct))
+    .then(runOptional("s", consumerBase.echoString))
+    .then(runOptional("b", consumerBase.echoByteArray))
+    .then(runOptional("k", consumerBase.echoByteArrayWithSizeTimesK))
+    .then(consumerBase.shutdown)
+    .then(function() {
+        console.log("SUCCEEDED");
+        process.exit(0);
+    })
+    .catch(function(error) {
+        console.log("Error while performing test: " + error);
+        throw error;
+    });
