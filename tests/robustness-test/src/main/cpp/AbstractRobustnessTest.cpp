a48daf5b348ef76b68ca657b50d80002b638d51f
Atanas Gegov <atanas.gegov@bmw-carit.de>
2017-08-04 15:20:21 +0200

    [C++, Tests] Change DLT application ids for robustness test
    Consumer (test) and provider inside robustness-test were both using
    'JOYT'. This makes differentiation of log messages impossible. Now
    chosen:
    * JYRC - joynr robustness consumer (test)
    * JYRP - joynr robustness provider
    
    Change-Id: Ie5ade2f28bc5e781081789056cc3718e0811a4a1

--- tests/robustness-test/src/main/cpp/AbstractRobustnessTest.cpp
+++ tests/robustness-test/src/main/cpp/AbstractRobustnessTest.cpp
@@ -32,7 +32,7 @@ int main(int argc, char** argv)
 {
 #ifdef JOYNR_ENABLE_DLT_LOGGING
     // Register app at the dlt-daemon for logging
-    DLT_REGISTER_APP("JOYT", argv[0]);
+    DLT_REGISTER_APP("JYRC", argv[0]);
 #endif // JOYNR_ENABLE_DLT_LOGGING
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
