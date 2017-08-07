84503928876d1f49150e8516524c6369b08e77da
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2017-08-07 04:36:41 +0000

    [JS] Prefer static functions: SharedMqttClient
    Change-Id: I349c99844e329e0126e9fe3623a073a8833be139

--- javascript/libjoynr-js/src/main/js/joynr/messaging/mqtt/SharedMqttClient.js
+++ javascript/libjoynr-js/src/main/js/joynr/messaging/mqtt/SharedMqttClient.js
@@ -65,7 +65,8 @@ define(
             }
 
             function sendQueuedSubscriptions(client, queuedSubscriptions, qosLevel) {
-                return new Promise(function(resolve, reject) {
+
+                function sendQueuedSubscriptionsResolver(resolve, reject) {
                     var i, topic, subscribeObject = {};
                     for (i = 0; i < queuedSubscriptions.length; i++) {
                         topic = queuedSubscriptions[i];
@@ -76,28 +77,33 @@ define(
                         queuedSubscriptions = [];
                         resolve();
                     });
-                });
+                }
+
+                return new Promise(sendQueuedSubscriptionsResolver);
             }
 
             function sendMessage(client, topic, joynrMessage, sendQosLevel, queuedMessages) {
-                return new Promise(function(resolve, reject){
+
+                function sendMessageResolver(resolve, reject){
                     try {
-                        client.publish(topic, JSONSerializer.stringify(joynrMessage), { qos : sendQosLevel });
+                        client.publish(topic, JSONSerializer.stringify(joynrMessage), { qos: sendQosLevel });
                         resolve();
                         // Error is thrown if the socket is no longer open, so requeue to the front
                     } catch (e) {
                         // add the message back to the front of the queue
                         queuedMessages.unshift({
-                            message : joynrMessage,
-                            resolve : resolve,
-                            options : {
-                                qos : sendQosLevel,
+                            message: joynrMessage,
+                            resolve: resolve,
+                            options: {
+                                qos: sendQosLevel
                             },
-                            topic : topic
+                            topic: topic
                         });
                         throw e;
                     }
-                });
+                }
+
+                return new Promise(sendMessageResolver);
             }
 
             /**
@@ -109,132 +115,127 @@ define(
              *            settings.address to be used to connect to mqtt broker
              */
             var SharedMqttClient =
-                    function SharedMqttClient(settings) {
-                        Typing.checkProperty(settings, "Object", "settings");
-                        Typing.checkProperty(
-                                settings.address,
-                                "MqttAddress",
-                                "settings.address");
+                function SharedMqttClient(settings) {
+                    Typing.checkProperty(settings, "Object", "settings");
+                    Typing.checkProperty( settings.address, "MqttAddress", "settings.address");
 
-                        var client = null;
-                        var address = settings.address;
-                        var onmessageCallback = null;
-                        var queuedMessages = [];
-                        var onOpen;
-                        var resolve;
-                        var closed = false;
-                        var connected = false;
-                        var onConnectedPromise = new Promise(function(resolveTrigger) {
-                            resolve = resolveTrigger;
-                        });
-                        var qosLevel = SharedMqttClient.DEFAULT_QOS_LEVEL;
+                    var client = null;
+                    var address = settings.address;
+                    var onmessageCallback = null;
+                    var queuedMessages = [];
+                    var onOpen;
+                    var resolve;
+                    var closed = false;
+                    var connected = false;
+                    var onConnectedPromise = new Promise(function(resolveTrigger) { // TODO: refactor this
+                        resolve = resolveTrigger;
+                    });
+                    var qosLevel = SharedMqttClient.DEFAULT_QOS_LEVEL;
 
-                        if (settings.provisioning.qosLevel !== undefined) {
-                            qosLevel = settings.provisioning.qosLevel;
+                    if (settings.provisioning.qosLevel !== undefined) {
+                        qosLevel = settings.provisioning.qosLevel;
+                    }
+
+                    var queuedSubscriptions = [];
+                    var queuedUnsubscriptions = [];
+
+                    var onMessage = function(topic, payload) {
+                        if (onmessageCallback !== undefined) {
+                            onmessageCallback(topic, new JoynrMessage(JSON.parse(payload.toString())));
+                        }
+                    };
+
+                    var resetConnection = function resetConnection() {
+                        if (closed) {
+                            return;
+                        }
+                        client = new Mqtt.connect(address.brokerUri);
+                        client.on("connect", onOpen);
+                        client.on("message", onMessage);
+                    };
+
+                    this.onConnected = function() {
+                        return onConnectedPromise;
+                    };
+
+                    // send all queued messages, requeuing to the front in case of a problem
+                    onOpen = function onOpen() {
+                        try {
+                            connected = true;
+                            sendQueuedMessages(client, queuedMessages);
+                            sendQueuedUnsubscriptions(client, queuedUnsubscriptions);
+                            sendQueuedSubscriptions(client, queuedSubscriptions, qosLevel).then(resolve); // TODO uncatched promise
+                        } catch (e) {
+                            resetConnection();
+                        }
+                    };
+
+                    resetConnection();
+
+                    /**
+                     * @name SharedMqttClient#send
+                     * @function
+                     * @param {String}
+                     *            topic the topic to publish the message
+                     * @param {JoynrMessage}
+                     *            joynrMessage the joynr message to transmit
+                     */
+                    this.send = function send(topic, joynrMessage) {
+                        var sendQosLevel = qosLevel;
+                        if (MessagingQosEffort.BEST_EFFORT === joynrMessage.effort) {
+                            sendQosLevel = SharedMqttClient.BEST_EFFORT_QOS_LEVEL;
                         }
 
-                        var queuedSubscriptions = [];
-                        var queuedUnsubscriptions = [];
-
-                        var onMessage = function(topic, payload) {
-                            if (onmessageCallback !== undefined) {
-                                onmessageCallback(topic, new JoynrMessage(JSON.parse(payload.toString())));
-                            }
-                        };
-
-                        var resetConnection = function resetConnection() {
-                            if (closed) {
-                                return;
-                            }
-                            client = new Mqtt.connect(address.brokerUri);
-                            client.on('connect', onOpen);
-                            client.on('message', onMessage);
-                        };
-
-                        this.onConnected = function() {
-                            return onConnectedPromise;
-                        };
-
-                        // send all queued messages, requeuing to the front in case of a problem
-                        onOpen = function onOpen() {
-                            try {
-                                connected = true;
-                                sendQueuedMessages(client, queuedMessages);
-                                sendQueuedUnsubscriptions(client, queuedUnsubscriptions);
-                                sendQueuedSubscriptions(client, queuedSubscriptions, qosLevel).then(resolve);
-                            } catch (e) {
-                                resetConnection();
-                            }
-                        };
-
-                        resetConnection();
-
-                        /**
-                         * @name SharedMqttClient#send
-                         * @function
-                         * @param {String}
-                         *            topic the topic to publish the message
-                         * @param {JoynrMessage}
-                         *            joynrMessage the joynr message to transmit
-                         */
-                        this.send = function send(topic, joynrMessage) {
-                            var sendQosLevel = qosLevel;
-                            if (MessagingQosEffort.BEST_EFFORT === joynrMessage.effort) {
-                                sendQosLevel = SharedMqttClient.BEST_EFFORT_QOS_LEVEL;
-                            }
-
-                            return sendMessage(client, topic, joynrMessage, sendQosLevel, queuedMessages).catch(function(e1) {
-                                    resetConnection();
-                                });
-                        };
-
-                        /**
-                         * @name SharedMqttClient#close
-                         * @function
-                         */
-                        this.close = function close() {
-                            closed = true;
-                            if (client !== null && client.end) {
-                                client.end();
-                            }
-                        };
-
-                        this.subscribe = function(topic) {
-                            if (connected) {
-                                client.subscribe(topic, { qos : qosLevel} );
-                            } else {
-                                queuedSubscriptions.push(topic);
-                            }
-                        };
-
-                        this.unsubscribe = function(topic) {
-                            if (connected) {
-                                client.unsubscribe(topic);
-                            } else {
-                                queuedUnsubscriptions.push(topic);
-                            }
-                        };
-
-                        // using the defineProperty syntax for onmessage to be able to keep
-                        // the same API as WebSocket but have a setter function called when
-                        // the attribute is set.
-                        Object.defineProperty(this, "onmessage", {
-                            set : function(newCallback) {
-                                onmessageCallback = newCallback;
-                                if (typeof newCallback !== "function") {
-                                    throw new Error(
-                                            "onmessage callback must be a function, but instead was of type "
-                                                + typeof newCallback);
-                                }
-                            },
-                            get : function() {
-                                return onmessageCallback;
-                            },
-                            enumerable : false,
-                            configurable : false
-                        });
-
+                        return sendMessage(client, topic, joynrMessage, sendQosLevel, queuedMessages).catch(resetConnection);
                     };
+
+                    /**
+                     * @name SharedMqttClient#close
+                     * @function
+                     */
+                    this.close = function close() {
+                        closed = true;
+                        if (client !== null && client.end) {
+                            client.end();
+                        }
+                    };
+
+                    this.subscribe = function(topic) {
+                        if (connected) {
+                            client.subscribe(topic, { qos: qosLevel } );
+                        } else {
+                            queuedSubscriptions.push(topic);
+                        }
+                    };
+
+                    this.unsubscribe = function(topic) {
+                        if (connected) {
+                            client.unsubscribe(topic);
+                        } else {
+                            queuedUnsubscriptions.push(topic);
+                        }
+                    };
+
+                    // using the defineProperty syntax for onmessage to be able to keep
+                    // the same API as WebSocket but have a setter function called when
+                    // the attribute is set.
+                    Object.defineProperty(this, "onmessage", {
+                        set: function(newCallback) {
+                            onmessageCallback = newCallback;
+                            if (typeof newCallback !== "function") {
+                                throw new Error(
+                                        "onmessage callback must be a function, but instead was of type "
+                                            + typeof newCallback);
+                            }
+                        },
+                        get: function() {
+                            return onmessageCallback;
+                        },
+                        enumerable  : false,
+                        configurable: false
+                    });
+
+                };
 
             SharedMqttClient.DEFAULT_QOS_LEVEL = 1;
             SharedMqttClient.BEST_EFFORT_QOS_LEVEL = 0;
