b39ed941ea06a7c9af51fc916d0a59b7c8d9010e
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2017-08-11 14:07:08 +0200

    [C++] Fixed SystemServicesRoutingTest
    * reset runtime shared_ptr before deleting persistence files
    
    Change-Id: I57f3d9d0c05c9bafe4d4b344ab8c8616e04bef22

--- cpp/tests/integration-tests/SystemServicesRoutingTest.cpp
+++ cpp/tests/integration-tests/SystemServicesRoutingTest.cpp
@@ -90,21 +90,21 @@ public:
     ~SystemServicesRoutingTest(){
         runtime->deleteChannel();
         runtime->stopExternalCommunication();
+        runtime.reset();
+
         std::remove(settingsFilename.c_str());
+
+        // Delete persisted files
+        std::remove(ClusterControllerSettings::DEFAULT_LOCAL_CAPABILITIES_DIRECTORY_PERSISTENCE_FILENAME().c_str());
+        std::remove(LibjoynrSettings::DEFAULT_MESSAGE_ROUTER_PERSISTENCE_FILENAME().c_str());
+        std::remove(LibjoynrSettings::DEFAULT_SUBSCRIPTIONREQUEST_PERSISTENCE_FILENAME().c_str());
+        std::remove(LibjoynrSettings::DEFAULT_PARTICIPANT_IDS_PERSISTENCE_FILENAME().c_str());
     }
 
     void SetUp(){
         participantId = util::createUuid();
         isGloballyVisible = true;
         routingProxyBuilder = runtime->createProxyBuilder<joynr::system::RoutingProxy>(routingDomain);
-    }
-
-    void TearDown(){
-        // Delete persisted files
-        std::remove(ClusterControllerSettings::DEFAULT_LOCAL_CAPABILITIES_DIRECTORY_PERSISTENCE_FILENAME().c_str());
-        std::remove(LibjoynrSettings::DEFAULT_MESSAGE_ROUTER_PERSISTENCE_FILENAME().c_str());
-        std::remove(LibjoynrSettings::DEFAULT_SUBSCRIPTIONREQUEST_PERSISTENCE_FILENAME().c_str());
-        std::remove(LibjoynrSettings::DEFAULT_PARTICIPANT_IDS_PERSISTENCE_FILENAME().c_str());
     }
 
 protected:
