ba0bfc5657d76a2465ef4a589aadef7eae937e09
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2017-08-23 04:44:10 +0000

    [JS] performance: Use prototype: security
    Change-Id: I6aec5fc8cabbb81ea82957ae621707ebcfca1180

--- javascript/libjoynr-js/src/main/js/joynr/security/PlatformSecurityManagerNode.js
+++ javascript/libjoynr-js/src/main/js/joynr/security/PlatformSecurityManagerNode.js
@@ -31,21 +31,23 @@ define("joynr/security/PlatformSecurityManager", [], function() {
      *            [JoynrMessage.JOYNRMESSAGE_TYPE_*]{@link JoynrMessage}
      */
     function PlatformSecurityManager() {
-        /**
-         * @name PlatformSecurityManager(2)#getCurrentProcessUserId
-         * @function
-         *
-         * @returns {String} the user ID that executes node
-         */
-        this.getCurrentProcessUserId = function getCurrentProcessUserId() {
-            // Remark: Faking the user name by setting this environment variable
-            // is not a security threat, since the process can only access the
-            // private key of the real user. This key is used to sign the message.
-            // On the receiving side joynr will try to verify the message signature
-            // with the public key of the faked user which will fail.
-            return process.env.USER;
-        };
+
     }
+
+    /**
+     * @name PlatformSecurityManager(2)#getCurrentProcessUserId
+     * @function
+     *
+     * @returns {String} the user ID that executes node
+     */
+    PlatformSecurityManager.prototype.getCurrentProcessUserId = function getCurrentProcessUserId() {
+        // Remark: Faking the user name by setting this environment variable
+        // is not a security threat, since the process can only access the
+        // private key of the real user. This key is used to sign the message.
+        // On the receiving side joynr will try to verify the message signature
+        // with the public key of the faked user which will fail.
+        return process.env.USER;
+    };
 
     return PlatformSecurityManager;
 
