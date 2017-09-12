c68acf0f3d2a518143326268b3f3e56db10618c9
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2017-09-12 08:30:23 +0200

    [Java] Fixed headers in java/common/infrastructure-common
    Change-Id: I51531cc3d24a657adb07b01e30c0dd0676871956

--- java/common/infrastructure-common/src/main/java/io/joynr/accesscontrol/DomainAccessControlStoreEhCache.java
+++ java/common/infrastructure-common/src/main/java/io/joynr/accesscontrol/DomainAccessControlStoreEhCache.java
@@ -1,16 +1,3 @@
-package io.joynr.accesscontrol;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import javax.annotation.Nonnull;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.inject.Inject;
-
 /*
  * #%L
  * %%
@@ -19,9 +6,9 @@ import com.google.inject.Inject;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +16,15 @@ import com.google.inject.Inject;
  * limitations under the License.
  * #L%
  */
+package io.joynr.accesscontrol;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.inject.Inject;
 
 import io.joynr.accesscontrol.primarykey.UserDomainInterfaceOperationKey;
 import io.joynr.accesscontrol.primarykey.UserRoleKey;
