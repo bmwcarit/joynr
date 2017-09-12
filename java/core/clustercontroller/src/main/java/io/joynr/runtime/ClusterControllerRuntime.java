e8b52b8b14205b993ae2f7f51b58fee9952bd065
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2017-09-12 08:30:23 +0200

    [Java] Fixed headers in java/core/clustercontroller
    Change-Id: Ia46298402427e64358ca7ff20896c5be6429dae5

--- java/core/clustercontroller/src/main/java/io/joynr/runtime/ClusterControllerRuntime.java
+++ java/core/clustercontroller/src/main/java/io/joynr/runtime/ClusterControllerRuntime.java
@@ -1,18 +1,3 @@
-package io.joynr.runtime;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.inject.Inject;
-import com.google.inject.name.Named;
-
-import io.joynr.capabilities.CapabilitiesRegistrar;
-import io.joynr.capabilities.LocalCapabilitiesDirectory;
-import io.joynr.discovery.LocalDiscoveryAggregator;
-import io.joynr.dispatching.Dispatcher;
-import io.joynr.messaging.MessagingSkeletonFactory;
-
 /*
  * #%L
  * %%
@@ -21,9 +6,9 @@ import io.joynr.messaging.MessagingSkeletonFactory;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,6 +16,18 @@ import io.joynr.messaging.MessagingSkeletonFactory;
  * limitations under the License.
  * #L%
  */
+package io.joynr.runtime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import io.joynr.capabilities.CapabilitiesRegistrar;
+import io.joynr.capabilities.LocalCapabilitiesDirectory;
+import io.joynr.discovery.LocalDiscoveryAggregator;
+import io.joynr.dispatching.Dispatcher;
+import io.joynr.messaging.MessagingSkeletonFactory;
 
 import io.joynr.messaging.routing.RoutingTable;
 import io.joynr.proxy.ProxyBuilderFactory;
