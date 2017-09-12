365d07294a1d317a35d0de16c1ef46708dec8533
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2017-09-12 08:30:24 +0200

    [Java] Fixed headers in java/javaapi
    Change-Id: I2046dfa7e2801eb57a7bbfc17ad2ef88a3bc5e41

--- java/javaapi/src/main/java/joynr/BroadcastFilterParameters.java
+++ java/javaapi/src/main/java/joynr/BroadcastFilterParameters.java
@@ -1,12 +1,3 @@
-package joynr;
-
-import io.joynr.subtypes.JoynrType;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.annotation.Nonnull;
-
 /*
  * #%L
  * %%
@@ -15,9 +6,9 @@ import javax.annotation.Nonnull;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +16,13 @@ import javax.annotation.Nonnull;
  * limitations under the License.
  * #L%
  */
+package joynr;
+
+import io.joynr.subtypes.JoynrType;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
+
 public class BroadcastFilterParameters implements JoynrType {
     private static final long serialVersionUID = 1L;
     Map<String, String> filterParameters = new HashMap<String, String>();
