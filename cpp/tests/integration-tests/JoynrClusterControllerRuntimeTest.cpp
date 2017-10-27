67287e215595ee72f2b3113e6677949499aa0478
Manuel Schiller <manuel.schiller@bmw-carit.de>
2017-10-27 07:01:02 +0200

    [C++] fix shutdown of JoynrClusterControllerRuntimeTest
    * wait for runtime to be destroyed
    * delete all persistency files
    
    Change-Id: I77bebb25e457e4098d4d0da54e736bd222b7b292

--- cpp/tests/integration-tests/JoynrClusterControllerRuntimeTest.cpp
+++ cpp/tests/integration-tests/JoynrClusterControllerRuntimeTest.cpp
@@ -44,6 +44,7 @@
 #include "tests/mock/MockTransportMessageReceiver.h"
 #include "tests/mock/MockSubscriptionListener.h"
 #include "tests/mock/MockTestProvider.h"
+#include "tests/utils/PtrUtils.h"
 
 using namespace ::testing;
 using namespace joynr;
@@ -103,20 +104,14 @@ public:
         serializedMqttAddress = joynr::serializer::serializeToJson(MqttAddress(mqttBrokerUrl, mqttTopic));
     }
 
-    ~JoynrClusterControllerRuntimeTest(){
+    ~JoynrClusterControllerRuntimeTest()
+    {
         if (runtime) {
             runtime->deleteChannel();
             runtime->stopExternalCommunication();
-            runtime.reset();
-
-            // Delete persisted files
-            std::remove(ClusterControllerSettings::DEFAULT_LOCAL_CAPABILITIES_DIRECTORY_PERSISTENCE_FILENAME().c_str());
-            std::remove(ClusterControllerSettings::DEFAULT_LOCAL_DOMAIN_ACCESS_STORE_PERSISTENCE_FILENAME().c_str());
-            std::remove(ClusterControllerSettings::DEFAULT_MULTICAST_RECEIVER_DIRECTORY_PERSISTENCE_FILENAME().c_str());
-            std::remove(LibjoynrSettings::DEFAULT_MESSAGE_ROUTER_PERSISTENCE_FILENAME().c_str());
-            std::remove(LibjoynrSettings::DEFAULT_SUBSCRIPTIONREQUEST_PERSISTENCE_FILENAME().c_str());
-            std::remove(LibjoynrSettings::DEFAULT_PARTICIPANT_IDS_PERSISTENCE_FILENAME().c_str());
+            test::util::resetAndWaitUntilDestroyed(runtime);
         }
+        test::util::removeAllCreatedSettingsAndPersistencyFiles();
         EXPECT_TRUE(Mock::VerifyAndClearExpectations(mockHttpMessageReceiver.get()));
         EXPECT_TRUE(Mock::VerifyAndClearExpectations(mockMqttMessageReceiver.get()));
     }
