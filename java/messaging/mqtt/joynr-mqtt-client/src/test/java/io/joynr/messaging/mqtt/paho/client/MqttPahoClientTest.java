1e090f1b5eace47389cbcd25bb002395715139d8
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2017-11-17 10:11:52 +0000

    [Java] Test for Mqtt clean session configurable
    Change-Id: I546e1f22afe2fc08a308ab77febe7b4580418298

--- java/messaging/mqtt/joynr-mqtt-client/src/test/java/io/joynr/messaging/mqtt/paho/client/MqttPahoClientTest.java
+++ java/messaging/mqtt/joynr-mqtt-client/src/test/java/io/joynr/messaging/mqtt/paho/client/MqttPahoClientTest.java
@@ -22,6 +22,9 @@ import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -35,10 +38,12 @@ import org.mockito.MockitoAnnotations;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.Multibinder;
+import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 
 import io.joynr.common.JoynrPropertiesModule;
@@ -50,6 +55,7 @@ import io.joynr.messaging.MessagingPropertyKeys;
 import io.joynr.messaging.NoOpRawMessagingPreprocessor;
 import io.joynr.messaging.RawMessagingPreprocessor;
 import io.joynr.messaging.mqtt.MqttClientFactory;
+import io.joynr.messaging.mqtt.MqttClientIdProvider;
 import io.joynr.messaging.mqtt.MqttMessagingStub;
 import io.joynr.messaging.mqtt.IMqttMessagingSkeleton;
 import io.joynr.messaging.mqtt.JoynrMqttClient;
@@ -61,6 +67,8 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.atLeast;
 
 public class MqttPahoClientTest {
 
@@ -76,6 +84,7 @@ public class MqttPahoClientTest {
     private JoynrMqttClient joynrMqttClient;
     private Properties properties;
     private ArgumentCaptor<Integer> mqttMessageIdCaptor;
+    private byte[] serializedMessage;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -98,14 +107,7 @@ public class MqttPahoClientTest {
         MockitoAnnotations.initMocks(this);
         properties = new Properties();
         mqttMessageIdCaptor = ArgumentCaptor.forClass(Integer.class);
-    }
 
-    @After
-    public void tearDown() {
-        joynrMqttClient.shutdown();
-    }
-
-    private void createJoynrMqttClient() {
         properties.put(MqttModule.PROPERTY_KEY_MQTT_BROKER_URI, "tcp://localhost:1883");
         properties.put(MqttModule.PROPERTY_KEY_MQTT_RECONNECT_SLEEP_MS, "100");
         properties.put(MqttModule.PROPERTY_KEY_MQTT_KEEP_ALIVE_TIMER_SEC, "60");
@@ -121,6 +123,26 @@ public class MqttPahoClientTest {
                        "1000");
         properties.put(ConfigurableMessagingSettings.PROPERTY_BACKPRESSURE_MAX_INCOMING_MQTT_MESSAGES_IN_QUEUE, "20");
         properties.put(ConfigurableMessagingSettings.PROPERTY_BACKPRESSURE_ENABLED, "false");
+        properties.put(MqttModule.PROPERTY_MQTT_CLEAN_SESSION, "false");
+        properties.put(MqttModule.PROPERTY_KEY_MQTT_MAX_MESSAGE_SIZE_BYTES, "0");
+        serializedMessage = new byte[10];
+    }
+
+    @After
+    public void tearDown() {
+        joynrMqttClient.shutdown();
+    }
+
+    private void createJoynrMqttClient() {
+
+        joynrMqttClient = createMqttClientWithoutSubscription();
+
+        ownTopic = injector.getInstance((Key.get(MqttAddress.class,
+                                                 Names.named(MqttModule.PROPERTY_MQTT_GLOBAL_ADDRESS))));
+        joynrMqttClient.subscribe(ownTopic.getTopic());
+    }
+
+    private JoynrMqttClient createMqttClientWithoutSubscription() {
 
         injector = Guice.createInjector(new MqttPahoModule(),
                                         new JoynrPropertiesModule(properties),
@@ -137,17 +159,13 @@ public class MqttPahoClientTest {
                                                                          });
                                             }
                                         });
+        // create a new Factory because the factory caches its client.
         mqttClientFactory = injector.getInstance(MqttClientFactory.class);
-        ownTopic = injector.getInstance((Key.get(MqttAddress.class,
-                                                 Names.named(MqttModule.PROPERTY_MQTT_GLOBAL_ADDRESS))));
 
-        // note:
-        // - the client below is used as sender and receiver at the same time
-        // - any received message needs to be acknowledged since otherwise the MQTT broker will resend it
-        joynrMqttClient = mqttClientFactory.create();
-        joynrMqttClient.start();
-        joynrMqttClient.subscribe(ownTopic.getTopic());
-        joynrMqttClient.setMessageListener(mockReceiver);
+        JoynrMqttClient client = mqttClientFactory.create();
+        client.start();
+        client.setMessageListener(mockReceiver);
+        return client;
     }
 
     private void joynrMqttClientPublishAndVerifyReceivedMessage(byte[] serializedMessage) {
@@ -187,5 +205,122 @@ public class MqttPahoClientTest {
 
         byte[] largeSerializedMessage = new byte[initialMessageSize + 1];
         joynrMqttClientPublishAndVerifyReceivedMessage(largeSerializedMessage);
+    }
+
+    @Test
+    public void mqttClientTestWithDisabledCleanSession() throws Exception {
+        properties.put(MqttModule.PROPERTY_MQTT_CLEAN_SESSION, "false");
+        String topic = "otherTopic";
+
+        // create a MqttClient which was subscribed on the topic and shut it down.
+        joynrMqttClient = createMqttClientWithoutSubscription();
+        joynrMqttClient.subscribe(topic);
+        joynrMqttClient.shutdown();
+
+        // use another MqttClient to publish a message for the first topic
+        joynrMqttClient = createMqttClientWithoutSubscription();
+        joynrMqttClient.publishMessage(topic, serializedMessage);
+        Thread.sleep(100);
+        joynrMqttClient.shutdown();
+
+        // create a MqttClient and subscribe to the same topic as the first one
+        // MqttClient will receive message if cleanSession is disabled
+        joynrMqttClient = createMqttClientWithoutSubscription();
+        joynrMqttClient.subscribe(topic);
+
+        Thread.sleep(100);
+        verify(mockReceiver, atLeast(1)).transmit(eq(serializedMessage),
+                                                  mqttMessageIdCaptor.capture(),
+                                                  eq(MqttMessagingStub.DEFAULT_QOS_LEVEL),
+                                                  any(FailureAction.class));
+
+        joynrMqttClient.messageReceivedAndProcessingFinished(mqttMessageIdCaptor.getValue(),
+                                                             MqttMessagingStub.DEFAULT_QOS_LEVEL);
+    }
+
+    @Test
+    public void mqttClientTestWithEnabledCleanSession() throws Exception {
+        properties.put(MqttModule.PROPERTY_MQTT_CLEAN_SESSION, "true");
+        String topic = "otherTopic1";
+
+        // create a MqttClient which was subscribed on the topic and shut it down.
+        joynrMqttClient = createMqttClientWithoutSubscription();
+        joynrMqttClient.subscribe(topic);
+        joynrMqttClient.shutdown();
+
+        // use another MqttClient to publish a message for the first topic
+        joynrMqttClient = createMqttClientWithoutSubscription();
+        joynrMqttClient.publishMessage(topic, serializedMessage);
+        Thread.sleep(100);
+        joynrMqttClient.shutdown();
+
+        // create a MqttClient and subscribe to the same topic as the first one
+        // MqttClient will receive message if cleanSession is disabled
+        joynrMqttClient = createMqttClientWithoutSubscription();
+        joynrMqttClient.subscribe(topic);
+
+        Thread.sleep(100);
+        verify(mockReceiver, times(0)).transmit(eq(serializedMessage),
+                                                mqttMessageIdCaptor.capture(),
+                                                eq(MqttMessagingStub.DEFAULT_QOS_LEVEL),
+                                                any(FailureAction.class));
+    }
+
+    @Test
+    public void mqttClientTestResubscriptionWithCleanRestartEnabled() throws Exception {
+
+        injector = Guice.createInjector(new MqttPahoModule(),
+                                        new JoynrPropertiesModule(properties),
+                                        new AbstractModule() {
+
+                                            @Override
+                                            protected void configure() {
+                                                bind(MessageRouter.class).toInstance(mockMessageRouter);
+                                                bind(ScheduledExecutorService.class).annotatedWith(Names.named(MessageRouter.SCHEDULEDTHREADPOOL))
+                                                                                    .toInstance(Executors.newScheduledThreadPool(10));
+                                                bind(RawMessagingPreprocessor.class).to(NoOpRawMessagingPreprocessor.class);
+                                                Multibinder.newSetBinder(binder(),
+                                                                         new TypeLiteral<JoynrMessageProcessor>() {
+                                                                         });
+                                            }
+                                        });
+
+        ownTopic = injector.getInstance((Key.get(MqttAddress.class,
+                                                 Names.named(MqttModule.PROPERTY_MQTT_GLOBAL_ADDRESS))));
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(10);
+        MqttClientIdProvider mqttClientIdProvider = injector.getInstance(MqttClientIdProvider.class);
+
+        String clientId = mqttClientIdProvider.getClientId();
+        String brokerUri = "tcp://localhost:1883";
+        int reconnectSleepMs = 100;
+        int keepAliveTimerSec = 60;
+        int connectionTimeoutSec = 60;
+        int timeToWaitMs = -1;
+        int maxMsgsInflight = 100;
+        int maxMsgSizeBytes = 0;
+        boolean cleanSession = true;
+
+        MqttClient mqttClient = new MqttClient(brokerUri, clientId, new MemoryPersistence(), scheduledExecutorService);
+        joynrMqttClient = new MqttPahoClient(mqttClient,
+                                             reconnectSleepMs,
+                                             keepAliveTimerSec,
+                                             connectionTimeoutSec,
+                                             timeToWaitMs,
+                                             maxMsgsInflight,
+                                             maxMsgSizeBytes,
+                                             cleanSession);
+
+        joynrMqttClient.start();
+        joynrMqttClient.setMessageListener(mockReceiver);
+        joynrMqttClient.subscribe(ownTopic.getTopic());
+
+        // manually call disconnect and connectionLost
+        mqttClient.disconnect(500);
+        MqttException exeption = new MqttException(MqttException.REASON_CODE_CLIENT_TIMEOUT);
+        MqttPahoClient mqttPahoClient = (MqttPahoClient) joynrMqttClient;
+        mqttPahoClient.connectionLost(exeption);
+
+        joynrMqttClientPublishAndVerifyReceivedMessage(serializedMessage);
     }
 }
