9de2a3829b76aaab72a27bb1b5b4dbcd0b6018ab
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2017-11-20 10:53:25 +0100

    [C++] Updated websocketpp
    * removed earlier applied patch
    * updated to official websocketpp version with
      git sha 19cad9925f83d15d7487c16f0491f4741ec9f674
    * updated docker container definition
    
    Change-Id: I86a14bdd3fa726d18a320c9ce1a6e5c517addc67

--- cpp/CMake/AddWebSocketPP.cmake
+++ cpp/CMake/AddWebSocketPP.cmake
@@ -5,7 +5,7 @@ else(USE_PLATFORM_WEBSOCKETPP)
     AddExternalProject(
         websocketpp
         GIT_REPOSITORY https://github.com/zaphoyd/websocketpp.git
-        GIT_TAG 378437aecdcb1dfe62096ffd5d944bf1f640ccc3 # git tag 0.7.0
+        GIT_TAG 19cad9925f83d15d7487c16f0491f4741ec9f674
         TIMEOUT 10
         # Disable configure step
         CONFIGURE_COMMAND ""
@@ -15,4 +15,3 @@ else(USE_PLATFORM_WEBSOCKETPP)
     ExternalProject_Get_Property(websocketpp SOURCE_DIR)
     set(WEBSOCKETPP_INCLUDE_DIR "${SOURCE_DIR}")
 endif(USE_PLATFORM_WEBSOCKETPP)
-
