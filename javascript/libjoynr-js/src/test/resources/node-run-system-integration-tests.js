284d6256b6affb8ce2dbd3dbf1a63e3d2ea04112
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2017-11-20 08:19:49 +0000

    [JS] SIT: Run configuration for parent node process
    Change-Id: Ia794e72c7c2a156594c512a0cc72c1e22736892c

---
+++ javascript/libjoynr-js/src/test/resources/node-run-system-integration-tests.js
@@ -0,0 +1,71 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2017 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+console.log("joynr Jasmine 2.x system integration tests");
+
+var Jasmine = require("jasmine");
+var jasmine = new Jasmine();
+jasmine.loadConfigFile("spec/support/jasmine.json");
+
+console.log("Jasmine version: " + jasmine.version);
+
+// because the generated code uses require('joynr') without knowing the location, will work only
+// when joynr is a submodule and is placed inside node_modules folder. In order to emulate this
+// behavior the require function is adapted here in order to always return the correct joynr while
+// running tests.
+var mod = require("module");
+var joynr = require("../classes/joynr");
+var path = require("path");
+var req = mod.prototype.require;
+mod.prototype.require = function(md) {
+    if (md === "joynr") {
+        return joynr;
+    }
+
+    // mock localStorage
+    if (md.endsWith("LocalStorageNode")) {
+        var appDir = path.dirname(require.main.filename);
+        return req.call(this, appDir + "/node_integration/LocalStorageMock.js");
+    }
+
+    if (
+        md.startsWith("joynr/vehicle") ||
+        md.startsWith("joynr/datatypes") ||
+        md.startsWith("joynr/tests") ||
+        md.startsWith("joynr/provisioning")
+    ) {
+        return req.call(this, "../" + md);
+    }
+
+    if (md.startsWith("joynr")) {
+        return req.call(this, "../../classes/" + md);
+    }
+
+    if (md === "joynr/Runtime") {
+        return req.call(this, "joynr/Runtime.inprocess");
+    }
+    return req.apply(this, arguments);
+};
+console.log("require config setup");
+var End2EndRPCTest = require("../test-classes/node_integration/End2EndRPCTest.js");
+var End2EndSubscriptionTest = require("../test-classes/node_integration/End2EndSubscriptionTest.js");
+var End2EndDatatypesTest = require("../test-classes/node_integration/End2EndDatatypesTest.js");
+var LocalDiscoveryTest = require("../test-classes/node_integration/LocalDiscoveryTest");
+console.log("all tests modules loaded");
+jasmine.execute();
