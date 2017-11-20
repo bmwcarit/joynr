cff77943ae851f09811e0229686d8432b185b6ac
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2017-11-20 08:37:07 +0000

    [JS] SIT: Port Datatypes-Provider WebWorker to Node ChildProcess
    Change-Id: I6b970c893159dc4a87a6149693d899f829465e46

--- javascript/libjoynr-js/src/test/js/node_integration/TestEnd2EndDatatypesProviderProcess.js
+++ javascript/libjoynr-js/src/test/js/node_integration/TestEnd2EndDatatypesProviderProcess.js
@@ -1,5 +1,5 @@
-/*jslint es5: true */
-/*global Promise: true, WorkerUtils: true, importScripts: true, joynr: true, DatatypesProvider: true, providerParticipantIdDatatypes, domain: true, TestEnd2EndDatatypesTestData: true, interfaceNameDatatypes: true, providerParticipantIdDatatypes: true, providerChannelIdDatatypes: true, globalCapDirCapability: true, channelUrlDirCapability: true */
+/*jslint es5: true, node: true */
+/* domain: true, interfaceNameDatatypes: true, globalCapDirCapability: true, channelUrlDirCapability: true */
 
 /*
  * #%L
@@ -20,34 +20,25 @@
  * #L%
  */
 
+var ChildProcessUtils = require("./ChildProcessUtils");
+ChildProcessUtils.overrideRequirePaths();
+
+var Promise = require("../../classes/global/Promise");
 // anything that you load here is served through the jsTestDriverServer, if you add an entry you
 // have to make it available through the jsTestDriverIntegrationTests.conf
-importScripts("WorkerUtils.js");
-importScripts("../joynr/provisioning/provisioning_root.js");
-importScripts("LocalStorageSimulator.js");
+var ChildProcessUtils = require("./ChildProcessUtils");
+var joynr = require("joynr");
+var provisioning = require("../joynr/provisioning/provisioning_cc.js");
 
-importScripts("../../jar-classes/joynr.js");
-importScripts("../joynr/provisioning/provisioning_cc.js");
+var DatatypesProvider = require("joynr/datatypes/DatatypesProvider.js");
+var TestEnd2EndDatatypesTestData = require("./TestEnd2EndDatatypesTestData");
 
-var document = { URL: window.joynr.provisioning.brokerUri };
-
-importScripts("provisioning_end2end_common.js");
-
-importScripts("../joynr/datatypes/exampleTypes/Country.js");
-importScripts("../joynr/datatypes/exampleTypes/ComplexRadioStation.js");
-importScripts("../joynr/datatypes/DatatypesProvider.js");
-importScripts("TestEnd2EndDatatypesTestData.js");
-importScripts("../../classes/lib/bluebird.js");
-
-var Promise = Promise.Promise;
 var providerDomain;
 
 // attribute values for provider
-var currentAttributeValue,
-    isOn = true;
+var currentAttributeValue;
 
 var datatypesProvider;
-var runtime;
 
 var providerQos;
 
@@ -69,101 +60,94 @@ function setter(value) {
 }
 
 function initializeTest(provisioningSuffix, providedDomain) {
-    return new Promise(function(resolve, reject) {
-        providerDomain = providedDomain !== undefined ? providedDomain : domain;
-
-        // set joynr provisioning
-        localStorage.setItem(
-            "joynr.participants." + providerDomain + interfaceNameDatatypes,
-            providerParticipantIdDatatypes + provisioningSuffix
-        );
-        joynr.provisioning.channelId = providerChannelIdDatatypes + provisioningSuffix;
-        joynr.provisioning.logging = {
-            configuration: {
-                name: "TestEnd2EndCommProviderWorkLogging",
-                appenders: {
-                    WebWorker: {
-                        name: "WEBWORKER"
-                    }
-                },
-                loggers: {
-                    root: {
-                        level: "debug",
-                        AppenderRef: {
-                            ref: "WEBWORKER"
+    providerDomain = providedDomain;
+    provisioning.persistency = "localStorage";
+    provisioning.channelId = "End2EndDatatypesTestParticipantId" + provisioningSuffix;
+    provisioning.logging = {
+        configuration: {
+            appenders: {
+                appender: [
+                    {
+                        type: "Console",
+                        name: "STDOUT",
+                        PatternLayout: {
+                            pattern: "[%d{HH:mm:ss,SSS}][%c][%p] %m{2}"
                         }
                     }
+                ]
+            },
+            loggers: {
+                root: {
+                    level: "debug",
+                    AppenderRef: [
+                        {
+                            ref: "STDOUT"
+                        }
+                    ]
                 }
             }
-        };
+        }
+    };
 
-        joynr.provisioning.persistency = "localStorage";
-
-        joynr
-            .load(joynr.provisioning)
-            .then(function(newJoynr) {
-                joynr = newJoynr;
-                providerQos = new joynr.types.ProviderQos({
-                    customParameters: [],
-                    priority: Date.now(),
-                    scope: joynr.types.ProviderScope.GLOBAL,
-                    supportsOnChangeSubscriptions: true
-                });
-
-                // build the provider
-                datatypesProvider = joynr.providerBuilder.build(DatatypesProvider, {});
-
-                var i;
-                // there are so many attributes for testing different datatypes => register them
-                // all by cycling over their names in the attribute
-                for (i = 0; i < TestEnd2EndDatatypesTestData.length; ++i) {
-                    var attribute = datatypesProvider[TestEnd2EndDatatypesTestData[i].attribute];
-                    attribute.registerGetter(getter);
-                    attribute.registerSetter(setter);
-                }
-
-                // registering operation functions
-                datatypesProvider.getJavascriptType.registerOperation(function(opArgs) {
-                    return {
-                        javascriptType: getObjectType(opArgs.arg)
-                    };
-                });
-                datatypesProvider.getArgumentBack.registerOperation(function(opArgs) {
-                    return {
-                        returnValue: opArgs.arg
-                    };
-                });
-                datatypesProvider.multipleArguments.registerOperation(function(opArgs) {
-                    return {
-                        serialized: JSON.stringify(opArgs)
-                    };
-                });
-
-                // register provider at the given domain
-                joynr.registration
-                    .registerProvider(providerDomain, datatypesProvider, providerQos)
-                    .then(function() {
-                        // signal test driver that we are ready
-                        resolve(joynr);
-                        return;
-                    })
-                    .catch(function(error) {
-                        reject(error);
-                        throw new Error("error registering provider: " + error);
-                    });
-                return joynr;
-            })
-            .catch(function(error) {
-                throw error;
+    joynr.selectRuntime("inprocess");
+    return joynr
+        .load(provisioning)
+        .then(function(newJoynr) {
+            providerQos = new joynr.types.ProviderQos({
+                customParameters: [],
+                priority: Date.now(),
+                scope: joynr.types.ProviderScope.GLOBAL,
+                supportsOnChangeSubscriptions: true
             });
-    });
+
+            // build the provider
+            datatypesProvider = joynr.providerBuilder.build(DatatypesProvider, {});
+
+            var i;
+            // there are so many attributes for testing different datatypes => register them
+            // all by cycling over their names in the attribute
+            for (i = 0; i < TestEnd2EndDatatypesTestData.length; ++i) {
+                var attribute = datatypesProvider[TestEnd2EndDatatypesTestData[i].attribute];
+                attribute.registerGetter(getter);
+                attribute.registerSetter(setter);
+            }
+
+            // registering operation functions
+            datatypesProvider.getJavascriptType.registerOperation(function(opArgs) {
+                return {
+                    javascriptType: getObjectType(opArgs.arg)
+                };
+            });
+            datatypesProvider.getArgumentBack.registerOperation(function(opArgs) {
+                return {
+                    returnValue: opArgs.arg
+                };
+            });
+            datatypesProvider.multipleArguments.registerOperation(function(opArgs) {
+                return {
+                    serialized: JSON.stringify(opArgs)
+                };
+            });
+
+            // register provider at the given domain
+            return newJoynr.registration
+                .registerProvider(providerDomain, datatypesProvider, providerQos)
+                .then(function() {
+                    return Promise.resolve(newJoynr);
+                });
+        })
+        .catch(function(error) {
+            throw error;
+        });
 }
 
 function startTest() {
-    // nothing to do here, evertything is already performed in initialize
+    // nothing to do here, everything is already performed in initialize
     return Promise.resolve();
 }
 
 function terminateTest() {
     return joynr.registration.unregisterProvider(providerDomain, datatypesProvider);
 }
+
+ChildProcessUtils.registerHandlers(initializeTest, startTest, terminateTest);
