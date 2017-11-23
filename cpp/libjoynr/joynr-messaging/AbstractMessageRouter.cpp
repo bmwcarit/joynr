3a6b62c73626f3a0ac30573e678159f19ec6c160
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2017-11-23 08:40:39 +0100

    [C++] Fixed memory leak in AbstractMessageRouter
    * callback now uses weak_ptr to ITransportStatus to avoid
      cyclic dependency
    
    Change-Id: I8a3bff0a864750330620796b287dabfde07dbc10

--- cpp/libjoynr/joynr-messaging/AbstractMessageRouter.cpp
+++ cpp/libjoynr/joynr-messaging/AbstractMessageRouter.cpp
@@ -357,15 +357,19 @@ void AbstractMessageRouter::activateRoutingTableCleanerTimer()
 void AbstractMessageRouter::registerTransportStatusCallbacks()
 {
     for (auto& transportStatus : transportStatuses) {
-        transportStatus->setAvailabilityChangedCallback(
-                [ thisWeakPtr = joynr::util::as_weak_ptr(shared_from_this()), transportStatus ](
-                        bool isAvailable) {
-                    if (auto thisSharedPtr = thisWeakPtr.lock()) {
-                        if (isAvailable) {
-                            thisSharedPtr->rescheduleQueuedMessagesForTransport(transportStatus);
-                        }
+        transportStatus->setAvailabilityChangedCallback([
+            thisWeakPtr = joynr::util::as_weak_ptr(shared_from_this()),
+            transportStatusWeakPtr = joynr::util::as_weak_ptr(transportStatus)
+        ](bool isAvailable) {
+            if (isAvailable) {
+                if (auto thisSharedPtr = thisWeakPtr.lock()) {
+                    if (auto transportStatusSharedPtr = transportStatusWeakPtr.lock()) {
+                        thisSharedPtr->rescheduleQueuedMessagesForTransport(
+                                transportStatusSharedPtr);
                     }
-                });
+                }
+            }
+        });
     }
 }
 
@@ -538,13 +542,13 @@ void MessageRunnable::run()
                     std::chrono::milliseconds delay = delayException.getDelayMs();
 
                     if (auto messageRouterSharedPtr = thisSharedPtr->messageRouter.lock()) {
-                        JOYNR_LOG_TRACE(
-                                logger(),
-                                "Rescheduling message after error: messageId: {}, new delay {}ms, "
-                                "reason: {}",
-                                thisSharedPtr->message->getId(),
-                                delay.count(),
-                                e.getMessage());
+                        JOYNR_LOG_TRACE(logger(),
+                                        "Rescheduling message after error: messageId: {}, new "
+                                        "delay {}ms, "
+                                        "reason: {}",
+                                        thisSharedPtr->message->getId(),
+                                        delay.count(),
+                                        e.getMessage());
                         messageRouterSharedPtr->scheduleMessage(thisSharedPtr->message,
                                                                 thisSharedPtr->destAddress,
                                                                 thisSharedPtr->tryCount + 1,
