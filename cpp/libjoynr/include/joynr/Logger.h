30294c09d3a1bbf5052c625c550ca155d8e1fd36
Thomas Pal <thomas.pal@bmw-carit.de>
2017-12-20 07:50:01 +0100

    [C++] Use error as default runtime log level
    Change-Id: I3f5755589731edc3d1e41186544d64af61cbc2a0

--- cpp/libjoynr/include/joynr/Logger.h
+++ cpp/libjoynr/include/joynr/Logger.h
@@ -85,7 +85,7 @@ enum class LogLevel { Trace, Debug, Info, Warn, Error, Fatal };
 #endif // JOYNR_DEFAULT_RUNTIME_LOG_LEVEL_DEBUG
 
 #ifndef JOYNR_DEFAULT_RUNTIME_LOG_LEVEL
-#define JOYNR_DEFAULT_RUNTIME_LOG_LEVEL spdlog::level::trace
+#define JOYNR_DEFAULT_RUNTIME_LOG_LEVEL spdlog::level::err
 #endif
 
 #define JOYNR_CONDITIONAL_SPDLOG(level, method, logger, ...)                                       \
