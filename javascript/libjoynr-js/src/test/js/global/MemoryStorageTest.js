088d3a7cc59a0b89c3b7a2953ba13883344f2190
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2018-01-30 13:12:20 +0000

    [JS] persistency test for MemoryStorage
    Change-Id: I53c0811ae77d7cb04381be8ee50b1978b34ad99a

---
+++ javascript/libjoynr-js/src/test/js/global/MemoryStorageTest.js
@@ -0,0 +1,62 @@
+/*jslint node: true, stupid: true */
+
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2018 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+var MemoryStorage = require("../../classes/global/MemoryStorage");
+
+describe("memory storage", function() {
+    var storage;
+    var item = {
+        hi: "bla"
+    };
+    var key = "key";
+
+    beforeEach(function(done) {
+        storage = new MemoryStorage();
+        done();
+    });
+
+    it("can set and load item", function() {
+        storage.setItem(key, JSON.stringify(item));
+        var result = JSON.parse(storage.getItem(key));
+        expect(result).toEqual(item);
+    });
+
+    it("can set and load long items", function() {
+        var longString = new Array(200).join("a");
+
+        storage.setItem(longString, JSON.stringify(item));
+        var result = JSON.parse(storage.getItem(longString));
+        expect(result).toEqual(item);
+    });
+
+    it("can remove items", function() {
+        storage.setItem(key, JSON.stringify(item));
+        storage.removeItem(key);
+        var result = storage.getItem(key);
+        expect(result).toEqual(undefined);
+    });
+
+    it("can clear items", function() {
+        storage.setItem(key, JSON.stringify(item));
+        storage.clear();
+        var result = storage.getItem(key);
+        expect(result).toEqual(undefined);
+    });
+});
