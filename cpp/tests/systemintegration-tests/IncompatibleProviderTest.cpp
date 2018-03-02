0178992ce409732edd06e04ce7e40c9a89bb8257
Lars Brokmeier <Lars.Brokmeier@partner.bmw.de>
2018-03-02 16:31:37 +0100

    [C++] Add systemintegration test for version generation
    Adds two system integration tests.
    One where a provider is created for one version of an interface
    and a proxy for another.
    One which tests compatibility of the same version generated with
    different flags.
    
    Change-Id: I6aca1a120aff2826715523146c9051ffb1982b72

---
+++ cpp/tests/systemintegration-tests/IncompatibleProviderTest.cpp
@@ -0,0 +1,73 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2018 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#include <gtest/gtest.h>
+
+#include "joynr/Settings.h"
+#include "joynr/tests/v1/DefaultMultipleVersionsInterfaceProvider.h"
+#include "joynr/tests/v2/MultipleVersionsInterfaceProxy.h"
+
+#include "joynr/JoynrRuntime.h"
+#include "joynr/exceptions/JoynrException.h"
+#include "tests/JoynrTest.h"
+
+using namespace ::testing;
+using namespace joynr;
+
+class IncompatibleProviderTest : public Test
+{
+public:
+
+    IncompatibleProviderTest() :
+        runtime(),
+        testDomain("testDomain")
+    {
+        runtime = JoynrRuntime::createRuntime(std::make_unique<Settings>());
+        discoveryQos.setDiscoveryTimeoutMs(100);
+        discoveryQos.setDiscoveryScope(joynr::types::DiscoveryScope::LOCAL_ONLY);
+    }
+
+    ~IncompatibleProviderTest() override {
+        test::util::removeAllCreatedSettingsAndPersistencyFiles();
+    }
+
+protected:
+    std::shared_ptr<JoynrRuntime> runtime;
+    const std::string testDomain;
+    DiscoveryQos discoveryQos;
+    MessagingQos messagingQos;
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(IncompatibleProviderTest);
+};
+
+TEST_F(IncompatibleProviderTest, proxyCreationFails) {
+    auto testProvider = std::make_shared<tests::v1::DefaultMultipleVersionsInterfaceProvider>();
+    joynr::types::ProviderQos providerQos;
+    providerQos.setScope(joynr::types::ProviderScope::LOCAL);
+    runtime->registerProvider<tests::v1::MultipleVersionsInterfaceProvider>(testDomain, testProvider, providerQos);
+
+    std::shared_ptr<ProxyBuilder<tests::v2::MultipleVersionsInterfaceProxy>> testProxyBuilder(
+        runtime->createProxyBuilder<tests::v2::MultipleVersionsInterfaceProxy>(testDomain)
+    );
+
+    EXPECT_THROW(testProxyBuilder
+       ->setMessagingQos(messagingQos)
+       ->setDiscoveryQos(discoveryQos)
+       ->build(), joynr::exceptions::DiscoveryException);
+}
