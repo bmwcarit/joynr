b5faddcc7776c6945ef4baa081e0ac42b2aed08f
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2018-03-14 09:31:47 +0000

    [JS] build ilt with JS tests
    because of node v8 creates a symlink, js needs to be properly build
    * the gyp bindings for smrf, etc. need to be created
     (no npm i --ignore-scripts)
    * the fixrequire script needs to be run
    
    this is currently only the case when js is build with tests
    
    Change-Id: I6399d95f77fc4957bd8c6a29268aab03b466c064

--- docker/joynr-ilt-gcc/scripts/build/build_all.sh
+++ docker/joynr-ilt-gcc/scripts/build/build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./java-clean-build skipTests
-./javascript-clean-build --skipTests
+./javascript-clean-build
 ./cpp-install-muesli.sh --default
 ./cpp-clean-build.sh --buildtests OFF --enableclangformatter OFF
 ./cpp-build-tests.sh inter-language-test
