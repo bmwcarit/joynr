8ed228032e79f233f0cef9c3d154c014e1ebeb72
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2018-05-16 10:33:14 +0000

    [Docker] build JS with skipTests when possible
    it's possible to build with skipTests due to change
    from mvn to npm
    
    Change-Id: I248209ff6d12aeca12fa2843d4f03915dd0b533f

--- docker/joynr-ilt-gcc/scripts/build/build_all.sh
+++ docker/joynr-ilt-gcc/scripts/build/build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./java-clean-build skipTests
-./javascript-clean-build
+./javascript-clean-build --skipTests
 ./cpp-install-muesli.sh --default
 ./cpp-clean-build.sh --buildtests OFF --enableclangformatter OFF
 ./cpp-build-tests.sh inter-language-test
