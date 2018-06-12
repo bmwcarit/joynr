d3425cc17cbfc479afe5c27b1f2ce1ce114eef28
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2018-06-12 12:38:20 +0000

    [JS] perf-tests: simplify configuration files
    Change-Id: I92810aafd35d6570316108d09b288e6b29c6ba53

--- tests/performance-test/src/main/js/config/configTls.js
+++ tests/performance-test/src/main/js/config/configTls.js
@@ -17,59 +17,12 @@
  * #L%
  */
 
-module.exports = {
-    benchmarks: [
-        {
-            name: "attributeString",
-            enabled: "true",
-            numRuns: 8000
-        },
-        {
-            name: "echoComplexStruct",
-            enabled: "true",
-            numRuns: 8000
-        },
-        {
-            name: "echoString",
-            enabled: "true",
-            numRuns: 8000
-        },
-        {
-            name: "echoByteArray",
-            enabled: "true",
-            numRuns: 8000
-        },
-        {
-            name: "registerPlentyOfConsumers",
-            enabled: "true",
-            numRuns: 500
-        },
-        {
-            name: "simpleBroadcast",
-            enabled: "false",
-            numRuns: 8000,
-            type: "broadcast",
-            numProxies: 4
-        }
-    ],
-    global: {
-        testRuns: 30,
-        domain: "performance_test_domain",
-        cc: {
-            host: "localhost",
-            port: "4243"
-        },
-        measureMemory: "false",
-        heapSnapShot: "false",
-        stringLength: 10,
-        timeout: 3600000,
-        byteArraySize: 20,
-        testType: "burst" // burst, concurrency, single
-    },
-    tls: {
-        certPath: "/data/ssl-data/certs/client.cert.pem",
-        keyPath: "/data/ssl-data/private/client.key.pem",
-        caPath: "/data/ssl-data/certs/ca.cert.pem",
-        ownerId: "client"
-    }
+const baseConfig = require("./config");
+baseConfig.tls = {
+    certPath: "/data/ssl-data/certs/client.cert.pem",
+    keyPath: "/data/ssl-data/private/client.key.pem",
+    caPath: "/data/ssl-data/certs/ca.cert.pem",
+    ownerId: "client"
 };
+
+module.exports = baseConfig;
