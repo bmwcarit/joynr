49afdbf26d7e83560b546f6c3ba7464ebe537db6
Donato Sciarra <donato.sciarra@bmw-carit.de>
2018-07-24 15:05:37 +0200

    [C++] Do not use corrupted ParticipantIdStorage files.
    Change-Id: Ibf4d019b74427608acd2508e8cec0ef76043719d

--- cpp/libjoynr/capabilities/ParticipantIdStorage.cpp
+++ cpp/libjoynr/capabilities/ParticipantIdStorage.cpp
@@ -59,14 +59,12 @@ void ParticipantIdStorage::loadEntriesFromFile()
             auto retVal = storage.insert(std::move(item));
             assert(retVal.second);
         }
-    } catch (const boost::property_tree::ini_parser_error& ex) {
-        JOYNR_LOG_WARN(logger(),
-                       "The specified participantId file {} is not valid. Exception: ",
-                       ex.what());
-        return;
     } catch (const std::exception& ex) {
-        JOYNR_LOG_WARN(logger(), "Cannot read participantId storage file. Exception: ", ex.what());
-        return;
+        JOYNR_LOG_WARN(logger(),
+                       "Cannot read participantId storage file {}. Removing file. Exception: {}",
+                       fileName,
+                       ex.what());
+        std::remove(fileName.c_str());
     }
 
     JOYNR_LOG_TRACE(
