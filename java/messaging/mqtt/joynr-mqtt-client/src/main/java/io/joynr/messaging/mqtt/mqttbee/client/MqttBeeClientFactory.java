7cd5f87c9199ad67d41262cbb4344799b23439cd
Clive Jevons <clive@jevons-it.net>
2018-08-14 20:31:10 +0200

    [Java] first draft of PoC impl of mqtt-bee integration
    Change-Id: Ief5849537a5b6e0f7146f76910099e24745cb6a1

---
+++ java/messaging/mqtt/joynr-mqtt-client/src/main/java/io/joynr/messaging/mqtt/mqttbee/client/MqttBeeClientFactory.java
@@ -0,0 +1,123 @@
+package io.joynr.messaging.mqtt.mqttbee.client;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ScheduledExecutorService;
+
+import com.google.inject.Singleton;
+import io.joynr.messaging.routing.MessageRouter;
+import io.reactivex.schedulers.Schedulers;
+import org.mqttbee.api.mqtt.MqttClient;
+import org.mqttbee.api.mqtt.MqttClientExecutorConfig;
+import org.mqttbee.api.mqtt.mqtt3.Mqtt3Client;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+import io.joynr.exceptions.JoynrIllegalStateException;
+import io.joynr.messaging.mqtt.JoynrMqttClient;
+import io.joynr.messaging.mqtt.MqttClientFactory;
+import io.joynr.messaging.mqtt.MqttClientIdProvider;
+import io.joynr.messaging.mqtt.MqttModule;
+import joynr.system.RoutingTypes.MqttAddress;
+import org.mqttbee.api.mqtt.mqtt3.Mqtt3ClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This factory class is responsible for producing joynr MQTT clients using the mqtt-bee library.
+ * The mqtt-bee library is explicitly intended for use in backend applications, so using it on
+ * constrained devices might not work well. Consider using the Paho client in those circumstances.
+ *
+ * Currently mqtt-bee is pre 1.0. Here are a list of things that need doing once the relevant missing
+ * feature have been completed in mqtt-bee and we want to make this integration production ready:
+ *
+ * TODO
+ * - Add ability to configure SSL setup (trust store / key store etc)
+ * - When auto-reconnect feature exists, then enable it
+ * - When persistent session configuration exists, then enable configuration thereof
+ * - Document usage and configuration of mqtt-bee variant
+ */
+@Singleton
+public class MqttBeeClientFactory implements MqttClientFactory {
+
+    private static final Logger logger = LoggerFactory.getLogger(MqttBeeClientFactory.class);
+
+    private final MqttAddress ownAddress;
+    private final boolean separateConnections;
+    private final MqttClientIdProvider mqttClientIdProvider;
+    private final ScheduledExecutorService scheduledExecutorService;
+    private final int keepAliveTimeSeconds;
+
+    private JoynrMqttClient sender;
+    private JoynrMqttClient receiver;
+
+    @Inject
+    public MqttBeeClientFactory(@Named(MqttModule.PROPERTY_MQTT_GLOBAL_ADDRESS) MqttAddress ownAddress,
+                                @Named(MqttModule.PROPERTY_KEY_MQTT_SEPARATE_CONNECTIONS) boolean separateConnections,
+                                @Named(MqttModule.PROPERTY_KEY_MQTT_KEEP_ALIVE_TIMER_SEC) int keepAliveTimeSeconds,
+                                @Named(MessageRouter.SCHEDULEDTHREADPOOL) ScheduledExecutorService scheduledExecutorService,
+                                MqttClientIdProvider mqttClientIdProvider) {
+        this.ownAddress = ownAddress;
+        this.separateConnections = separateConnections;
+        this.scheduledExecutorService = scheduledExecutorService;
+        this.mqttClientIdProvider = mqttClientIdProvider;
+        this.keepAliveTimeSeconds = keepAliveTimeSeconds;
+    }
+
+    @Override
+    public JoynrMqttClient createSender() {
+        if (sender == null) {
+            synchronized (this) {
+                if (sender == null) {
+                    logger.info("Creating sender MQTT client");
+                    sender = createClient(mqttClientIdProvider.getClientId() + (separateConnections ? "Pub" : ""));
+                    logger.info("Sender MQTT client now: {}", sender);
+                }
+            }
+        }
+        return sender;
+    }
+
+    @Override
+    public JoynrMqttClient createReceiver() {
+        if (receiver == null) {
+            synchronized (this) {
+                if (receiver == null) {
+                    logger.info("Creating receiver MQTT client");
+                    if (separateConnections) {
+                        receiver = createClient(mqttClientIdProvider.getClientId() + "Sub");
+                    } else {
+                        receiver = createSender();
+                    }
+                    logger.info("Receiver MQTT client now: {}", receiver);
+                }
+            }
+        }
+        return receiver;
+    }
+
+    private JoynrMqttClient createClient(String clientId) {
+        try {
+            URI serverUri = new URI(ownAddress.getBrokerUri());
+            logger.info("Connecting to {}:{}", serverUri.getHost(), serverUri.getPort());
+            MqttClientExecutorConfig executorConfig = MqttClientExecutorConfig.builder()
+                                                                              .nettyExecutor(scheduledExecutorService)
+                                                                              .applicationScheduler(Schedulers.from(scheduledExecutorService))
+                                                                              .build();
+            Mqtt3ClientBuilder clientBuilder = MqttClient.builder()
+                                                         .useMqttVersion3()
+                                                         .identifier(clientId)
+                                                         .serverHost(serverUri.getHost())
+                                                         .serverPort(serverUri.getPort())
+                                                         .executorConfig(executorConfig);
+            if (serverUri.getScheme().equals("ssl") || serverUri.getScheme().equals("tls")) {
+                clientBuilder.useSslWithDefaultConfig();
+            }
+            Mqtt3Client client = clientBuilder.buildReactive();
+            return new MqttBeeClient(client, keepAliveTimeSeconds);
+        } catch (URISyntaxException e) {
+            throw new JoynrIllegalStateException("Invalid MQTT broker URI: " + ownAddress.getBrokerUri(), e);
+        }
+    }
+}
---
+++ java/messaging/mqtt/joynr-mqtt-client/src/main/java/io/joynr/messaging/mqtt/mqttbee/client/MqttBeeModule.java
@@ -0,0 +1,30 @@
+package io.joynr.messaging.mqtt.mqttbee.client;
+
+import com.google.inject.AbstractModule;
+import io.joynr.messaging.mqtt.MqttClientFactory;
+import io.joynr.messaging.mqtt.MqttModule;
+
+/**
+ * Use this module if you want to use the mqtt-bee backed implementation of MQTT communication for joynr.
+ *
+ * Currently mqtt-bee is pre 1.0. Here are a list of things that need doing once the relevant missing
+ * feature have been completed in mqtt-bee and we want to make this integration production ready:
+ *
+ * TODO
+ * - As mqtt-bee brings in netty and rxjava as dependencies, we should probably move this integration out
+ *   into its own artifact, so that constrained environments can still use the paho library implementation
+ *   without pulling in netty and rxjava
+ * - Equally, we should pull out the paho implementation into its own artifact, so that when you choose to
+ *   use the mqtt-bee version, you don't end up pulling in paho and all of its dependencies
+ * - Document how to choose which implementation your runtime uses, and also what the pros and cons of each
+ *   are
+ */
+public class MqttBeeModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        install(new MqttModule());
+        bind(MqttClientFactory.class).to(MqttBeeClientFactory.class);
+    }
+
+}
