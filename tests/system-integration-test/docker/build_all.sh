85a34e9c970f10d4300d12e8da98078e03f8a051
Thomas Reuter <thomas.reuter@de.bertrandt.com>
2018-08-31 15:11:11 +0200

    [SIT] Reorder build in build_all.sh
    Change-Id: I2cbcdbe0bdf28b5c382320e5d7d93ed977e9a5f5

--- tests/system-integration-test/docker/build_all.sh
+++ tests/system-integration-test/docker/build_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for BUILD_DIR in joynr-backend-jee sit-jee-app onboard
+# build onboard image first because it builds joynr
+# which is required by the other build scripts
+for BUILD_DIR in onboard joynr-backend-jee sit-jee-app
 do
 	pushd $BUILD_DIR
 	./build_docker_image.sh
