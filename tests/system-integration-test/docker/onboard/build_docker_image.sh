bdc43477575540e3bf924a03bdc00c983e29d9d8
Donato Sciarra <donato.sciarra@bmw-carit.de>
2018-10-01 13:49:34 +0200

    [SIT] Do not call exit in scripts.
    Change-Id: Ide729b9fe67db4ab4c5c288e3071735123e099b9

--- tests/system-integration-test/docker/onboard/build_docker_image.sh
+++ tests/system-integration-test/docker/onboard/build_docker_image.sh
@@ -296,8 +296,7 @@ echo "environment:" `env`
 echo "docker build -t sit-onboard-apps:latest --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg no_proxy=${no_proxy} $DOCKER_BUILDDIR"
 docker build -t sit-onboard-apps:latest --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg no_proxy=${no_proxy} $DOCKER_BUILDDIR
 
-docker images --filter "dangling=true" -q | xargs docker rmi -f 2>/dev/null
-rm -Rf $DOCKER_BUILDDIR
+echo "### end build_docker_image.sh for joynr-backend-jee ###"
+rm -rf $DOCKER_BUILDDIR
 
 echo "### end build_docker_image.sh for onboard ###"
-exit 0
--- tests/system-integration-test/docker/sit-jee-app/build_docker_image.sh
+++ tests/system-integration-test/docker/sit-jee-app/build_docker_image.sh
@@ -2,7 +2,7 @@
 
 echo "### start build_docker_image.sh for sit-jee-app ###"
 
-set -e
+set -e -x
 
 if [ ! -d ../../sit-jee-app/target ] || [ ! -f ../../sit-jee-app/target/sit-jee-app.war ]; then
 	echo "ERROR: You have to have built the project with 'mvn package' first."
@@ -17,8 +17,7 @@ if [ -z "$(docker version 2>/dev/null)" ]; then
 fi
 
 docker build -t sit-jee-app:latest .
-docker images --filter "dangling=true" -q | xargs docker rmi -f 2>/dev/null
+docker image prune
 rm sit-jee-app.war
 
 echo "### end build_docker_image.sh for sit-jee-app ###"
-exit 0
