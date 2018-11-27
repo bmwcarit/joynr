280d14555e687642395bed5a394a4f74480b5332
Thomas Reuter <thomas.reuter@de.bertrandt.com>
2018-11-27 13:58:17 +0100

    [SIT] Do not sleep twice in sit-jee-stateless-consumer/start-me-up.sh
    Change-Id: I5ca839b2b87b4c6b1d63d9cd62aa3c26f826f16e

--- tests/system-integration-test/docker/sit-jee-stateless-consumer/start-me-up.sh
+++ tests/system-integration-test/docker/sit-jee-stateless-consumer/start-me-up.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Give the JEE Discovery Directory a chance to start ...
-sleep 30
-
 asadmin --interactive=false start-domain --debug --verbose &
 PID=$!
-# Wait grace period to let the server and database start up
-sleep 20
+
+# Give the JEE Discovery Directory a chance to start ...
+# and wait grace period to let the server and database start up
+sleep 30
 
 asadmin --interactive=false --user admin --passwordfile=/tmp/pwdfile deploy /sit-jee-stateless-consumer.war
 # Prevent script from exiting which would cause the container to terminate immediately
