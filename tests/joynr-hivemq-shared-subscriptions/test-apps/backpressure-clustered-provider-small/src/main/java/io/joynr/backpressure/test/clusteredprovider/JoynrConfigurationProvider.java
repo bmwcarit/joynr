ab490118617dd9a62037b3ad437f1741176760f1
Lars Brokmeier <Lars.Brokmeier@partner.bmw.de>
2018-12-03 16:54:15 +0100

    [SST] Add MQTT keystore and truststore properties for ssl
    Change-Id: Iddf3df8154a9021d3ab2db6fc72ddc8692a07361

--- tests/joynr-hivemq-shared-subscriptions/test-apps/backpressure-clustered-provider-small/src/main/java/io/joynr/backpressure/test/clusteredprovider/JoynrConfigurationProvider.java
+++ tests/joynr-hivemq-shared-subscriptions/test-apps/backpressure-clustered-provider-small/src/main/java/io/joynr/backpressure/test/clusteredprovider/JoynrConfigurationProvider.java
@@ -23,6 +23,9 @@ import java.util.Properties;
 import javax.ejb.Singleton;
 import javax.enterprise.inject.Produces;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.joynr.jeeintegration.api.JoynrLocalDomain;
 import io.joynr.jeeintegration.api.JoynrProperties;
 import io.joynr.messaging.ConfigurableMessagingSettings;
@@ -33,18 +36,33 @@ import io.joynr.messaging.mqtt.settings.LimitAndBackpressureSettings;
 @Singleton
 public class JoynrConfigurationProvider {
 
+    private static final Logger logger = LoggerFactory.getLogger(JoynrConfigurationProvider.class);
+
     @Produces
     @JoynrProperties
     public Properties joynrProperties() {
+
         Properties joynrProperties = new Properties();
         joynrProperties.setProperty(MessagingPropertyKeys.PROPERTY_SERVLET_CONTEXT_ROOT,
                                     "/backpressure-provider/messaging");
         joynrProperties.setProperty(MessagingPropertyKeys.PROPERTY_SERVLET_HOST_PATH,
                                     System.getenv("joynr_servlet_hostpath"));
         joynrProperties.setProperty(MessagingPropertyKeys.CHANNELID, "io.joynr.backpressure.test.clusteredprovider");
-        joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_BROKER_URI, System.getenv("MQTT_BROKER_URL"));
-        joynrProperties.setProperty(MessagingPropertyKeys.DISCOVERYDIRECTORYURL, System.getenv("MQTT_BROKER_URL"));
-        joynrProperties.setProperty(MessagingPropertyKeys.DOMAINACCESSCONTROLLERURL, System.getenv("MQTT_BROKER_URL"));
+
+        final String brokerUri = System.getenv("MQTT_BROKER_URL");
+        if (brokerUri == null) {
+            logger.error("Environment variable MQTT_BROKER_URL has not been set!");
+        } else {
+            logger.info("Environment variable MQTT_BROKER_URL has been set to: {}", brokerUri);
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_BROKER_URI, brokerUri);
+            joynrProperties.setProperty(MessagingPropertyKeys.DISCOVERYDIRECTORYURL, brokerUri);
+            joynrProperties.setProperty(MessagingPropertyKeys.DOMAINACCESSCONTROLLERURL, brokerUri);
+
+            // secure ssl connection
+            if (brokerUri.startsWith("ssl:")) {
+                setPropertiesForSecureMqttConnection(joynrProperties);
+            }
+        }
 
         joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_ENABLE_SHARED_SUBSCRIPTIONS, Boolean.TRUE.toString());
 
@@ -69,4 +87,40 @@ public class JoynrConfigurationProvider {
         return "io.joynr.backpressure.test.provider";
     }
 
+    private void setPropertiesForSecureMqttConnection(Properties joynrProperties) {
+        logger.debug("Using secure connection to the MQTT broker, setting keystore and truststore.");
+        final String keyStorePath = System.getenv("CLIENT_KEYSTORE");
+        final String keyStorePwd = System.getenv("KEYSTORE_PWD");
+        final String trustStorePath = System.getenv("CA_TRUSTSTORE");
+        final String trustStorePwd = System.getenv("TRUSTSTORE_PWD");
+
+        if (keyStorePath == null) {
+            logger.error("Environment variable CLIENT_KEYSTORE has not been set!");
+        } else {
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_KEYSTORE_PATH, keyStorePath);
+            logger.debug("Environment variable CLIENT_KEYSTORE has been set to: {}", keyStorePath);
+            if (keyStorePath.endsWith(".p12")) {
+                joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_KEYSTORE_TYPE, "PKCS12");
+            }
+        }
+        if (keyStorePwd == null) {
+            logger.error("Environment variable KEYSTORE_PWD has not been set!");
+        } else {
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_KEYSTORE_PWD, keyStorePwd);
+        }
+        if (trustStorePath == null) {
+            logger.error("Environment variable CA_TRUSTSTORE has not been set!");
+        } else {
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_TRUSTSTORE_PATH, trustStorePath);
+            logger.debug("Environment variable CA_TRUSTSTORE has been set to: {}", trustStorePath);
+            if (trustStorePath.endsWith(".p12")) {
+                joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_TRUSTSTORE_TYPE, "PKCS12");
+            }
+        }
+        if (trustStorePwd == null) {
+            logger.error("Environment variable TRUSTSTORE_PWD has not been set!");
+        } else {
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_TRUSTSTORE_PWD, trustStorePwd);
+        }
+    }
 }
--- tests/joynr-hivemq-shared-subscriptions/test-apps/backpressure-monitor-app/src/main/java/io/joynr/backpressure/test/monitorapp/JoynrConfigurationProvider.java
+++ tests/joynr-hivemq-shared-subscriptions/test-apps/backpressure-monitor-app/src/main/java/io/joynr/backpressure/test/monitorapp/JoynrConfigurationProvider.java
@@ -23,6 +23,9 @@ import java.util.Properties;
 import javax.ejb.Singleton;
 import javax.enterprise.inject.Produces;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.joynr.jeeintegration.api.JoynrLocalDomain;
 import io.joynr.jeeintegration.api.JoynrProperties;
 import io.joynr.messaging.MessagingPropertyKeys;
@@ -31,19 +34,34 @@ import io.joynr.messaging.mqtt.MqttModule;
 @Singleton
 public class JoynrConfigurationProvider {
 
+    private static final Logger logger = LoggerFactory.getLogger(JoynrConfigurationProvider.class);
+
     @Produces
     @JoynrProperties
     public Properties joynrProperties() {
+
         Properties joynrProperties = new Properties();
         joynrProperties.setProperty(MessagingPropertyKeys.PROPERTY_SERVLET_CONTEXT_ROOT,
                                     "/backpressure-monitor-app/messaging");
         joynrProperties.setProperty(MessagingPropertyKeys.PROPERTY_SERVLET_HOST_PATH,
                                     "http://backpressure-monitor-app:8080");
         joynrProperties.setProperty(MessagingPropertyKeys.CHANNELID, "io.joynr.backpressure.test.monitorapp");
-        joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_BROKER_URI, System.getenv("MQTT_BROKER_URL"));
         joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_MAX_MSGS_INFLIGHT, "100");
-        joynrProperties.setProperty(MessagingPropertyKeys.DISCOVERYDIRECTORYURL, System.getenv("MQTT_BROKER_URL"));
-        joynrProperties.setProperty(MessagingPropertyKeys.DOMAINACCESSCONTROLLERURL, System.getenv("MQTT_BROKER_URL"));
+
+        final String brokerUri = System.getenv("MQTT_BROKER_URL");
+        if (brokerUri == null) {
+            logger.error("Environment variable MQTT_BROKER_URL has not been set!");
+        } else {
+            logger.info("Environment variable MQTT_BROKER_URL has been set to: {}", brokerUri);
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_BROKER_URI, brokerUri);
+            joynrProperties.setProperty(MessagingPropertyKeys.DISCOVERYDIRECTORYURL, brokerUri);
+            joynrProperties.setProperty(MessagingPropertyKeys.DOMAINACCESSCONTROLLERURL, brokerUri);
+
+            // secure ssl connection
+            if (brokerUri.startsWith("ssl:")) {
+                setPropertiesForSecureMqttConnection(joynrProperties);
+            }
+        }
 
         return joynrProperties;
     }
@@ -54,4 +72,40 @@ public class JoynrConfigurationProvider {
         return "io.joynr.backpressure.test.monitorapp";
     }
 
+    private void setPropertiesForSecureMqttConnection(Properties joynrProperties) {
+        logger.debug("Using secure connection to the MQTT broker, setting keystore and truststore.");
+        final String keyStorePath = System.getenv("CLIENT_KEYSTORE");
+        final String keyStorePwd = System.getenv("KEYSTORE_PWD");
+        final String trustStorePath = System.getenv("CA_TRUSTSTORE");
+        final String trustStorePwd = System.getenv("TRUSTSTORE_PWD");
+
+        if (keyStorePath == null) {
+            logger.error("Environment variable CLIENT_KEYSTORE has not been set!");
+        } else {
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_KEYSTORE_PATH, keyStorePath);
+            logger.debug("Environment variable CLIENT_KEYSTORE has been set to: {}", keyStorePath);
+            if (keyStorePath.endsWith(".p12")) {
+                joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_KEYSTORE_TYPE, "PKCS12");
+            }
+        }
+        if (keyStorePwd == null) {
+            logger.error("Environment variable KEYSTORE_PWD has not been set!");
+        } else {
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_KEYSTORE_PWD, keyStorePwd);
+        }
+        if (trustStorePath == null) {
+            logger.error("Environment variable CA_TRUSTSTORE has not been set!");
+        } else {
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_TRUSTSTORE_PATH, trustStorePath);
+            logger.debug("Environment variable CA_TRUSTSTORE has been set to: {}", trustStorePath);
+            if (trustStorePath.endsWith(".p12")) {
+                joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_TRUSTSTORE_TYPE, "PKCS12");
+            }
+        }
+        if (trustStorePwd == null) {
+            logger.error("Environment variable TRUSTSTORE_PWD has not been set!");
+        } else {
+            joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_TRUSTSTORE_PWD, trustStorePwd);
+        }
+    }
 }
