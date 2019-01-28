8ac4df09b475b02ed4daf019a74746c20140e625
Thomas Reuter <thomas.reuter@de.bertrandt.com>
2019-01-28 15:57:22 +0100

    [Java] Adapt MultipleVersionsEnd2EndTest for abstract super class

--- java/integration-tests/src/test/java/io/joynr/integration/MultipleVersionsEnd2EndTest.java
+++ java/integration-tests/src/test/java/io/joynr/integration/MultipleVersionsEnd2EndTest.java
@@ -22,6 +22,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
@@ -43,18 +47,20 @@ import joynr.tests.MultipleVersionsInterface2Proxy;
 import joynr.tests.v2.MultipleVersionsInterfaceProxy;
 
 public class MultipleVersionsEnd2EndTest extends AbstractMultipleVersionsEnd2EndTest {
-    private static final String DOMAIN = "MultipleVersionsTestDomain";
+    private static final String DOMAIN_PREFIX = "MultipleVersionsTestDomain-";
     private static final String PROXYBUILD_FAILED_MESSAGE = "Building of proxy failed: ";
     private static final String REGISTERING_FAILED_MESSAGE = "Registering of provider failed: ";
     private static final String UNREGISTERING_FAILED_MESSAGE = "Unregistering of provider failed: ";
 
-    private Semaphore proxyBuilt;
+    private Semaphore proxyBuiltSemaphore;
+    private String domain;
 
     @Override
     @Before
     public void setUp() {
         super.setUp();
-        proxyBuilt = new Semaphore(0);
+        domain = DOMAIN_PREFIX + UUID.randomUUID().toString();
+        proxyBuiltSemaphore = new Semaphore(0);
     }
 
     @After
@@ -67,9 +73,10 @@ public class MultipleVersionsEnd2EndTest extends AbstractMultipleVersionsEnd2End
     /**
      * Registers given provider in runtime.
      * @param provider
+     * @param domain
      */
-    private void asyncRegisterProvider(AbstractJoynrProvider provider) {
-        Future<Void> future = runtime.registerProvider(DOMAIN, provider, providerQos);
+    private void registerProvider(AbstractJoynrProvider provider, String domain) {
+        Future<Void> future = runtime.registerProvider(domain, provider, providerQos);
 
         try {
             future.get(100);
@@ -83,14 +90,14 @@ public class MultipleVersionsEnd2EndTest extends AbstractMultipleVersionsEnd2End
      * @param interfaceClass Interface of the proxy to build.
      * @return The built proxy.
      */
-    private <T> T asyncBuildProxyForInterface(final Class<T> interfaceClass) {
-        ProxyBuilder<T> proxyBuilder = runtime.getProxyBuilder(DOMAIN, interfaceClass);
+    private <T> T buildProxy(final Class<T> interfaceClass, final Set<String> domains, final boolean waitForProxyCreation) throws Exception {
+        ProxyBuilder<T> proxyBuilder = runtime.getProxyBuilder(domains, interfaceClass);
         T proxy = null;
         try {
             proxy = proxyBuilder.setDiscoveryQos(discoveryQos).build(new ProxyCreatedCallback<T>() {
                 @Override
                 public void onProxyCreationFinished(T result) {
-                    proxyBuilt.release();
+                    proxyBuiltSemaphore.release();
                 }
 
                 @Override
@@ -98,8 +105,13 @@ public class MultipleVersionsEnd2EndTest extends AbstractMultipleVersionsEnd2End
                     throw error;
                 }
             });
-            assertTrue(proxyBuilt.tryAcquire(1, TimeUnit.SECONDS));
+            if (waitForProxyCreation) {
+                assertTrue(proxyBuiltSemaphore.tryAcquire(1, TimeUnit.SECONDS));
+            }
         } catch (DiscoveryException | InterruptedException e) {
+            if (!waitForProxyCreation) {
+                throw e;
+            }
             fail(PROXYBUILD_FAILED_MESSAGE + e);
         }
         return proxy;
@@ -110,15 +122,15 @@ public class MultipleVersionsEnd2EndTest extends AbstractMultipleVersionsEnd2End
     * one provider (unversioned) and communicate with this without mutual interference.
     */
     @Test
-    public void twoProxiesOfDifferentVersioningTypesVsUnversionedProvider() {
+    public void twoProxiesOfDifferentVersioningTypesVsUnversionedProvider() throws Exception {
         // register provider
         DefaultMultipleVersionsInterfaceProvider provider = new DefaultMultipleVersionsInterfaceProvider();
 
-        asyncRegisterProvider(provider);
+        registerProvider(provider, domain);
 
         // build fitting proxies
-        MultipleVersionsInterface2Proxy proxy1 = asyncBuildProxyForInterface(MultipleVersionsInterface2Proxy.class);
-        MultipleVersionsInterfaceProxy proxy2 = asyncBuildProxyForInterface(MultipleVersionsInterfaceProxy.class);
+        MultipleVersionsInterface2Proxy proxy1 = buildProxy(MultipleVersionsInterface2Proxy.class, new HashSet<String>(Arrays.asList(domain)), true);
+        MultipleVersionsInterfaceProxy proxy2 = buildProxy(MultipleVersionsInterfaceProxy.class, new HashSet<String>(Arrays.asList(domain)), true);
 
         try {
             //set UInt8Attribute1 and check if it can be retrieved correctly
@@ -135,7 +147,7 @@ public class MultipleVersionsEnd2EndTest extends AbstractMultipleVersionsEnd2End
             assertEquals((byte) value2, 50);
 
             // unregister provider
-            runtime.unregisterProvider(DOMAIN, provider);
+            runtime.unregisterProvider(domain, provider);
         } catch (JoynrRuntimeException e) {
             fail(UNREGISTERING_FAILED_MESSAGE + e);
         }
@@ -146,17 +158,17 @@ public class MultipleVersionsEnd2EndTest extends AbstractMultipleVersionsEnd2End
      * and 2 proxies can communicate with those without mutual interference.
      */
     @Test
-    public void twoNameVersionedProvidersInSingleRuntime() {
+    public void twoNameVersionedProvidersInSingleRuntime() throws Exception {
         // register providers
         DefaultMultipleVersionsInterface1Provider provider1 = new DefaultMultipleVersionsInterface1Provider();
         DefaultMultipleVersionsInterface2Provider provider2 = new DefaultMultipleVersionsInterface2Provider();
 
-        asyncRegisterProvider(provider1);
-        asyncRegisterProvider(provider2);
+        registerProvider(provider1, domain);
+        registerProvider(provider2, domain);
 
         // build fitting proxies
-        MultipleVersionsInterface1Proxy proxy1 = asyncBuildProxyForInterface(MultipleVersionsInterface1Proxy.class);
-        MultipleVersionsInterface2Proxy proxy2 = asyncBuildProxyForInterface(MultipleVersionsInterface2Proxy.class);
+        MultipleVersionsInterface1Proxy proxy1 = buildProxy(MultipleVersionsInterface1Proxy.class, new HashSet<String>(Arrays.asList(domain)), true);
+        MultipleVersionsInterface2Proxy proxy2 = buildProxy(MultipleVersionsInterface2Proxy.class, new HashSet<String>(Arrays.asList(domain)), true);
 
         //set UInt8Attribute1 and check if it can be retrieved correctly
         proxy1.setUInt8Attribute1((byte) 100);
@@ -168,8 +180,8 @@ public class MultipleVersionsEnd2EndTest extends AbstractMultipleVersionsEnd2End
 
         // unregister providers
         try {
-            runtime.unregisterProvider(DOMAIN, provider1);
-            runtime.unregisterProvider(DOMAIN, provider2);
+            runtime.unregisterProvider(domain, provider1);
+            runtime.unregisterProvider(domain, provider2);
         } catch (JoynrRuntimeException e) {
             fail(UNREGISTERING_FAILED_MESSAGE + e);
         }
