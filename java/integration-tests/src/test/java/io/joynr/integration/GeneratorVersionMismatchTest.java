313770a1c0f2842c2251a9e1113199fa2ac95510
Thomas Reuter <thomas.reuter@de.bertrandt.com>
2019-01-28 15:09:03 +0100

    [Java] Adapt GeneratorVersionMismatchTest for abstract super class

--- java/integration-tests/src/test/java/io/joynr/integration/GeneratorVersionMismatchTest.java
+++ java/integration-tests/src/test/java/io/joynr/integration/GeneratorVersionMismatchTest.java
@@ -32,75 +32,96 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import io.joynr.exceptions.DiscoveryException;
 import io.joynr.exceptions.JoynrRuntimeException;
 import io.joynr.exceptions.MultiDomainNoCompatibleProviderFoundException;
 import io.joynr.exceptions.NoCompatibleProviderFoundException;
+import io.joynr.provider.AbstractJoynrProvider;
+import io.joynr.proxy.Future;
 import io.joynr.proxy.ProxyBuilder;
 import io.joynr.proxy.ProxyBuilder.ProxyCreatedCallback;
 import joynr.tests.v1.DefaultMultipleVersionsInterfaceProvider;
 
 public class GeneratorVersionMismatchTest extends AbstractMultipleVersionsEnd2EndTest {
     private static final long CONST_DEFAULT_TEST_TIMEOUT_MS = 3000;
+    private static final String DOMAIN_PREFIX = "MultipleVersionsTestDomain-";
+    private static final String PROXYBUILD_FAILED_MESSAGE = "Building of proxy failed: ";
+    private static final String REGISTERING_FAILED_MESSAGE = "Registering of provider failed: ";
 
-    private Semaphore errorCallbackSemaphore;
+    private Semaphore proxyBuiltSemaphore;
+    private Semaphore noCompatibleProviderFoundCallbackSemaphore;
     private String domain;
     private String domain2;
     private joynr.tests.v1.DefaultMultipleVersionsInterfaceProvider provider;
-    private ProxyCreatedCallback<joynr.tests.v2.MultipleVersionsInterfaceProxy> callback;
     private joynr.tests.v2.MultipleVersionsInterfaceProxy proxy;
 
     @Before
     public void setUp() {
         super.setUp();
+        domain = DOMAIN_PREFIX + UUID.randomUUID().toString();
+        proxyBuiltSemaphore = new Semaphore(0);
+        noCompatibleProviderFoundCallbackSemaphore = new Semaphore(0, true);
 
-        // the error callback will make a permit available.
-        // The test will wait until a permit is available, or fail.
-        errorCallbackSemaphore = new Semaphore(0, true);
-        domain = "domain-" + UUID.randomUUID().toString();
         domain2 = "domain2-" + UUID.randomUUID().toString();
-
         provider = new DefaultMultipleVersionsInterfaceProvider();
-        runtime.registerProvider(domain, provider, providerQos);
-        runtime.registerProvider(domain2, provider, providerQos);
 
-        callback = new ProxyCreatedCallback<joynr.tests.v2.MultipleVersionsInterfaceProxy>() {
-
-            @Override
-            public void onProxyCreationFinished(joynr.tests.v2.MultipleVersionsInterfaceProxy result) {
-                // Fail doesn't work here, so just do nothing
-            }
-
-            @Override
-            public void onProxyCreationError(JoynrRuntimeException error) {
-                if (error instanceof NoCompatibleProviderFoundException
-                        || error instanceof MultiDomainNoCompatibleProviderFoundException) {
-                    errorCallbackSemaphore.release();
-                }
-            }
-        };
+        registerProvider(provider, domain);
+        registerProvider(provider, domain2);
     }
 
     @After
     public void tearDown() {
+        runtime.unregisterProvider(domain, provider);
+        runtime.unregisterProvider(domain2, provider);
+
         if (runtime != null) {
             runtime.shutdown(true);
         }
     }
+    private void registerProvider(AbstractJoynrProvider provider, String domain) {
+        Future<Void> future = runtime.registerProvider(domain, provider, providerQos);
 
-    private void createProxy(final Set<String> domains, boolean async) {
-        ProxyBuilder<joynr.tests.v2.MultipleVersionsInterfaceProxy> proxyBuilder = runtime.getProxyBuilder(domains,
-                                                                                                           joynr.tests.v2.MultipleVersionsInterfaceProxy.class);
-        if (async) {
-            proxy = proxyBuilder.setDiscoveryQos(discoveryQos).build(callback);
-        } else {
-            proxy = proxyBuilder.setDiscoveryQos(discoveryQos).build();
+        try {
+            future.get(100);
+        } catch (Exception e) {
+            fail(REGISTERING_FAILED_MESSAGE + e);
         }
+    }
+
+    private <T> T buildProxy(final Class<T> interfaceClass, final Set<String> domains, final boolean waitForProxyCreation) throws Exception {
+        ProxyBuilder<T> proxyBuilder = runtime.getProxyBuilder(domains, interfaceClass);
+        T proxy = null;
+        try {
+            proxy = proxyBuilder.setDiscoveryQos(discoveryQos).build(new ProxyCreatedCallback<T>() {
+                @Override
+                public void onProxyCreationFinished(T result) {
+                    proxyBuiltSemaphore.release();
+                }
+    
+                @Override
+                public void onProxyCreationError(JoynrRuntimeException error) {
+                    if (error instanceof NoCompatibleProviderFoundException
+                            || error instanceof MultiDomainNoCompatibleProviderFoundException) {
+                        noCompatibleProviderFoundCallbackSemaphore.release();
+                    }
+                }
+            });
+            if (waitForProxyCreation) {
+                assertTrue(proxyBuiltSemaphore.tryAcquire(1, TimeUnit.SECONDS));
+            }
+        } catch (DiscoveryException | InterruptedException e) {
+            if (!waitForProxyCreation) {
+                throw e;
+            }
+            fail(PROXYBUILD_FAILED_MESSAGE + e);
+        }
+        return proxy;
     }
 
     private void checkProxy() {
         try {
             proxy.getTrue();
-            fail("Proxy call didn't cause an discovery exception");
+            fail("Proxy call didn't cause a discovery exception");
         } catch (NoCompatibleProviderFoundException | MultiDomainNoCompatibleProviderFoundException e) {
             // These exceptions are expected, so no need to fail here.
         } catch (Exception e) {
@@ -111,11 +132,11 @@ public class GeneratorVersionMismatchTest extends AbstractMultipleVersionsEnd2En
     @Test(timeout = CONST_DEFAULT_TEST_TIMEOUT_MS)
     public void testNoCompatibleProviderFound() throws Exception {
 
-        createProxy(new HashSet<String>(Arrays.asList(domain)), true);
+        proxy = buildProxy(joynr.tests.v2.MultipleVersionsInterfaceProxy.class, new HashSet<String>(Arrays.asList(domain)), false);
 
         // wait for the proxy created error callback to be called
         assertTrue("Unexpected successful proxy creation or timeout",
-                   errorCallbackSemaphore.tryAcquire(3, TimeUnit.SECONDS));
+                   noCompatibleProviderFoundCallbackSemaphore.tryAcquire(3, TimeUnit.SECONDS));
 
         checkProxy();
     }
@@ -123,11 +144,11 @@ public class GeneratorVersionMismatchTest extends AbstractMultipleVersionsEnd2En
     @Test(timeout = CONST_DEFAULT_TEST_TIMEOUT_MS)
     public void testMultiDomainNoCompatibleProviderFound() throws Exception {
 
-        createProxy(new HashSet<String>(Arrays.asList(domain, domain2)), true);
+        proxy = buildProxy(joynr.tests.v2.MultipleVersionsInterfaceProxy.class, new HashSet<String>(Arrays.asList(domain, domain2)), false);
 
         // wait for the proxy created error callback to be called
         assertTrue("Unexpected successful proxy creation or timeout",
-                   errorCallbackSemaphore.tryAcquire(3, TimeUnit.SECONDS));
+                   noCompatibleProviderFoundCallbackSemaphore.tryAcquire(3, TimeUnit.SECONDS));
 
         checkProxy();
     }
@@ -135,7 +156,7 @@ public class GeneratorVersionMismatchTest extends AbstractMultipleVersionsEnd2En
     @Test(timeout = CONST_DEFAULT_TEST_TIMEOUT_MS)
     public void testProxyIsInvalidatedOnceArbitrationExceptionThrown() throws Exception {
 
-        createProxy(new HashSet<String>(Arrays.asList(domain)), false);
+        proxy = buildProxy(joynr.tests.v2.MultipleVersionsInterfaceProxy.class, new HashSet<String>(Arrays.asList(domain)), false);
 
         checkProxy();
         checkProxy();
