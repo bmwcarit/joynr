d1d674b6ea4e75a3374adc7f2eaf1f8411c801a9
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2019-06-05 09:11:14 +0000

    [JS] Rewrite ProxyEvent to TS

---
+++ javascript/libjoynr-js/src/main/js/joynr/proxy/ProxyEvent.ts
@@ -0,0 +1,168 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2019 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+import BroadcastFilterParameters from "./BroadcastFilterParameters";
+
+import * as SubscriptionUtil from "../dispatching/subscription/util/SubscriptionUtil";
+import MessagingQos from "../messaging/MessagingQos";
+import SubscriptionManager from "../dispatching/subscription/SubscriptionManager";
+import SubscriptionQos from "./SubscriptionQos";
+import DiscoveryQos = require("./DiscoveryQos");
+
+/**
+ * Checks if the given datatypes and values match the given broadcast parameters
+ *
+ * @param unnamedBroadcastValues an array containing the unnamedBroadcastValues, e.g. [1234, "asdf"]
+ * @param broadcastParameter an array of supported parametes
+ * @returns undefined if unnamedBroadcastValues does not match broadcastSignature
+ */
+function getNamedParameters(unnamedBroadcastValues: any[], broadcastParameter: any[]): Record<string, any> | undefined {
+    const namedParameters: Record<string, any> = {};
+
+    // check if number of given parameters matches number
+    // of parameters in broadcast signature (keys.length)
+    if (unnamedBroadcastValues.length !== broadcastParameter.length) {
+        return undefined;
+    }
+
+    // cycle over all parameters
+    for (let i = 0; i < unnamedBroadcastValues.length; ++i) {
+        const parameter = broadcastParameter[i];
+        namedParameters[parameter.name] = unnamedBroadcastValues[i];
+    }
+
+    return namedParameters;
+}
+
+class ProxyEvent {
+    private parent: any;
+    private settings: any;
+    /**
+     * Constructor of ProxyEvent object that is used in the generation of proxy objects
+     *
+     * @constructor
+     * @param parent is the proxy object that contains this attribute
+     * @param settings the settings for this broadcast proxy
+     * @param settings.discoveryQos the Quality of Service parameters for arbitration
+     * @param settings.messagingQos the Quality of Service parameters for messaging
+     * @param settings.dependencies the dependencies object for this function call
+     * @param settings.dependencies.subscriptionManager
+     * @param settings.selective true if the broadcast is selective
+     * @param settings.broadcastName the name of the broadcast as modelled in Franca
+     * @param settings.broadcastParameter the parameter meta information of the broadcast being subscribed to
+     * @param settings.filterParameters the filter parameters of the broadcast
+     * @returns {ProxyEvent} */
+    public constructor(
+        parent: any,
+        settings: {
+            discoveryQos: DiscoveryQos;
+            messagingQos: MessagingQos;
+            dependencies: { subscriptionManager: SubscriptionManager };
+            selective?: boolean;
+            broadcastName: string;
+            broadcastParameter: { name: string; type: string }[];
+            filterParameters?: Record<string, any>;
+        }
+    ) {
+        this.settings = settings;
+        this.parent = parent;
+    }
+
+    /**
+     * @param subscribeParameters the settings object for this function call
+     * @param subscribeParameters.subscriptionQos the subscription quality of service object
+     * @param [subscribeParameters.subscriptionId] optional subscriptionId. Used to refresh or
+     *            reinstate an existing subscription.
+     * @param [subscribeParameters.partitions] optional parameter for multicast subscriptions.
+     *            This parameter becomes relevant for non selective broadcasts and specifies the interested partitions
+     *            of publications. It is interpreted hierarchically.
+     * @param subscribeParameters.onReceive this function is called when an event as been
+     *            received. method signature: "void onReceive({?}value)"
+     * @param subscribeParameters.onError this function is called when an error occurs with
+     *            a subscribed event. method signature: "void onError({Error} error)"
+     * @param subscribeParameters.onSubscribed the callback to inform once the subscription request has
+     *            been delivered successfully
+     * @returns returns a promise that is resolved with the subscriptionId, which is to
+     *          be used to unsubscribe from this subscription later.
+     * @throws {Error} if subscribeParameters.partitions contains invalid characters
+     */
+    public subscribe(subscribeParameters: {
+        subscriptionQos: SubscriptionQos;
+        subscriptionId?: string;
+        partitions?: string[];
+        onReceive: Function;
+        onError?: (error: any) => void;
+        onSubscribed?: Function;
+        filterParameters?: any;
+    }): Promise<string> {
+        SubscriptionUtil.validatePartitions(subscribeParameters.partitions);
+        if (subscribeParameters.filterParameters !== undefined && subscribeParameters.filterParameters !== null) {
+            const checkResult = SubscriptionUtil.checkFilterParameters(
+                this.settings.filterParameters,
+                subscribeParameters.filterParameters.filterParameters,
+                this.settings.broadcastName
+            );
+            if (checkResult.caughtErrors.length !== 0) {
+                const errorMessage = JSON.stringify(checkResult.caughtErrors);
+                return Promise.reject(
+                    new Error(
+                        `SubscriptionRequest could not be processed, as the filterParameters "${JSON.stringify(
+                            subscribeParameters.filterParameters
+                        )}" are wrong: ${errorMessage}`
+                    )
+                );
+            }
+        }
+        return this.settings.dependencies.subscriptionManager.registerBroadcastSubscription({
+            proxyId: this.parent.proxyParticipantId,
+            providerDiscoveryEntry: this.parent.providerDiscoveryEntry,
+            broadcastName: this.settings.broadcastName,
+            broadcastParameter: this.settings.broadcastParameter,
+            subscriptionQos: subscribeParameters.subscriptionQos,
+            subscriptionId: subscribeParameters.subscriptionId,
+            onReceive: (response: any) => {
+                subscribeParameters.onReceive(getNamedParameters(response, this.settings.broadcastParameter));
+            },
+            selective: this.settings.selective,
+            partitions: subscribeParameters.partitions || [],
+            onError: subscribeParameters.onError,
+            onSubscribed: subscribeParameters.onSubscribed,
+            filterParameters: subscribeParameters.filterParameters
+        });
+    }
+
+    public createFilterParameters(): BroadcastFilterParameters {
+        return new BroadcastFilterParameters(this.settings.filterParameters);
+    }
+
+    /**
+     * @param unsubscribeParameters the settings object for this function call
+     * @param unsubscribeParameters.subscriptionId the subscription token retrieved from the
+     *            subscribe function
+     * @returns returns a promise that is resolved when unsubscribe has been executed.
+     * @see ProxyEvent#subscribe
+     */
+    public unsubscribe(unsubscribeParameters: { subscriptionId: string }): Promise<void> {
+        return this.settings.dependencies.subscriptionManager.unregisterSubscription({
+            messagingQos: this.settings.messagingQos,
+            subscriptionId: unsubscribeParameters.subscriptionId
+        });
+    }
+}
+
+export = ProxyEvent;
---
+++ javascript/libjoynr-js/src/test/js/joynr/proxy/ProxyEventTest.ts
@@ -0,0 +1,291 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2017 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+import ProxyEvent from "../../../../main/js/joynr/proxy/ProxyEvent";
+
+import DiscoveryQos from "../../../../main/js/joynr/proxy/DiscoveryQos";
+import MessagingQos from "../../../../main/js/joynr/messaging/MessagingQos";
+import MulticastSubscriptionQos from "../../../../main/js/joynr/proxy/MulticastSubscriptionQos";
+import DiscoveryEntryWithMetaInfo from "../../../../main/js/generated/joynr/types/DiscoveryEntryWithMetaInfo";
+import Version from "../../../../main/js/generated/joynr/types/Version";
+import ProviderQos from "../../../../main/js/generated/joynr/types/ProviderQos";
+import * as UtilInternal from "../../../../main/js/joynr/util/UtilInternal";
+
+describe("libjoynr-js.joynr.proxy.ProxyEvent", () => {
+    let weakSignal: any, broadcastWithoutFilterParameters: any;
+    let subscriptionId: any;
+    const subscriptionQos = new MulticastSubscriptionQos();
+    let subscriptionManagerSpy: any;
+    let proxyParticipantId: any;
+    let providerDiscoveryEntry: any;
+    let broadcastName: string;
+
+    beforeEach(done => {
+        proxyParticipantId = "proxyParticipantId";
+        providerDiscoveryEntry = new DiscoveryEntryWithMetaInfo({
+            providerVersion: new Version({ majorVersion: 0, minorVersion: 23 }),
+            domain: "testProviderDomain",
+            interfaceName: "interfaceName",
+            participantId: "providerParticipantId",
+            qos: new ProviderQos({} as any),
+            lastSeenDateMs: Date.now(),
+            expiryDateMs: Date.now() + 60000,
+            publicKeyId: "publicKeyId",
+            isLocal: true
+        });
+        const fakeProxy = {
+            proxyParticipantId,
+            providerDiscoveryEntry
+        };
+
+        subscriptionManagerSpy = {
+            registerBroadcastSubscription: jest.fn(),
+            unregisterSubscription: jest.fn()
+        };
+        subscriptionId = "subscriptionId";
+
+        subscriptionManagerSpy.registerBroadcastSubscription.mockReturnValue(Promise.resolve(subscriptionId));
+        subscriptionManagerSpy.unregisterSubscription.mockReturnValue(Promise.resolve());
+
+        broadcastName = "weakSignal";
+        weakSignal = new ProxyEvent(fakeProxy, {
+            broadcastName,
+            discoveryQos: new DiscoveryQos(),
+            messagingQos: new MessagingQos(),
+            dependencies: {
+                subscriptionManager: subscriptionManagerSpy
+            },
+            selective: true,
+            filterParameters: {
+                a: "reservedForTypeInfo",
+                b: "reservedForTypeInfo",
+                c: "reservedForTypeInfo"
+            },
+            broadcastParameter: []
+        });
+
+        broadcastWithoutFilterParameters = new ProxyEvent(fakeProxy, {
+            broadcastName,
+            discoveryQos: new DiscoveryQos(),
+            messagingQos: new MessagingQos(),
+            dependencies: {
+                subscriptionManager: subscriptionManagerSpy
+            },
+            broadcastParameter: []
+        });
+
+        done();
+    });
+
+    it("selective broadcast without filter parameters works", done => {
+        const filterParameterValues = { a: "valueForA" };
+
+        const broadcastFilterParameters = broadcastWithoutFilterParameters.createFilterParameters();
+        broadcastFilterParameters.filterParameters = filterParameterValues;
+        expect(broadcastFilterParameters.filterParameters).toEqual(filterParameterValues);
+        done();
+    });
+
+    it("is of correct type", done => {
+        expect(weakSignal).toBeDefined();
+        expect(weakSignal).not.toBeNull();
+        expect(typeof weakSignal === "object").toBeTruthy();
+        expect(weakSignal instanceof ProxyEvent).toBeTruthy();
+        done();
+    });
+
+    it("has correct members", () => {
+        expect(weakSignal.subscribe).toBeDefined();
+        expect(weakSignal.unsubscribe).toBeDefined();
+    });
+
+    it("subscribe calls subscriptionManager", async () => {
+        const partitions = ["1", "2", "3"];
+        const onReceive = function() {};
+        const onError = function() {};
+        const onSubscribed = function() {};
+
+        const expectedPartitions = UtilInternal.extend([], partitions);
+        const expectedSubscriptionQos = new MulticastSubscriptionQos(UtilInternal.extendDeep({}, subscriptionQos));
+        let expectedDiscoveryEntry: any = UtilInternal.extendDeep({}, providerDiscoveryEntry);
+        expectedDiscoveryEntry.providerVersion = new Version(expectedDiscoveryEntry.providerVersion);
+        expectedDiscoveryEntry.qos = new ProviderQos(expectedDiscoveryEntry.qos);
+        expectedDiscoveryEntry = new DiscoveryEntryWithMetaInfo(expectedDiscoveryEntry);
+
+        await weakSignal.subscribe({
+            subscriptionQos,
+            partitions,
+            onReceive,
+            onError,
+            onSubscribed
+        });
+
+        expect(subscriptionManagerSpy.registerBroadcastSubscription).toHaveBeenCalled();
+        expect(subscriptionManagerSpy.registerBroadcastSubscription.mock.calls[0][0].proxyId).toEqual(
+            proxyParticipantId
+        );
+        expect(subscriptionManagerSpy.registerBroadcastSubscription.mock.calls[0][0].providerDiscoveryEntry).toEqual(
+            expectedDiscoveryEntry
+        );
+        expect(subscriptionManagerSpy.registerBroadcastSubscription.mock.calls[0][0].broadcastName).toBe(broadcastName);
+        expect(subscriptionManagerSpy.registerBroadcastSubscription.mock.calls[0][0].subscriptionQos).toEqual(
+            expect.objectContaining({
+                _typeName: expectedSubscriptionQos._typeName,
+                expiryDateMs: expectedSubscriptionQos.expiryDateMs
+            })
+        );
+        expect(subscriptionManagerSpy.registerBroadcastSubscription.mock.calls[0][0].subscriptionId).toBeUndefined();
+        expect(subscriptionManagerSpy.registerBroadcastSubscription.mock.calls[0][0].selective).toEqual(true);
+        expect(subscriptionManagerSpy.registerBroadcastSubscription.mock.calls[0][0].partitions).toEqual(
+            expectedPartitions
+        );
+        expect(subscriptionManagerSpy.registerBroadcastSubscription.mock.calls[0][0].onError).toBe(onError);
+        expect(subscriptionManagerSpy.registerBroadcastSubscription.mock.calls[0][0].onSubscribed).toBe(onSubscribed);
+    });
+
+    it("subscribe provides a subscriptionId", async () => {
+        const passedId = await weakSignal.subscribe({
+            subscriptionQos,
+            onReceive() {}
+        });
+        expect(passedId).toBeDefined();
+        expect(typeof passedId === "string").toBeTruthy();
+    });
+
+    it("subscribe and unsubscribe notify on success", async () => {
+        // precondition: the broadcast object has already been created
+        expect(weakSignal.subscribe).toBeDefined();
+        expect(typeof weakSignal.subscribe === "function").toBeTruthy();
+        expect(weakSignal.unsubscribe).toBeDefined();
+        expect(typeof weakSignal.unsubscribe === "function").toBeTruthy();
+
+        const passedSubscriptionId = await weakSignal.subscribe({
+            subscriptionQos,
+            receive() {}
+        });
+        return weakSignal.unsubscribe({
+            subscriptionId: passedSubscriptionId
+        });
+    });
+
+    it("subscribe rejects if filter parameters are only partially filled", done => {
+        subscriptionManagerSpy.registerBroadcastSubscription.mockReturnValue(Promise.resolve());
+
+        //subscribing without filter parameters should work
+        weakSignal
+            .subscribe({
+                subscriptionQos,
+                receive() {}
+            })
+            .then(() => {
+                //subscribing with empty filter parameters should work
+                return weakSignal.subscribe({
+                    subscriptionQos,
+                    receive() {},
+                    filterParameters: weakSignal.createFilterParameters()
+                });
+            })
+            .then(() => {
+                //subscribing with filter parameters having value null should work
+                return weakSignal.subscribe({
+                    subscriptionQos,
+                    receive() {}
+                });
+            })
+            .then(() => {
+                //subscribing with filter parameters having value null should work
+                return weakSignal.subscribe({
+                    subscriptionQos,
+                    receive() {},
+                    filterParameters: null
+                });
+            })
+            .then(() => {
+                //subscribing with partially defined filter parameters should fail
+                const filterParameters = weakSignal.createFilterParameters();
+                filterParameters.setA("a");
+                filterParameters.setB("b");
+                //do not set filter paramter "c", so assuming an error during subscribe
+                return weakSignal
+                    .subscribe({
+                        subscriptionQos,
+                        receive() {},
+                        filterParameters
+                    })
+                    .then(() => done.fail())
+                    .catch(() => done());
+            })
+            .catch(() => done.fail());
+    });
+
+    it("subscribe notifies on failure", done => {
+        // precondition: the broadcast object has already been created
+        expect(weakSignal.subscribe).toBeDefined();
+        expect(typeof weakSignal.subscribe === "function").toBeTruthy();
+
+        const expectedError = new Error("error registering broadcast");
+        subscriptionManagerSpy.registerBroadcastSubscription.mockReturnValue(Promise.reject(expectedError));
+
+        weakSignal
+            .subscribe({
+                subscriptionQos,
+                receive() {}
+            })
+            .then(() => done.fail())
+            .catch((error: any) => {
+                expect(error).toEqual(expectedError);
+                done();
+            })
+            .catch(() => done.fail());
+    });
+
+    it("unsubscribe notifies on failure", async () => {
+        // precondition: the broadcast object has already been created
+        expect(weakSignal.subscribe).toBeDefined();
+        expect(typeof weakSignal.subscribe === "function").toBeTruthy();
+        expect(weakSignal.unsubscribe).toBeDefined();
+        expect(typeof weakSignal.unsubscribe === "function").toBeTruthy();
+
+        const expectedError = new Error("error unsubscribing from broadcast");
+        subscriptionManagerSpy.unregisterSubscription.mockReturnValue(Promise.reject(expectedError));
+
+        const passedSubscriptionId = await weakSignal.subscribe({
+            subscriptionQos,
+            receive() {}
+        });
+        await expect(
+            weakSignal.unsubscribe({
+                subscriptionId: passedSubscriptionId
+            })
+        ).rejects.toBeInstanceOf(Error);
+    });
+    it("throws error if subscribe is invoked with invalid partitions", () => {
+        expect(() => {
+            weakSignal.subscribe({
+                subscriptionQos,
+                partitions: ["_"]
+            });
+        }).toThrow();
+        expect(() => {
+            weakSignal.subscribe({
+                subscriptionQos,
+                partitions: ["./$"]
+            });
+        }).toThrow();
+    });
+});
