d027cce2ab05bc66d2e7eaa56a19ce153d45e0de
Lecki Filip <Filip.Lecki@bmw.de>
2019-06-19 15:33:43 +0200

    [jee] changed PROPERTY_KEY_MQTT_BROKER_URI to PROPERTY_MQTT_BROKER_URIS
    - stateless-async-jee-car-sim was using still old property. Now it is
    fixed

--- examples/stateless-async/stateless-async-jee-car-sim/src/main/java/io/joynr/examples/statelessasync/jee/carsim/JoynrConfigurationProvider.java
+++ examples/stateless-async/stateless-async-jee-car-sim/src/main/java/io/joynr/examples/statelessasync/jee/carsim/JoynrConfigurationProvider.java
@@ -38,7 +38,7 @@ public class JoynrConfigurationProvider {
         joynrProperties.setProperty(MessagingPropertyKeys.PROPERTY_SERVLET_CONTEXT_ROOT, "/messaging");
         joynrProperties.setProperty(MessagingPropertyKeys.PROPERTY_SERVLET_HOST_PATH, "http://localhost:8181");
         joynrProperties.setProperty(MessagingPropertyKeys.CHANNELID, "io.joynr.examples.statelessasync.jee.carsim");
-        joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_BROKER_URI, "tcp://mqttbroker:1883");
+        joynrProperties.setProperty(MqttModule.PROPERTY_MQTT_BROKER_URIS, "tcp://mqttbroker:1883");
         joynrProperties.setProperty(MqttModule.PROPERTY_KEY_MQTT_ENABLE_SHARED_SUBSCRIPTIONS, Boolean.TRUE.toString());
         joynrProperties.setProperty(MessagingPropertyKeys.PERSISTENCE_FILE, "jee-carsim-joynr.properties");
 
