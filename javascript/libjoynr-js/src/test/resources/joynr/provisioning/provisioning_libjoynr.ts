b15ed962896b7651b5da0bd4b7d3c7a067fa37e4
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2019-08-12 06:30:12 +0000

    [JS] Rewrite test/resources/provisioning to TS

--- javascript/libjoynr-js/src/test/resources/joynr/provisioning/provisioning_libjoynr.ts
+++ javascript/libjoynr-js/src/test/resources/joynr/provisioning/provisioning_libjoynr.ts
@@ -1,14 +1,14 @@
 /*
  * #%L
  * %%
- * Copyright (C) 2011 - 2017 BMW Car IT GmbH
+ * Copyright (C) 2019 BMW Car IT GmbH
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,5 +16,6 @@
  * limitations under the License.
  * #L%
  */
+import provisioningRoot = require("./provisioning_root");
 
-module.exports = require("provisioning_root");
+export = provisioningRoot;
--- javascript/libjoynr-js/src/test/resources/joynr/provisioning/provisioning_root.ts
+++ javascript/libjoynr-js/src/test/resources/joynr/provisioning/provisioning_root.ts
@@ -1,14 +1,14 @@
 /*
  * #%L
  * %%
- * Copyright (C) 2011 - 2017 BMW Car IT GmbH
+ * Copyright (C) 2019 BMW Car IT GmbH
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,32 +17,32 @@
  * #L%
  */
 
-const provisioning = {};
-provisioning.ttl = 10000;
-
-provisioning.mqtt = {
-    qosLevel: 0
-};
-provisioning.logging = {
-    configuration: {
-        name: "test config",
-        appenders: {
-            Console: {
-                name: "STDOUT",
-                PatternLayout: {
-                    pattern: "%m%n"
+const provisioning = {
+    ttl: 10000,
+    mqtt: {
+        qosLevel: 0
+    },
+    logging: {
+        configuration: {
+            name: "test config",
+            appenders: {
+                Console: {
+                    name: "STDOUT",
+                    PatternLayout: {
+                        pattern: "%m%n"
+                    }
                 }
-            }
-        },
-        loggers: {
-            root: {
-                level: "debug",
-                AppenderRef: {
-                    ref: "STDOUT"
+            },
+            loggers: {
+                root: {
+                    level: "debug",
+                    AppenderRef: {
+                        ref: "STDOUT"
+                    }
                 }
             }
         }
     }
 };
 
-module.exports = provisioning;
+export = provisioning;
