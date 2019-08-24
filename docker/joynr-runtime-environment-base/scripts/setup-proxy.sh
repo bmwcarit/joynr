ebba5bb3d8d6db8e86a09a8679283364c31deb2f
Lecki Filip <Filip.Lecki@bmw.de>
2019-08-24 16:07:06 +0200

    cleaned up proxy handling in docker

--- docker/joynr-runtime-environment-base/scripts/setup-proxy.sh
+++ docker/joynr-runtime-environment-base/scripts/setup-proxy.sh
@@ -1,35 +1,46 @@
-#!/bin/bash
-if [ -z "$PROXY_HOST_BUILD_ARG" ]
+if [ -n "$http_proxy" ]; then 
+    if [[ "$http_proxy" =~ .*"@" ]]; then
+        read protocol proxy_user proxy_password proxy_host proxy_port <<< $( echo ${http_proxy} | awk -F '://|:|@' '{ print $1, $2, $3, $4, $5 }')
+    else
+        read protocol proxy_host proxy_port <<< $( echo ${http_proxy} | awk -F'://|:' '{ print $1, $2, $3 }')
+    fi
+    echo "protocol = $protocol" 
+    echo "proxy_host = $proxy_host"
+    echo "proxy_port = $proxy_port"
+    echo "proxy_user = $proxy_user"
+    echo "proxy_password = $proxy_password"
+fi
+
+if [ -z "$proxy_host" ]
 then
     echo "No proxy configured, using direct internet access."
     exit 0
-fi
-if [ -z "$PROXY_PORT_BUILD_ARG" ]
-then
-    echo "PROXY_PORT not set."
-    exit 1
-fi
-proxy=""
-if [ -z "$PROXY_USER_BUILD_ARG" ]
-then
-    proxy=http://$PROXY_HOST_BUILD_ARG:$PROXY_PORT_BUILD_ARG
 else
-    echo "PROXY_USER_BUILD_ARG set, using authenticated proxy."
-    if [ -z "$PROXY_PASSWORD_BUILD_ARG" ]
+    if [ -z "$proxy_port" ]
     then
-        echo "PROXY_USER_BUILD_ARG set but PROXY_PASSWORD_BUILD_ARG is not set."
+        echo "No port for proxy defined."
         exit 1
     fi
-    proxy=http://$PROXY_USER_BUILD_ARG:$PROXY_PASSWORD_BUILD_ARG@$PROXY_HOST_BUILD_ARG:$PROXY_PORT_BUILD_ARG
+    if [ -n "$proxy_user" ]
+    then
+        echo "Using authenticated proxy."
+        if [ -z "$proxy_password" ]
+        then
+            echo "Proxy user is set but no proxy password supplied."
+            exit 1
+        fi
+    else
+        echo "Using Proxy."
+    fi
 fi
-echo "Starting to setup proxy configuration, PROXY_HOST_BUILD_ARG=$PROXY_HOST_BUILD_ARG, PROXY_HOST_BUILD_ARG=$PROXY_PORT_BUILD_ARG."
 echo "Setting up proxy configuration in /etc/dnf/dnf.conf"
-cat > /etc/dnf/dnf.conf <<EOF
+cat >> /etc/dnf/dnf.conf <<EOF
 [main]
+zchunk=false
 gpgcheck=1
 installonly_limit=3
 clean_requirements_on_remove=false
-proxy=$proxy
+proxy=$http_proxy
 sslverify=false
 EOF
 echo "Final Configuration /etc/dnf/dnf.conf:"
@@ -37,9 +48,9 @@ cat /etc/dnf/dnf.conf
 echo "Setting up proxy configuration in /etc/wgetrc"
 cat > /etc/wgetrc <<EOF
 use_proxy=on
-http_proxy=$proxy
-https_proxy=$proxy
-ftp_proxy=$proxy
+http_proxy=$http_proxy
+https_proxy=$http_proxy
+ftp_proxy=$http_proxy
 check_certificate=off
 EOF
 echo "Final Configuration /etc/wgetrc:"
@@ -51,14 +62,14 @@ EOF
 echo "Setting up proxy configuration in /etc/profile.d/use-my-proxy.sh"
 cat > /etc/profile.d/use-my-proxy.sh <<EOF
 echo "use-my-proxy.sh started"
-PROXY_HOST=$PROXY_HOST_BUILD_ARG
-PROXY_PORT=$PROXY_PORT_BUILD_ARG
-http_proxy="$proxy"
-https_proxy="$proxy"
-ftp_proxy="$proxy"
-HTTP_PROXY="$proxy"
-HTTPS_PROXY="$proxy"
-FTP_PROXY="$proxy"
+PROXY_HOST=$proxy_host
+PROXY_PORT=$proxy_port
+http_proxy="$http_proxy"
+https_proxy="$http_proxy"
+ftp_proxy="$http_proxy"
+HTTP_PROXY="$http_proxy"
+HTTPS_PROXY="$http_proxy"
+FTP_PROXY="$http_proxy"
 export PROXY_HOST
 export PROXY_PORT
 export http_proxy
