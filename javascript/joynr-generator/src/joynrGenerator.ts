ec39f2604262e15039ea938f244da7d488b3a06b
Max van gen Hassend <Max.vangenHassend@de.bertrandt.com>
2019-08-27 05:47:48 +0000

    [JS] Generate joynr-includes for joynr-generator

--- javascript/joynr-generator/src/joynrGenerator.ts
+++ javascript/joynr-generator/src/joynrGenerator.ts
@@ -24,6 +24,10 @@ import { execSync } from "child_process";
 import * as ts from "typescript";
 import * as glob from "glob";
 import * as util from "util";
+import * as handlebars from "handlebars";
+import * as fs from "fs";
+import mkdirp = require("mkdirp");
+import _ = require("lodash");
 
 const globAsync = util.promisify(glob);
 
@@ -42,13 +46,35 @@ async function main() {
         .option("outputPath", { alias: "o", demandOption: true, desc: "output path will be created if not exist" })
         .option("js", {
             boolean: true,
-            desc: "compile to js instead of ts"
+            desc: "compile to js with d.ts instead of ts"
+        })
+        .option("includes", {
+            alias: "i",
+            boolean: true,
+            desc: "create joynr includes"
         })
         .help()
         .wrap(yargs.terminalWidth()).argv;
-    await generateTSSources(argv.modelPath as string | string[], argv.outputPath as string);
+
+    const modelPathArray: string[] = Array.isArray(argv.modelPath) ? argv.modelPath : [argv.modelPath];
+
+    await generateTSSources(modelPathArray, argv.outputPath as string);
+    if (argv.includes) {
+        let files: string[] = [];
+        modelPathArray.forEach(modelPath => {
+            if (modelPath.endsWith(".fidl")) {
+                files.push(modelPath);
+            } else {
+                // assume directory
+                files = files.concat(glob.sync(`${modelPath}/**/*.fidl`));
+            }
+        });
+        createJoynrIncludes(files, argv.outputPath as string);
+    }
     const files = await globAsync(`${argv.outputPath}/**/*.ts`);
-    compileToJS(files);
+    if (argv.js) {
+        compileToJS(files);
+    }
 }
 
 async function generateTSSources(modelPaths: string | string[], outputPath: string) {
@@ -98,6 +124,99 @@ function compileToJS(fileNames: string[]) {
     const exitCode = emitResult.emitSkipped ? 1 : 0;
     console.log(`Process exiting with code '${exitCode}'.`);
     process.exit(exitCode);
+}
+
+// Setup the template file that will be used to generate the JavaScript file(s). A handler
+// must be registered so Handlebars can determine if a variable is an object or not.
+handlebars.registerHelper("ifObject", function(this: any, item, options) {
+    if (typeof item === "object") {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
+
+handlebars.registerHelper("concat", function(...args) {
+    return new handlebars.SafeString(
+        args
+            .slice(0, -1)
+            .filter(Boolean)
+            .join("_")
+    );
+});
+
+/**
+ * Scans a directory, creating a map of data in order to generate the JavaScript file.
+ *
+ * @returns An object mapping bracket notation interface to file path to required file
+ * @param dir The directory to begin scanning for files
+ * @param relativeFromDir The file path will be calculated as a relative path
+ * from this directory
+ */
+function createRequiresFromDir(dir: string, relativeFromDir: string): Record<string, any> {
+    const files = fs.readdirSync(dir);
+    const modulePaths: Record<string, any> = {};
+
+    files.forEach(file => {
+        const fullPath = path.join(dir, file);
+        const currentFileStat = fs.statSync(fullPath);
+
+        if (currentFileStat.isDirectory()) {
+            const subDirModules = createRequiresFromDir(fullPath, relativeFromDir);
+            const transformedSubDirModules: Record<string, any> = {};
+            Object.keys(subDirModules).forEach(subModule => {
+                transformedSubDirModules[subModule] = subDirModules[subModule];
+            });
+            modulePaths[file] = transformedSubDirModules;
+        } else if (currentFileStat.isFile()) {
+            const moduleName = path.basename(file, ".ts");
+            const modulePath = path.relative(relativeFromDir, path.join(dir, moduleName));
+            modulePaths[moduleName] = modulePath;
+        }
+    });
+
+    return modulePaths;
+}
+
+function createJoynrIncludes(fidlFiles: string[], outputFolder: string, fidlFileGroup: string = "joynr-includes") {
+    const templateFilePath = path.join(__dirname, "joynr-require-interface.hbs");
+    const templateFile = fs.readFileSync(templateFilePath, "utf8");
+    const requiresTemplate = handlebars.compile(templateFile, { noEscape: true });
+
+    const requiresPerFile: Record<string, any> = {};
+
+    fidlFiles.forEach(fidlFile => {
+        // Gather the package name in order to construct a path, generate the method
+        // names and paths and then generate the file's contents.
+        const fidlFileContents = fs.readFileSync(fidlFile, "utf8")!;
+        const packagePath = fidlFileContents.match(/^package\s+(.+)$/m)![1];
+        const packagePathParts = packagePath.split(".");
+        const outputPathSuffix = path.join.apply(path, packagePathParts);
+        const outputFolderPerGroup = path.join(outputFolder, fidlFileGroup);
+        const newFilename = path.join(outputFolderPerGroup, `${packagePathParts.pop()}`);
+
+        const requires = createRequiresFromDir(
+            path.join(outputFolder, "joynr", outputPathSuffix),
+            outputFolderPerGroup
+        );
+
+        requiresPerFile[newFilename] = _.merge(requiresPerFile[newFilename], requires);
+
+        try {
+            mkdirp.sync(outputFolderPerGroup);
+        } catch (e) {
+            if (e.code !== "EEXIST") {
+                throw e;
+            }
+        }
+    });
+
+    for (const file in requiresPerFile) {
+        if (requiresPerFile.hasOwnProperty(file)) {
+            const requires = requiresPerFile[file];
+            fs.writeFileSync(`${file}.ts`, requiresTemplate({ requires, fileName: path.basename(file, ".ts") }));
+        }
+    }
 }
 
 if (!module.parent) {
