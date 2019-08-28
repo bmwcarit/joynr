27179b427eaa2325e4513f5ef7a35193104219c0
Dumitru Suschevici <dumitru.suschevici@de.bertrandt.com>
2019-08-28 16:25:08 +0200

    [C++] Create AbstractGlobalMessagingSkeleton
    It will replace IMqttMessagingSkeleton and allow the registration
    of the replyTo address in the global messaging skeletons.

---
+++ cpp/libjoynrclustercontroller/include/joynr/AbstractGlobalMessagingSkeleton.h
@@ -0,0 +1,66 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2019 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#ifndef ABSTRACTGLOBALMESSAGINGSKELETON_H
+#define ABSTRACTGLOBALMESSAGINGSKELETON_H
+
+#include <functional>
+#include <memory>
+
+#include <smrf/ByteVector.h>
+
+#include "joynr/JoynrClusterControllerExport.h"
+
+#include "joynr/IMessagingMulticastSubscriber.h"
+
+#include "joynr/Logger.h"
+
+namespace joynr
+{
+
+namespace exceptions
+{
+class JoynrRuntimeException;
+} // namespace exceptions
+
+class ImmutableMessage;
+class IMessageRouter;
+
+class JOYNRCLUSTERCONTROLLER_EXPORT AbstractGlobalMessagingSkeleton
+        : public IMessagingMulticastSubscriber
+{
+public:
+    AbstractGlobalMessagingSkeleton() = default;
+    virtual ~AbstractGlobalMessagingSkeleton() = default;
+    virtual void transmit(
+            std::shared_ptr<ImmutableMessage> message,
+            const std::function<void(const exceptions::JoynrRuntimeException&)>& onFailure) = 0;
+
+    virtual void onMessageReceived(smrf::ByteVector&& rawMessage) = 0;
+
+protected:
+    void registerGlobalRoutingEntryIfRequired(const ImmutableMessage& message,
+                                              std::shared_ptr<IMessageRouter> messageRouter,
+                                              const std::string& gbid = "");
+
+private:
+    ADD_LOGGER(AbstractGlobalMessagingSkeleton)
+};
+
+} // namespace joynr
+#endif // ABSTRACTGLOBALMESSAGINGSKELETON_H
---
+++ cpp/libjoynrclustercontroller/messaging/AbstractGlobalMessagingSkeleton.cpp
@@ -0,0 +1,81 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2019 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+#include "joynr/AbstractGlobalMessagingSkeleton.h"
+
+#include "joynr/IMessageRouter.h"
+#include "joynr/ImmutableMessage.h"
+#include "joynr/Message.h"
+#include "joynr/Util.h"
+#include "joynr/exceptions/JoynrException.h"
+#include "joynr/system/RoutingTypes/MqttAddress.h"
+
+namespace joynr
+{
+
+void AbstractGlobalMessagingSkeleton::registerGlobalRoutingEntryIfRequired(
+        const ImmutableMessage& message,
+        std::shared_ptr<IMessageRouter> messageRouter,
+        const std::string& gbid)
+{
+    const std::string& messageType = message.getType();
+
+    if (messageType == Message::VALUE_MESSAGE_TYPE_REQUEST() ||
+        messageType == Message::VALUE_MESSAGE_TYPE_SUBSCRIPTION_REQUEST() ||
+        messageType == Message::VALUE_MESSAGE_TYPE_BROADCAST_SUBSCRIPTION_REQUEST() ||
+        messageType == Message::VALUE_MESSAGE_TYPE_MULTICAST_SUBSCRIPTION_REQUEST()) {
+
+        boost::optional<std::string> optionalReplyTo = message.getReplyTo();
+
+        if (!optionalReplyTo) {
+            std::string errorMessage("message " + message.getTrackingInfo() +
+                                     " did not contain replyTo header, discarding");
+            JOYNR_LOG_ERROR(logger(), errorMessage);
+            throw exceptions::JoynrMessageNotSentException(errorMessage);
+        }
+        const std::string& replyTo = *optionalReplyTo;
+        try {
+            // because the message is received via global transport (isGloballyVisible=true),
+            // isGloballyVisible must be true
+            const bool isGloballyVisible = true;
+            const TimePoint expiryDate = TimePoint::max();
+
+            const bool isSticky = false;
+            using system::RoutingTypes::Address;
+            std::shared_ptr<const Address> address;
+            joynr::serializer::deserializeFromJson(address, replyTo);
+            if (auto mqttAddress = dynamic_cast<const joynr::system::RoutingTypes::MqttAddress*>(
+                        address.get())) {
+                address = std::make_shared<const joynr::system::RoutingTypes::MqttAddress>(
+                        gbid, mqttAddress->getTopic());
+            }
+            messageRouter->addNextHop(message.getSender(),
+                                      address,
+                                      isGloballyVisible,
+                                      expiryDate.toMilliseconds(),
+                                      isSticky);
+        } catch (const std::invalid_argument& e) {
+            std::string errorMessage("could not deserialize Address from " + replyTo +
+                                     " - error: " + e.what());
+            JOYNR_LOG_FATAL(logger(), errorMessage);
+            // do not try to route the message if address is not valid
+            throw exceptions::JoynrMessageNotSentException(errorMessage);
+        }
+    }
+}
+}
