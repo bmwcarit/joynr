4f8d467086a38320fa579a0f26e2a951d126edd3
Guido Ostkamp <Guido.Ostkamp@de.bertrandt.com>
2019-09-06 11:22:21 +0200

    [SIT] Refactored run-onboard-sit.sh
    * start multiple consumers and providers per language with
      different GBID settings
    
    * let some time pass after provider start to allow for startup
      and registration before starting consumer test apps
    
    * kill still running processes in case this script is used
      interactively multiple times on same environment

--- tests/system-integration-test/docker/onboard/run-onboard-sit.sh
+++ tests/system-integration-test/docker/onboard/run-onboard-sit.sh
@@ -2,7 +2,7 @@
 ###
 # #%L
 # %%
-# Copyright (C) 2017 BMW Car IT GmbH
+# Copyright (C) 2019 BMW Car IT GmbH
 # %%
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,15 +19,27 @@
 ###
 
 # Give the JEE Discovery Directory a chance to start ...
-sleep 30
 
-echo "Onboard RUN END TO END TEST"
+echo "SIT: Onboard RUN END TO END TEST"
 
 echo "ENVIRONMENT"
 env
 
+# for repeated manual execution make sure there is nothing left
+pkill -9 cluster 2> /dev/null
+pkill -9 java 2> /dev/null
+pkill -9 jsit 2> /dev/null
+pkill -9 node 2> /dev/null
+sleep 1
+
+export JOYNR_LOG_LEVEL=TRACE
+
 # fail on first error
 set -e
+
+NVM_DIR=/usr/local/nvm
+. $NVM_DIR/nvm.sh
+type node
 
 DATA_DIR=/data
 CPP_HOME=${DATA_DIR}/sit-cpp-app
@@ -66,178 +78,299 @@ do
 	shift
 done
 
-echo "adding $DOMAIN_PREFIX also to the list of consumer domain prefixes"
+echo "SIT: adding $DOMAIN_PREFIX also to the list of consumer domain prefixes"
 CONSUMER_DOMAIN_PREFIXES=$CONSUMER_DOMAIN_PREFIXES$DOMAIN_PREFIX
 
-echo "start cluster controller + providers with domain prefix $DOMAIN_PREFIX"
-(
-	cd ${CPP_HOME}/bin
-	/usr/bin/cluster-controller ${DATA_DIR}/onboard-cc-messaging.settings & CLUSTER_CONTROLLER_PID=$!
-	./jsit-provider-ws $DOMAIN_PREFIX.cpp --runForever true & CPP_PROVIDER_PID=$!
+GBIDS[0]="joynrdefaultgbid"
+GBIDS[1]="othergbid"
+GBIDS[2]="joynrdefaultgbid,othergbid"
+BACKEND_PREFIX[0]="onlyBackend1"
+BACKEND_PREFIX[1]="onlyBackend2"
+BACKEND_PREFIX[2]="bothBackends"
 
-	cd ${NODE_APP_HOME}
-	npm run-script startprovider --sit-node-app:domain=$DOMAIN_PREFIX.node & NODE_PROVIDER_PID=$!
-	npm run-script startprovidertls --sit-node-app:domain=$DOMAIN_PREFIX.nodeTls & NODE_TLS_PROVIDER_PID=$!
+echo "SIT: Starting cluster controller + providers with domain prefix $DOMAIN_PREFIX"
 
-	cd ${JAVA_APP_HOME}
-	java -cp *.jar io.joynr.systemintegrationtest.ProviderApplication $DOMAIN_PREFIX.java runForever & JAVA_PROVIDER_PID=$!
-)
+# Prepare test environments
+for i in 0 1 2
+do
+	cd ${CPP_HOME}
+	rm -rf ${BACKEND_PREFIX[$i]}
+	cp -a bin ${BACKEND_PREFIX[$i]}
+	cd ${DATA_DIR}
+	rm -rf sit-node-app-${BACKEND_PREFIX[$i]}
+	cp -a sit-node-app sit-node-app-${BACKEND_PREFIX[$i]}
+	rm -rf sit-node-app-tls-${BACKEND_PREFIX[$i]}
+	cp -a sit-node-app sit-node-app-tls-${BACKEND_PREFIX[$i]}
+	cd ${DATA_DIR}
+	rm -rf sit-java-app-provider-${BACKEND_PREFIX[$i]}
+	cp -a sit-java-app sit-java-app-provider-${BACKEND_PREFIX[$i]}
+	rm -rf sit-java-app-consumer-${BACKEND_PREFIX[$i]}
+	cp -a sit-java-app sit-java-app-consumer-${BACKEND_PREFIX[$i]}
+done
 
-echo "Wait for JEE Application to be started"
+# Start cluster controller
+cd ${CPP_HOME}/bin
+/usr/bin/cluster-controller ${DATA_DIR}/onboard-cc-messaging.settings > cc.log 2>&1 &
+CLUSTER_CONTROLLER_PID=$!
+
+# Start provider
+for i in 0 1 2
+do
+	echo "SIT: Starting C++ provider for domain ${BACKEND_PREFIX[$i]}_${DOMAIN_PREFIX}.cpp"
+	cd ${CPP_HOME}/${BACKEND_PREFIX[$i]}
+	./jsit-provider-ws -d ${BACKEND_PREFIX[$i]}_${DOMAIN_PREFIX}.cpp -r -g ${GBIDS[$i]} -G > provider_cpp_${BACKEND_PREFIX[$i]}.log 2>&1 &
+	CPP_PROVIDER_PID[$i]=$!
+
+	echo "SIT: Starting Node provider for domain ${BACKEND_PREFIX[$i]}_$DOMAIN_PREFIX.node"
+	cd ${DATA_DIR}/sit-node-app-${BACKEND_PREFIX[$i]}
+	npm run-script startprovider --sit-node-app:domain=${BACKEND_PREFIX[$i]}_$DOMAIN_PREFIX.node --sit-node-app:gbids=${GBIDS[$i]} > provider_node_${BACKEND_PREFIX[$i]}.log 2>&1 &
+	NODE_PROVIDER_PID[$i]=$!
+
+	echo "SIT: Starting Node TLS provider for domain ${BACKEND_PREFIX[$i]}_$DOMAIN_PREFIX.nodeTls"
+	cd ${DATA_DIR}/sit-node-app-tls-${BACKEND_PREFIX[$i]}
+	npm run-script startprovidertls --sit-node-app:domain=${BACKEND_PREFIX[$i]}_$DOMAIN_PREFIX.nodeTls --sit-node-app:gbids=${GBIDS[$i]} > provider_tls_node_${BACKEND_PREFIX[$i]}.log 2>&1 &
+	NODE_TLS_PROVIDER_PID[$i]=$!
+
+	echo "SIT: Starting Java provider for domain ${BACKEND_PREFIX[$i]}_$DOMAIN_PREFIX.java"
+	cd ${DATA_DIR}/sit-java-app-provider-${BACKEND_PREFIX[$i]}
+	java -cp *.jar io.joynr.systemintegrationtest.ProviderApplication -d ${BACKEND_PREFIX[$i]}_$DOMAIN_PREFIX.java -r -g ${GBIDS[$i]} -G > provider_java_${BACKEND_PREFIX[$i]}.log 2>&1 &
+	JAVA_PROVIDER_PID[$i]=$!
+done
+
+echo "SIT: Sleeping 20 secs to let providers initialize and register at JDS"
+sleep 20
+
+echo "SIT: Waiting for JEE Application to be started"
 (
 	wait_for_jee_endpoint() {
-		retry_count=0
-		max_retries=60
-		while [ $retry_count -le $max_retries ]
+		for i in 1 2 3
 		do
-			curl -f -s http://sit-jee-app:8080/sit-jee-app/sit-controller/ping
-			if [ "$?" = 0 ]
-			then
-				break
-			fi
-			echo "JEE application not started yet ..."
-			sleep 2
-			retry_count=$(($retry_count+1))
-		done
+			retry_count=0
+			max_retries=60
+			while [ $retry_count -le $max_retries ]
+			do
+				curl -f -s http://sit-jee-app-$i:8080/sit-jee-app/sit-controller/ping
+				if [ "$?" = 0 ]
+				then
+					break
+				fi
+				echo "SIT: JEE application not started yet ..."
+				sleep 2
+				retry_count=$(($retry_count+1))
+			done
 
-		if [ $retry_count -gt $max_retries ]
-		then
-			echo "SIT RESULT error: JEE application failed to start in time."
-			exit 1
-		fi
+			if [ $retry_count -gt $max_retries ]
+			then
+				echo "SIT RESULT error: JEE application failed to start in time."
+				exit 1
+			fi
+		done
 		return 0
 	}
-	wait_for_jee_endpoint && echo "JEE endpoint started, continuing"
+	wait_for_jee_endpoint && echo "SIT: JEE endpoint started, continuing"
 )
+
+# ignore errors from here
+set +e
 
 for domainprefix in "${CONSUMER_DOMAIN_PREFIXES[@]}"
 do
-	echo "run cpp consumer test"
+	echo "SIT: Running cpp consumer test"
 	(
-		cd ${CPP_HOME}/bin
+		for i in 0 1 2
+		do
+			# cpp - run the test against cpp provider
+			echo "SIT: running cpp<->cpp joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.cpp"
+			cd ${CPP_HOME}/${BACKEND_PREFIX[$i]}
+			./jsit-consumer-ws -d ${BACKEND_PREFIX[$i]}_$domainprefix.cpp -g ${GBIDS[$i]} -G
 
-		# cpp - run the test against cpp provider
-        echo "run cpp<->cpp joynr system integration test"
-		./jsit-consumer-ws $domainprefix.cpp
+			if [ "$?" = "0" ]
+			then
+				echo "SIT: cpp<->cpp joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix succeeded"
+			else
+				echo "SIT RESULT ERROR joynr cpp system integration test against cpp provider failed with error code $?"
+			fi
+		done
 
-		if [ "$?" = "0" ]
-		then
-			echo "cpp<->cpp joynr system integration test succeeded"
-		else
-			echo "SIT RESULT ERROR joynr cpp system integration test against cpp provider failed with error code $?"
-		fi
+		for i in 0 1 2
+		do
+			# cpp - run the test against java provider
+			echo "SIT: running cpp<->java joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.java"
+			cd ${CPP_HOME}/${BACKEND_PREFIX[$i]}
+			./jsit-consumer-ws -d ${BACKEND_PREFIX[$i]}_$domainprefix.java -g ${GBIDS[$i]} -G
 
-		# cpp - run the test against java provider
-        echo "run cpp<->java joynr system integration test"
-		./jsit-consumer-ws $domainprefix.java
+			if [ "$?" = "0" ]
+			then
+				echo "SIT: cpp<->java joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix succeeded"
+			else
+				echo "SIT RESULT ERROR joynr cpp system integration test against java provider failed with error code $?"
+			fi
+		done
 
-		if [ "$?" = "0" ]
-		then
-			echo "cpp<->java joynr system integration test succeeded"
-		else
-			echo "SIT RESULT ERROR joynr cpp system integration test against java provider failed with error code $?"
-		fi
+		for i in 0 1 2
+		do
+			## cpp - run the test against node provider
+			echo "SIT: running cpp<->node joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.node"
+			cd ${CPP_HOME}/${BACKEND_PREFIX[$i]}
+			./jsit-consumer-ws -d ${BACKEND_PREFIX[$i]}_$domainprefix.node -g ${GBIDS[$i]} -G
 
-		# cpp - run the test against node provider
-        echo "run cpp<->node joynr system integration test"
-		./jsit-consumer-ws $domainprefix.node
+			if [ "$?" = "0" ]
+			then
+				echo "SIT: cpp<->node joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix succeeded"
+			else
+				echo "SIT RESULT ERROR joynr cpp system integration test against node provider failed with error code $?"
+			fi
+		done
 
-		if [ "$?" = "0" ]
-		then
-			echo "cpp<->node joynr system integration test succeeded"
-		else
-			echo "SIT RESULT ERROR joynr cpp system integration test against node provider failed with error code $?"
-		fi
+		for i in 0 1 2
+		do
+			# cpp - run the test against jee provider
+			cd ${CPP_HOME}/${BACKEND_PREFIX[$i]}
+			echo "SIT: running cpp<->jee joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.jee"
+			./jsit-consumer-ws -d ${BACKEND_PREFIX[$i]}_$domainprefix.jee -g ${GBIDS[$i]} -G
 
-		# cpp - run the test against jee provider
-        echo "run cpp<->jee joynr system integration test"
-		./jsit-consumer-ws $domainprefix.jee
-
-		if [ "$?" = "0" ]
-		then
-			echo "cpp<->jee joynr system integration test succeeded"
-		else
-			echo "SIT RESULT ERROR joynr cpp system integration test against jee provider failed with error code $?"
-		fi
+			if [ "$?" = "0" ]
+			then
+				echo "SIT: cpp<->jee joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix succeeded"
+			else
+				echo "SIT RESULT ERROR joynr cpp system integration test against jee provider failed with error code $?"
+			fi
+		done
 	)
+
 
 	echo "run java consumer test"
 	(
-		cd ${JAVA_APP_HOME}
+		for i in 0 1 2
+		do
+			# java - run the test against cpp provider
+			echo "SIT: run java<->cpp joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.cpp"
+			cd ${DATA_DIR}/sit-java-app-consumer-${BACKEND_PREFIX[$i]}
+			rm -f *.persistence_file joynr_participantIds.properties
+			java -cp *.jar io.joynr.systemintegrationtest.ConsumerApplication -d ${BACKEND_PREFIX[$i]}_$domainprefix.cpp -g ${GBIDS[$i]} -G
 
-		# java - run the test against cpp provider
-        echo "run java<->cpp joynr system integration test"
-		java -cp *.jar io.joynr.systemintegrationtest.ConsumerApplication $domainprefix.cpp
+			if [ "$?" = "0" ]
+			then
+				echo "SIT: java<->cpp joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix succeeded"
+			else
+				echo "SIT RESULT ERROR joynr java system integration test against cpp provider failed with error code $?"
+			fi
+		done
 
-		if [ "$?" = "0" ]
-		then
-			echo "java<->cpp joynr system integration test succeeded"
-		else
-			echo "SIT RESULT ERROR joynr java system integration test against cpp provider failed with error code $?"
-		fi
+		for i in 0 1 2
+		do
+			# java - run the test against java provider
+			echo "SIT: run java<->java joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.java"
+			cd ${DATA_DIR}/sit-java-app-consumer-${BACKEND_PREFIX[$i]}
+			rm -f *.persistence_file joynr_participantIds.properties
+			java -cp *.jar io.joynr.systemintegrationtest.ConsumerApplication -d ${BACKEND_PREFIX[$i]}_$domainprefix.java -g ${GBIDS[$i]} -G
 
-		# java - run the test against java provider
-        echo "run java<->java joynr system integration test"
-		java -cp *.jar io.joynr.systemintegrationtest.ConsumerApplication $domainprefix.java
+			if [ "$?" = "0" ]
+			then
+				echo "SIT: java<->java joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix succeeded"
+			else
+				echo "SIT RESULT ERROR joynr java system integration test against java provider failed with error code $?"
+			fi
+		done
 
-		if [ "$?" = "0" ]
-		then
-			echo "java<->java joynr system integration test succeeded"
-		else
-			echo "SIT RESULT ERROR joynr java system integration test against java provider failed with error code $?"
-		fi
+		for i in 0 1 2
+		do
+			# java - run the test against node provider
+			echo "SIT: run java<->node joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.node"
+			cd ${DATA_DIR}/sit-java-app-consumer-${BACKEND_PREFIX[$i]}
+			rm -f *.persistence_file joynr_participantIds.properties
+			java -cp *.jar io.joynr.systemintegrationtest.ConsumerApplication -d ${BACKEND_PREFIX[$i]}_$domainprefix.node -g ${GBIDS[$i]} -G
 
-		# java - run the test against node provider
-        echo "run java<->node joynr system integration test"
-		java -cp *.jar io.joynr.systemintegrationtest.ConsumerApplication $domainprefix.node
+			if [ "$?" = "0" ]
+			then
+				echo "SIT: java<->node joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix succeeded"
+			else
+				echo "SIT RESULT ERROR joynr java system integration test against node provider failed with error code $?"
+			fi
+		done
 
-		if [ "$?" = "0" ]
-		then
-			echo "java<->node joynr system integration test succeeded"
-		else
-			echo "SIT RESULT ERROR joynr java system integration test against node provider failed with error code $?"
-		fi
+		for i in 0 1 2
+		do
+			# java - run the test against jee provider
+			echo "SIT: run java<->jee joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.jee"
+			cd ${DATA_DIR}/sit-java-app-consumer-${BACKEND_PREFIX[$i]}
+			rm -f *.persistence_file joynr_participantIds.properties
+			java -cp *.jar io.joynr.systemintegrationtest.ConsumerApplication -d ${BACKEND_PREFIX[$i]}_$domainprefix.jee -g ${GBIDS[$i]} -G
 
-		# java - run the test against jee provider
-        echo "run java<->jee joynr system integration test"
-		java -cp *.jar io.joynr.systemintegrationtest.ConsumerApplication $domainprefix.jee
-
-		if [ "$?" = "0" ]
-		then
-			echo "java<->jee joynr system integration test succeeded"
-		else
-			echo "SIT RESULT ERROR joynr java system integration test against jee provider failed with error code $?"
-		fi
+			if [ "$?" = "0" ]
+			then
+				echo "SIT: java<->jee joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix succeeded"
+			else
+				echo "SIT RESULT ERROR joynr java system integration test against jee provider failed with error code $?"
+			fi
+		done
 	)
 
 	echo "run node consumer test"
 	(
-		cd ${NODE_APP_HOME}
+		for i in 0 1 2
+		do
+			# node - run the test against cpp provider
+			echo "SIT: run node<->cpp joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.cpp"
+			cd ${DATA_DIR}/sit-node-app-${BACKEND_PREFIX[$i]}
+			npm run-script startconsumer \
+				--sit-node-app:domain=${BACKEND_PREFIX[$i]}_$domainprefix.cpp \
+				--sit-node-app:cc:host=127.0.0.1 \
+				--sit-node-app:cc:port=4242 \
+				--sit-node-app:gbids=${GBIDS[$i]}
+		done
 
-		# node - run the test against cpp provider
-        echo "run node<->cpp joynr system integration test"
-		npm run-script startconsumer --sit-node-app:domain=$domainprefix.cpp --sit-node-app:cc:host=127.0.0.1 --sit-node-app:cc:port=4242
+		for i in 0 1 2
+		do
+			# node - run the test against java provider
+			echo "SIT: run node<->java joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.java"
+			cd ${DATA_DIR}/sit-node-app-${BACKEND_PREFIX[$i]}
+			npm run-script startconsumer \
+				--sit-node-app:domain=${BACKEND_PREFIX[$i]}_$domainprefix.java \
+				--sit-node-app:cc:host=127.0.0.1 \
+				--sit-node-app:cc:port=4242 \
+				--sit-node-app:gbids=${GBIDS[$i]}
+		done
 
-		# node - run the test against java provider
-        echo "run node<->cpp joynr system integration test"
-		npm run-script startconsumer --sit-node-app:domain=$domainprefix.java --sit-node-app:cc:host=127.0.0.1 --sit-node-app:cc:port=4242
+		for i in 0 1 2
+		do
+			# node - run the test against node provider
+			echo "SIT: run node<->node joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.node"
+			cd ${DATA_DIR}/sit-node-app-${BACKEND_PREFIX[$i]}
+			npm run-script startconsumer \
+				--sit-node-app:domain=${BACKEND_PREFIX[$i]}_$domainprefix.node \
+				--sit-node-app:cc:host=127.0.0.1 \
+				--sit-node-app:cc:port=4242 \
+				--sit-node-app:gbids=${GBIDS[$i]}
+		done
 
-		# node - run the test against node provider
-        echo "run node<->node joynr system integration test"
-		npm run-script startconsumer --sit-node-app:domain=$domainprefix.node
+		for i in 0 1 2
+		do
+			# node - run the test against node provider with tls
+			echo "SIT: run node<->node joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.nodeTls"
+			cd ${DATA_DIR}/sit-node-app-tls-${BACKEND_PREFIX[$i]}
+			npm run-script startconsumertls \
+				--sit-node-app:domain=${BACKEND_PREFIX[$i]}_$domainprefix.nodeTls \
+				--sit-node-app:cc:host=127.0.0.1 \
+				--sit-node-app:cc:port=4243 \
+				--sit-node-app:gbids=${GBIDS[$i]}
+		done
 
-		# node - run the test against node provider
-        echo "run node<->node TLS joynr system integration test"
-		npm run-script startconsumertls --sit-node-app:domain=$domainprefix.nodeTls
-
-		# node - run the test against jee provider
-        echo "run node<->jee joynr system integration test"
-		npm run-script startconsumer --sit-node-app:domain=$domainprefix.jee --sit-node-app:cc:host=127.0.0.1 --sit-node-app:cc:port=4242
-
+		for i in 0 1 2
+		do
+			# node - run the test against jee provider
+			echo "SIT: run node<->jee joynr system integration test for domain ${BACKEND_PREFIX[$i]}_$domainprefix.jee"
+			cd ${DATA_DIR}/sit-node-app-${BACKEND_PREFIX[$i]}
+			npm run-script startconsumer \
+				--sit-node-app:domain=${BACKEND_PREFIX[$i]}_$domainprefix.jee \
+				--sit-node-app:cc:host=127.0.0.1 \
+				--sit-node-app:cc:port=4242 \
+				--sit-node-app:gbids=${GBIDS[$i]}
+		done
 	)
 done
 
-echo "Finished Onboard RUN END TO END TEST"
+echo "SIT: Finished Onboard RUN END TO END TEST"
 
 # Clean up
 # disabled, as the provider and the cluster controller must still be available for the jee consumer test
@@ -246,5 +379,11 @@ echo "Finished Onboard RUN END TO END TEST"
 # kill $NODE_PROVIDER_PID
 # kill $CLUSTER_CONTROLLER_PID
 # kill $MOSQUITTO_PID
-kill NODE_TLS_PROVIDER_PID
+set +e
+for i in 0 1 2
+do
+	echo "kill -9 ${NODE_TLS_PROVIDER_PID[$i]}"
+	kill -9 ${NODE_TLS_PROVIDER_PID[$i]}
+done
+set -e
 wait
