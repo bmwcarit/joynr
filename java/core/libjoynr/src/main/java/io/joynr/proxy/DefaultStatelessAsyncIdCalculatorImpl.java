1652e5414690c3f5c35ba65b8057ca54f0692010
Renato Oliveira <renato.oliveira@criticaltechworks.com>
2019-10-10 16:46:16 +0100

    Revert to use previous version of orElseThrow()

--- java/core/libjoynr/src/main/java/io/joynr/proxy/DefaultStatelessAsyncIdCalculatorImpl.java
+++ java/core/libjoynr/src/main/java/io/joynr/proxy/DefaultStatelessAsyncIdCalculatorImpl.java
@@ -48,31 +48,32 @@ public class DefaultStatelessAsyncIdCalculatorImpl implements StatelessAsyncIdCa
     private final Map<String, String> participantIdMap = new ConcurrentHashMap<>();
 
     @Inject
-    public DefaultStatelessAsyncIdCalculatorImpl(@Named(MessagingPropertyKeys.CHANNELID) String channelId) {
+    public DefaultStatelessAsyncIdCalculatorImpl(@Named(MessagingPropertyKeys.CHANNELID)
+                                                 final String channelId) {
         this.channelId = channelId;
     }
 
     @Override
-    public String calculateParticipantId(String interfaceName, StatelessAsyncCallback statelessAsyncCallback) {
-        String statelessCallbackId = calculateStatelessCallbackId(interfaceName, statelessAsyncCallback);
-        String fullParticipantId = channelId + CHANNEL_SEPARATOR + statelessCallbackId;
+    public String calculateParticipantId(final String interfaceName, final StatelessAsyncCallback statelessAsyncCallback) {
+        final String statelessCallbackId = calculateStatelessCallbackId(interfaceName, statelessAsyncCallback);
+        final String fullParticipantId = channelId + CHANNEL_SEPARATOR + statelessCallbackId;
         try {
-            String uuid = UUID.nameUUIDFromBytes(fullParticipantId.getBytes("UTF-8")).toString();
+            final String uuid = UUID.nameUUIDFromBytes(fullParticipantId.getBytes("UTF-8")).toString();
             participantIdMap.putIfAbsent(uuid, statelessCallbackId);
             return uuid;
-        } catch (UnsupportedEncodingException e) {
+        } catch (final UnsupportedEncodingException e) {
             throw new JoynrRuntimeException("Platform does not support UTF-8", e);
         }
     }
 
     @Override
-    public String calculateStatelessCallbackId(String interfaceName, StatelessAsyncCallback statelessAsyncCallback) {
+    public String calculateStatelessCallbackId(final String interfaceName, final StatelessAsyncCallback statelessAsyncCallback) {
         return interfaceName + USE_CASE_SEPARATOR + statelessAsyncCallback.getUseCase();
     }
 
     @Override
-    public String calculateStatelessCallbackMethodId(Method method) {
-        StatelessCallbackCorrelation callbackCorrelation = method.getAnnotation(StatelessCallbackCorrelation.class);
+    public String calculateStatelessCallbackMethodId(final Method method) {
+        final StatelessCallbackCorrelation callbackCorrelation = method.getAnnotation(StatelessCallbackCorrelation.class);
         if (callbackCorrelation == null) {
             logger.error("Method {} on {} is missing StatelessCallbackCorrelation. Unable to generate callback method ID.",
                          method,
@@ -83,33 +84,28 @@ public class DefaultStatelessAsyncIdCalculatorImpl implements StatelessAsyncIdCa
     }
 
     @Override
-    public String calculateStatelessCallbackRequestReplyId(Method method) {
-        String requestReplyId = String.valueOf(random.nextLong());
-        String methodId = calculateStatelessCallbackMethodId(method);
+    public String calculateStatelessCallbackRequestReplyId(final Method method) {
+        final String requestReplyId = String.valueOf(random.nextLong());
+        final String methodId = calculateStatelessCallbackMethodId(method);
         return requestReplyId + REQUEST_REPLY_ID_SEPARATOR + methodId;
     }
 
     @Override
-    public String extractMethodIdFromRequestReplyId(String requestReplyId) {
+    public String extractMethodIdFromRequestReplyId(final String requestReplyId) {
         if (requestReplyId == null || requestReplyId.trim().isEmpty()
                 || !requestReplyId.contains(REQUEST_REPLY_ID_SEPARATOR)) {
             throw new JoynrIllegalStateException("Unable to extract method ID from invalid request/reply ID: "
                     + requestReplyId);
         }
-        int index = requestReplyId.indexOf(REQUEST_REPLY_ID_SEPARATOR);
+        final int index = requestReplyId.indexOf(REQUEST_REPLY_ID_SEPARATOR);
         return requestReplyId.substring(index + REQUEST_REPLY_ID_SEPARATOR.length());
     }
 
     @Override
-    public String fromParticipantUuid(String statelessParticipantIdUuid) {
-        String statelessCallbackId = null;
-        try {
-            statelessCallbackId = Optional.ofNullable(participantIdMap.get(statelessParticipantIdUuid))
-                                          .orElseThrow(() -> new JoynrIllegalStateException("Unknown stateless participant ID UUID: "
-                                                  + statelessParticipantIdUuid));
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        }
+    public String fromParticipantUuid(final String statelessParticipantIdUuid) {
+        final String statelessCallbackId = Optional.ofNullable(participantIdMap.get(statelessParticipantIdUuid))
+                                                   .orElseThrow(() -> new JoynrIllegalStateException("Unknown stateless participant ID UUID: "
+                                                     + statelessParticipantIdUuid));
         return statelessCallbackId;
     }
 
