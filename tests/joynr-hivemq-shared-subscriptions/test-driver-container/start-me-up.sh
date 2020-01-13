e4c3e54e026234114488e2b8f44c7e6579602855
Lecki Filip <Filip.Lecki@bmw.de>
2020-01-13 10:29:08 +0100

    [test] SST test-driver-container start-me-up.sh uses now correct apis

--- tests/joynr-hivemq-shared-subscriptions/test-driver-container/start-me-up.sh
+++ tests/joynr-hivemq-shared-subscriptions/test-driver-container/start-me-up.sh
@@ -21,11 +21,11 @@ function start_tests {
 	# We'll assume it takes AT LEAST 15 sec to start up the other containers
 	sleep 15
 	RESULT_SST=""
-	wait_for_endpoint "http://monitor-app:8080/monitor-app/test/0" && \
-		wait_for_endpoint "http://clustered-app-node-1:8080/clustered-app/callstats" && \
-		wait_for_endpoint "http://clustered-app-node-2:8080/clustered-app/callstats" && \
+	wait_for_endpoint "http://monitor-app:8080/test/0" && \
+		wait_for_endpoint "http://clustered-app-node-1:8080/callstats" && \
+		wait_for_endpoint "http://clustered-app-node-2:8080/callstats" && \
 		printf "\n\n >>>  STARTING SHARED SUBSCRIPTION TEST  <<<\n\n" && \
-		RESULT_SST="$(curl http://monitor-app:8080/monitor-app/test/100)"
+		RESULT_SST="$(curl http://monitor-app:8080/test/100)"
 	echo $RESULT_SST
 	if [ -n "$(echo $RESULT_SST | grep '100 were successful')" ]; then
 		echo "[SharedSubsTest] SUCCESS"
@@ -34,11 +34,11 @@ function start_tests {
 	fi
 
 	RESULT_BACKPRESSURE=""
-	wait_for_endpoint "http://backpressure-monitor-app:8080/backpressure-monitor-app/test/0" && \
-		wait_for_endpoint "http://backpressure-provider-small:8080/backpressure-provider-small/callstats" && \
-		wait_for_endpoint "http://backpressure-provider-large:8080/backpressure-provider-large/callstats" && \
+	wait_for_endpoint "http://backpressure-monitor-app:8080/test/0" && \
+		wait_for_endpoint "http://backpressure-provider-small:8080/callstats" && \
+		wait_for_endpoint "http://backpressure-provider-large:8080/callstats" && \
 		printf "\n\n >>>  STARTING BACKPRESSURE TEST  <<<\n\n" && \
-		RESULT_BACKPRESSURE=="$(curl http://backpressure-monitor-app:8080/backpressure-monitor-app/test/500)"
+		RESULT_BACKPRESSURE=="$(curl http://backpressure-monitor-app:8080/test/500)"
 	echo $RESULT_BACKPRESSURE
 	if [ -n "$(echo $RESULT_BACKPRESSURE | grep 'SUCCESS')" ]; then
 		echo "[BackpressureTest] SUCCESS"
