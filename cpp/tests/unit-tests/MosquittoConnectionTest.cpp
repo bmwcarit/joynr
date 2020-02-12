a19924ab207c930e2b8a58919fdb1c8c7798f992
Abdu Almamou <AbdAlbasset.Almamou@de.bertrandt.com>
2020-02-12 11:58:49 +0100

    [C++] Refactor MosquittoConnectionTest to reuse created connections

--- cpp/tests/unit-tests/MosquittoConnectionTest.cpp
+++ cpp/tests/unit-tests/MosquittoConnectionTest.cpp
@@ -20,6 +20,7 @@
 #include <cmath>
 #include <cstdint>
 #include <memory>
+#include <string>
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
@@ -41,7 +42,15 @@ using namespace joynr;
 class MosquittoConnectionTest : public ::testing::Test
 {
 public:
-    MosquittoConnectionTest()
+    MosquittoConnectionTest() :
+        _settings("test-resources/MqttJoynrClusterControllerRuntimeTest.settings"),
+        _messagingSettings(_settings),
+        _ccSettings(_settings),
+        _brokerUrl(_messagingSettings.getBrokerUrl()),
+        _mqttKeepAliveTimeSeconds(_messagingSettings.getMqttKeepAliveTimeSeconds()),
+        _mqttReconnectDelayTimeSeconds(_messagingSettings.getMqttReconnectDelayTimeSeconds()),
+        _mqttReconnectMaxDelayTimeSeconds(_messagingSettings.getMqttReconnectMaxDelayTimeSeconds()),
+        _isMqttExponentialBackoffEnabled(_messagingSettings.getMqttExponentialBackoffEnabled())
     {
     }
 
@@ -66,48 +75,63 @@ protected:
                 reinterpret_cast<const char*>(byteArrayView.data()), byteArrayView.size());
     }
 
+    std::shared_ptr<MosquittoConnection> createMosquittoConnection(
+            std::shared_ptr<joynr::Semaphore> readyToSendSemaphore,
+            const std::string clientId,
+            const std::string channelId
+            )
+    {
+        auto mosquittoConnection = std::make_shared<MosquittoConnection>(
+                _ccSettings,
+                _brokerUrl,
+                _mqttKeepAliveTimeSeconds,
+                _mqttReconnectDelayTimeSeconds,
+                _mqttReconnectMaxDelayTimeSeconds,
+                _isMqttExponentialBackoffEnabled,
+                clientId);
+
+        // register connection to channelId
+        mosquittoConnection->registerChannelId(channelId);
+        // connection is established and ready to send messages
+        mosquittoConnection->registerReadyToSendChangedCallback([readyToSendSemaphore](bool readyToSend) {
+                if (readyToSend) {
+                    readyToSendSemaphore->notify();
+                }
+        });
+        return mosquittoConnection;
+    }
+
+    Settings _settings;
+    MessagingSettings _messagingSettings;
+    ClusterControllerSettings _ccSettings;
+    const BrokerUrl _brokerUrl;
+    std::chrono::seconds _mqttKeepAliveTimeSeconds;
+    const std::chrono::seconds _mqttReconnectDelayTimeSeconds, _mqttReconnectMaxDelayTimeSeconds;
+    const bool _isMqttExponentialBackoffEnabled;
+
 private:
     DISALLOW_COPY_AND_ASSIGN(MosquittoConnectionTest);
 };
 
 TEST_F(MosquittoConnectionTest, generalTest)
 {
-    Settings settings("test-resources/MqttJoynrClusterControllerRuntimeTest.settings");
-    MessagingSettings messagingSettings(settings);
-    ClusterControllerSettings ccSettings(settings);
-
-    const BrokerUrl brokerUrl = messagingSettings.getBrokerUrl();
-    std::chrono::seconds mqttKeepAliveTimeSeconds = messagingSettings.getMqttKeepAliveTimeSeconds();
-    const std::chrono::seconds mqttReconnectDelayTimeSeconds = messagingSettings.getMqttReconnectDelayTimeSeconds();
-    const std::chrono::seconds mqttReconnectMaxDelayTimeSeconds = messagingSettings.getMqttReconnectMaxDelayTimeSeconds();
-    const bool isMqttExponentialBackoffEnabled = messagingSettings.getMqttExponentialBackoffEnabled();
+    // These values remain persisted in the broker
+    // use different ones when you need to create other connections
     const std::string clientId1("clientId1");
-    const std::string channelId1("channelId1");
     const std::string clientId2("clientId2");
+    const std::string channelId1("channelId1");
     const std::string channelId2("channelId2");
-    const std::string recipient2 = "recipient2";
-    const std::string payload2 = "demoPayloadRequest";
 
     // create two MosquittoConnections using different clientIds and channels,
     // both connections connect to the same broker
     auto readyToSendSemaphore1 = std::make_shared<joynr::Semaphore>(0);
-    auto mosquittoConnection1 = std::make_shared<MosquittoConnection>(
-            ccSettings,
-            brokerUrl,
-            mqttKeepAliveTimeSeconds,
-            mqttReconnectDelayTimeSeconds,
-            mqttReconnectMaxDelayTimeSeconds,
-            isMqttExponentialBackoffEnabled,
-            clientId1);
-
-    mosquittoConnection1->registerChannelId(channelId1);
+    auto mosquittoConnection1 = createMosquittoConnection(
+                readyToSendSemaphore1,
+                clientId1,
+                channelId1
+                );
     mosquittoConnection1->registerReceiveCallback([](smrf::ByteVector&& msg) {
             std::ignore = msg;
-    });
-    mosquittoConnection1->registerReadyToSendChangedCallback([readyToSendSemaphore1](bool readyToSend) {
-            if (readyToSend) {
-                readyToSendSemaphore1->notify();
-            }
     });
 
     // after connection is established, subscription to channel based topic
@@ -116,30 +140,22 @@ TEST_F(MosquittoConnectionTest, generalTest)
     EXPECT_TRUE(readyToSendSemaphore1->waitFor(std::chrono::seconds(5)));
     EXPECT_TRUE(mosquittoConnection1->isReadyToSend());
 
-    auto mosquittoConnection2 = std::make_shared<MosquittoConnection>(
-            ccSettings,
-            brokerUrl,
-            mqttKeepAliveTimeSeconds,
-            mqttReconnectDelayTimeSeconds,
-            mqttReconnectMaxDelayTimeSeconds,
-            isMqttExponentialBackoffEnabled,
-            clientId2);
+    auto readyToSendSemaphore2 = std::make_shared<joynr::Semaphore>(0);
+    auto mosquittoConnection2 = createMosquittoConnection(
+                readyToSendSemaphore2,
+                clientId2,
+                channelId2
+                );
 
     auto msgReceived2 = std::make_shared<joynr::Semaphore>(0);
-    auto readyToSendSemaphore2 = std::make_shared<joynr::Semaphore>(0);
-
-    mosquittoConnection2->registerChannelId(channelId2);
+    const std::string recipient2 = "recipient2";
+    const std::string payload2 = "payload2";
 
     mosquittoConnection2->registerReceiveCallback([recipient2, payload2, msgReceived2](smrf::ByteVector&& msg) {
             auto immutableMessage = std::make_shared<ImmutableMessage>(msg);
             EXPECT_EQ(immutableMessage->getRecipient(), recipient2);
             EXPECT_EQ(MosquittoConnectionTest::payloadAsString(immutableMessage), payload2);
             msgReceived2->notify();
-    });
-    mosquittoConnection2->registerReadyToSendChangedCallback([readyToSendSemaphore2](bool readyToSend) {
-            if (readyToSend) {
-                readyToSendSemaphore2->notify();
-            }
     });
 
     mosquittoConnection2->start();
