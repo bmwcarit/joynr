d5aa4780fa526e6acd47f923a3fdfd85017dd214
Abdu Almamou <AbdAlbasset.Almamou@de.bertrandt.com>
2020-02-13 09:20:53 +0100

    [C++] Unit tests for MQTT message expiry interval in MosquittoConnection

--- cpp/tests/unit-tests/MosquittoConnectionTest.cpp
+++ cpp/tests/unit-tests/MosquittoConnectionTest.cpp
@@ -18,6 +18,7 @@
  */
 #include <chrono>
 #include <cmath>
+#include <ctime>
 #include <cstdint>
 #include <memory>
 #include <string>
@@ -117,10 +118,12 @@ TEST_F(MosquittoConnectionTest, generalTest)
 {
     // These values remain persisted in the broker
     // use different ones when you need to create other connections
-    const std::string clientId1("clientId1");
-    const std::string clientId2("clientId2");
-    const std::string channelId1("channelId1");
-    const std::string channelId2("channelId2");
+    std::srand(std::time(0));
+    std::string random = std::to_string(std::rand());
+    const std::string clientId1("clientId1-" + random);
+    const std::string clientId2("clientId2-" + random);
+    const std::string channelId1("channelId1-" + random);
+    const std::string channelId2("channelId2-" + random);
 
     // create two MosquittoConnections using different clientIds and channels,
     // both connections connect to the same broker
@@ -132,6 +135,7 @@ TEST_F(MosquittoConnectionTest, generalTest)
                 );
     mosquittoConnection1->registerReceiveCallback([](smrf::ByteVector&& msg) {
             std::ignore = msg;
+            FAIL() << "We do not expect to receive msgs on connection1";
     });
 
     // after connection is established, subscription to channel based topic
@@ -167,9 +171,7 @@ TEST_F(MosquittoConnectionTest, generalTest)
     const auto now = TimePoint::now();
     auto immutableMessage = createMessage(now + 10000, recipient2, payload2);
 
-    std::chrono::milliseconds msgTtlMs = immutableMessage->getExpiryDate().relativeFromNow();
-    std::uint32_t msgTtlSec = static_cast<std::uint32_t>(std::ceil(msgTtlMs.count() / 1000.0));
-
+    const std::uint32_t msgTtlSec = 60;
     const smrf::ByteVector& rawMessage = immutableMessage->getSerializedMessage();
     const std::string topic = channelId2 + "/low";
     const int qosLevel = mosquittoConnection1->getMqttQos();
@@ -191,6 +193,178 @@ TEST_F(MosquittoConnectionTest, generalTest)
     mosquittoConnection1->unsubscribeFromTopic(additionalTopic);
     mosquittoConnection2->unsubscribeFromTopic(additionalTopic);
     std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    // cleanup
+    mosquittoConnection1->stop();
+    mosquittoConnection2->stop();
+}
+
+TEST_F(MosquittoConnectionTest, deliverMessageWithinItsExpiryIntervalAfterReconnect)
+{
+    std::srand(std::time(0));
+    std::string random = std::to_string(std::rand());
+    const std::string clientId3("clientId3-" + random);
+    const std::string clientId4("clientId4-" + random);
+    const std::string channelId3("channelId3-" + random);
+    const std::string channelId4("channelId4-" + random);
+
+    // connection1
+    auto readyToSendSemaphore1 = std::make_shared<joynr::Semaphore>(0);
+    auto mosquittoConnection1 = createMosquittoConnection(
+                readyToSendSemaphore1,
+                clientId3,
+                channelId3
+                );
+
+    // we do not plan to receive msgs on connection1, but we have to
+    // set registerReceiveCallback.
+    mosquittoConnection1->registerReceiveCallback([](smrf::ByteVector&& msg) {
+            std::ignore = msg;
+    });
+
+    mosquittoConnection1->start();
+    EXPECT_TRUE(readyToSendSemaphore1->waitFor(std::chrono::seconds(5)));
+    EXPECT_TRUE(mosquittoConnection1->isReadyToSend());
+
+    // connection2
+    auto readyToSendSemaphore2 = std::make_shared<joynr::Semaphore>(0);
+    auto mosquittoConnection2 = createMosquittoConnection(
+                readyToSendSemaphore2,
+                clientId4,
+                channelId4
+                );
+
+    const std::string dummyRecipient = "dummyRecipient";
+    const std::string dummyPayload = "dummyPayload";
+    // check the message has been received on mosquittoConnection2
+    auto msgWithExpiryDateReceived = std::make_shared<joynr::Semaphore>(0);
+    mosquittoConnection2->registerReceiveCallback([dummyRecipient, dummyPayload, msgWithExpiryDateReceived](smrf::ByteVector&& msg) {
+            auto receivedImmutableMessage = std::make_shared<ImmutableMessage>(msg);
+            EXPECT_EQ(receivedImmutableMessage->getRecipient(), dummyRecipient);
+            EXPECT_EQ(MosquittoConnectionTest::payloadAsString(receivedImmutableMessage), dummyPayload);
+            msgWithExpiryDateReceived->notify();
+    });
+
+    mosquittoConnection2->start();
+    EXPECT_TRUE(readyToSendSemaphore2->waitFor(std::chrono::seconds(10)));
+    EXPECT_TRUE(mosquittoConnection2->isReadyToSend());
+
+    mosquittoConnection2->stop();
+
+    // prepare a message and publish it via the broker to connection2 (while it is unavailable)
+    const std::function<void(const exceptions::JoynrRuntimeException&)>& onFailure = nullptr;
+    const auto now = TimePoint::now();
+    const std::int64_t msgDurationMs = 6000; // 1M
+    auto immutableMessage = createMessage(now + msgDurationMs, dummyRecipient, dummyPayload);
+
+    std::chrono::milliseconds msgTtlMs = immutableMessage->getExpiryDate().relativeFromNow();
+    std::uint32_t msgTtlSec = static_cast<std::uint32_t>(std::ceil(msgTtlMs.count() / 1000.0));
+
+    const smrf::ByteVector& rawMessage = immutableMessage->getSerializedMessage();
+    const std::string topic = channelId4 + "/low";
+    const int qosLevel = mosquittoConnection1->getMqttQos();
+
+    mosquittoConnection1->publishMessage(topic,
+                                         qosLevel,
+                                         onFailure,
+                                         msgTtlSec,
+                                         rawMessage.size(),
+                                         rawMessage.data());
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    // The broker should keep the msg for 1M
+    // start connection2 before message expires at the broker
+    mosquittoConnection2->start();
+    EXPECT_TRUE(readyToSendSemaphore2->waitFor(std::chrono::seconds(5)));
+    EXPECT_TRUE(mosquittoConnection2->isReadyToSend());
+
+    // message still not expired it will be received
+    EXPECT_TRUE(msgWithExpiryDateReceived->waitFor(std::chrono::seconds(5)));
+
+    // cleanup
+    mosquittoConnection1->stop();
+    mosquittoConnection2->stop();
+}
+
+TEST_F(MosquittoConnectionTest, noMessageDeliveryWhenExceedingItsExpiryIntervalAfterReconnect)
+{
+    std::srand(std::time(0));
+    std::string random = std::to_string(std::rand());
+    const std::string clientId5("clientId5-" + random);
+    const std::string clientId6("clientId6-" + random);
+    const std::string channelId5("channelId5-" + random);
+    const std::string channelId6("channelId6-" + random);
+
+    // connection1
+    auto readyToSendSemaphore1 = std::make_shared<joynr::Semaphore>(0);
+    auto mosquittoConnection1 = createMosquittoConnection(
+                readyToSendSemaphore1,
+                clientId5,
+                channelId5
+                );
+
+    // we do not plan to receive msgs on connection1, but we have to
+    // set registerReceiveCallback.
+    mosquittoConnection1->registerReceiveCallback([](smrf::ByteVector&& msg) {
+            std::ignore = msg;
+            FAIL() << "We do not expect to receive msgs on connection1";
+    });
+
+    mosquittoConnection1->start();
+    EXPECT_TRUE(readyToSendSemaphore1->waitFor(std::chrono::seconds(5)));
+    EXPECT_TRUE(mosquittoConnection1->isReadyToSend());
+
+    // connection2
+    auto readyToSendSemaphore2 = std::make_shared<joynr::Semaphore>(0);
+    auto mosquittoConnection2 = createMosquittoConnection(
+                readyToSendSemaphore2,
+                clientId6,
+                channelId6
+                );
+
+    const std::string anotherDummyRecipient = "anotherDummyRecipient";
+    const std::string anotherDummyPayload = "anotherDummyPayload";
+    // check the message has been received on mosquittoConnection2
+    auto msgWithExpiryDateReceived = std::make_shared<joynr::Semaphore>(0);
+    mosquittoConnection2->registerReceiveCallback([anotherDummyRecipient, anotherDummyPayload, msgWithExpiryDateReceived](smrf::ByteVector&& msg) {
+            msgWithExpiryDateReceived->notify();
+    });
+
+    mosquittoConnection2->start();
+    EXPECT_TRUE(readyToSendSemaphore2->waitFor(std::chrono::seconds(10)));
+    EXPECT_TRUE(mosquittoConnection2->isReadyToSend());
+
+    mosquittoConnection2->stop();
+
+    // prepare a message and publish it via the broker to connection2 (while it is unavailable)
+    const std::function<void(const exceptions::JoynrRuntimeException&)>& onFailure = nullptr;
+    const auto now = TimePoint::now();
+    const std::int64_t msgDurationMs = 3000; // 3sec
+    auto immutableMessage = createMessage(now + msgDurationMs, anotherDummyRecipient, anotherDummyPayload);
+
+    std::chrono::milliseconds msgTtlMs = immutableMessage->getExpiryDate().relativeFromNow();
+    std::uint32_t msgTtlSec = static_cast<std::uint32_t>(std::ceil(msgTtlMs.count() / 1000.0));
+
+    const smrf::ByteVector& rawMessage = immutableMessage->getSerializedMessage();
+    const std::string topic = channelId6 + "/low";
+    const int qosLevel = mosquittoConnection1->getMqttQos();
+
+    mosquittoConnection1->publishMessage(topic,
+                                         qosLevel,
+                                         onFailure,
+                                         msgTtlSec,
+                                         rawMessage.size(),
+                                         rawMessage.data());
+
+    // The broker should keep the msg for 3sec
+    std::this_thread::sleep_for(std::chrono::seconds(4));
+    // start connection2 after message expired at the broker
+    mosquittoConnection2->start();
+    EXPECT_TRUE(readyToSendSemaphore2->waitFor(std::chrono::seconds(5)));
+    EXPECT_TRUE(mosquittoConnection2->isReadyToSend());
+
+    // message expired nothing will be received
+    EXPECT_FALSE(msgWithExpiryDateReceived->waitFor(std::chrono::seconds(3)));
 
     // cleanup
     mosquittoConnection1->stop();
