28f8b31688fe51238053ac3abef4ed9b665b021a
Thomas Reuter <thomas.reuter@de.bertrandt.com>
2020-03-31 13:00:15 +0200

    [Docker] Write payara logs to console
    Additionally write payara logs to "/data/logs/payara.log"
    if the directory "/data/logs/" exists.

--- docker/joynr-base/scripts/start-payara.sh
+++ docker/joynr-base/scripts/start-payara.sh
@@ -22,7 +22,29 @@ done
 asadmin start-domain
 asadmin start-database --jvmoptions="-Dderby.storage.useDefaultFilePermissions=true"
 
+### configure log levels for debugging here, if required
+# asadmin set-log-levels io.joynr.messaging=FINEST
+### hide periodic RoutingTable.purge logs
+# asadmin set-log-levels io.joynr.messaging.routing.RoutingTableImpl=FINE
+
+if [ -d "/data/logs" ]
+then
+    asadmin set-log-attributes com.sun.enterprise.server.logging.GFFileHandler.file=/data/logs/payara.log
+fi
+asadmin set-log-attributes com.sun.enterprise.server.logging.GFFileHandler.logtoConsole=true
+asadmin set-log-attributes com.sun.enterprise.server.logging.GFFileHandler.multiLineMode=false
+asadmin set-log-attributes com.sun.enterprise.server.logging.GFFileHandler.rotationLimitInBytes=512000000
+
+asadmin stop-domain
+asadmin start-domain --verbose &
+sleep 20
+echo '####################################################'
+echo '# PAYARA list-log-levels'
+echo '####################################################'
+asadmin list-log-levels
+
 for warFile in $(echo $warFileList | tr "," "\n")
 do
     asadmin deploy --force=true $warFile
 done
+
