26f2c70dbeb67f9a2abf45def1a21c272484b27b
Ricardo Rocha <Ricardo.Rocha@partner.bmwgroup.com>
2020-07-17 14:11:12 +0100

    [Android] Add base content provider for Android persistent providers

---
+++ android/joynr-android-binder-runtime/src/main/java/io/joynr/android/contentprovider/PersistentJoynrContentProvider.java
@@ -0,0 +1,72 @@
+/*-
+ * #%L
+ * %%
+ * Copyright (C) 2020 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.joynr.android.contentprovider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Class that users can inherit in order to abstract from the process of initial persistent
+ * provider registration.
+ */
+public abstract class PersistentJoynrContentProvider extends ContentProvider {
+    
+    public PersistentJoynrContentProvider() {
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public boolean onCreate() {
+        registerPersistentProvider();
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * Method to override where providers need to be registered in order to be woken up at boot
+     */
+    public abstract void registerPersistentProvider();
+}
