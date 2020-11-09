ecc08c6171d6b2691c5c3cc0974e2e76ca3eef8c
Thomas Reuter <thomas.reuter@de.bertrandt.com>
2020-11-09 13:38:15 +0100

    [Java] Fix shutdown delay caused by HivemqMqttClient
    * Stop reactivex subscriptions for publishes Flowable, subscribe
      Flowable and unsubscribe Single during shutdown to release
      internal threads.
    * Adapt synchronization to the changes.

--- java/messaging/mqtt/hivemq-mqtt-client/src/main/java/io/joynr/messaging/mqtt/hivemq/client/HivemqMqttClient.java
+++ java/messaging/mqtt/hivemq-mqtt-client/src/main/java/io/joynr/messaging/mqtt/hivemq/client/HivemqMqttClient.java
@@ -19,9 +19,13 @@
 package io.joynr.messaging.mqtt.hivemq.client;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,8 +37,8 @@ import com.hivemq.client.mqtt.exceptions.MqttClientStateException;
 import com.hivemq.client.mqtt.exceptions.MqttSessionExpiredException;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5ClientConfig;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5RxClient;
-import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAckRestrictions;
 import com.hivemq.client.mqtt.mqtt5.message.connect.Mqtt5Connect;
+import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAckRestrictions;
 import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
 import com.hivemq.client.mqtt.mqtt5.message.subscribe.Mqtt5Subscribe;
 import com.hivemq.client.mqtt.mqtt5.message.subscribe.Mqtt5Subscription;
@@ -47,6 +51,7 @@ import io.joynr.messaging.SuccessAction;
 import io.joynr.messaging.mqtt.IMqttMessagingSkeleton;
 import io.joynr.messaging.mqtt.JoynrMqttClient;
 import io.joynr.statusmetrics.ConnectionStatusMetricsImpl;
+import io.reactivex.disposables.Disposable;
 
 /**
  * This implements the {@link JoynrMqttClient} using the HiveMQ MQTT Client library.
@@ -74,7 +79,12 @@ public class HivemqMqttClient implements JoynrMqttClient {
     private IMqttMessagingSkeleton messagingSkeleton;
     private ConnectionStatusMetricsImpl connectionStatusMetrics;
 
-    private Map<String, Mqtt5Subscription> subscriptions = new ConcurrentHashMap<>();
+    private Disposable publishesDisposable;
+    private HashMap<String, Disposable> subscriptionDisposables = new HashMap<>();
+    private AtomicLong obsoleteUnsubscribeDisposableCount = new AtomicLong(0);
+    private List<Disposable> unsubscribeDisposables = new ArrayList<>();
+
+    private Map<String, Mqtt5Subscription> subscriptions = new HashMap<>();
 
     // CHECKSTYLE IGNORE ParameterNumber FOR NEXT 1 LINES
     public HivemqMqttClient(Mqtt5RxClient client,
@@ -96,26 +106,34 @@ public class HivemqMqttClient implements JoynrMqttClient {
         this.isSender = isSender;
         clientInformation = createClientInformationString(gbid);
         this.connectionStatusMetrics = connectionStatusMetrics;
+        this.publishesDisposable = null;
         shuttingDown = false;
-        if (isReceiver) {
-            registerPublishCallback();
-        }
     }
 
     private void registerPublishCallback() {
-        client.publishes(MqttGlobalPublishFilter.ALL).subscribe(this::handleIncomingMessage, throwable -> {
-            if (!cleanSession && throwable instanceof MqttSessionExpiredException) {
-                logger.warn("{}: MqttSessionExpiredException encountered in publish callback, trying to resubscribe.",
-                            clientInformation,
-                            throwable);
-                registerPublishCallback();
-            } else {
-                logger.error("{}: Error encountered in publish callback, trying to resubscribe.",
-                             clientInformation,
-                             throwable);
-                registerPublishCallback();
-            }
-        });
+        if (!isReceiver || publishesDisposable != null) {
+            return;
+        }
+        publishesDisposable = client.publishes(MqttGlobalPublishFilter.ALL).subscribe(this::handleIncomingMessage,
+                                                                                      throwable -> {
+                                                                                          if (!cleanSession
+                                                                                                  && throwable instanceof MqttSessionExpiredException) {
+                                                                                              logger.warn("{}: MqttSessionExpiredException encountered in publish callback, trying to resubscribe.",
+                                                                                                          clientInformation,
+                                                                                                          throwable);
+                                                                                          } else {
+                                                                                              logger.error("{}: Error encountered in publish callback, trying to resubscribe.",
+                                                                                                           clientInformation,
+                                                                                                           throwable);
+                                                                                          }
+                                                                                          synchronized (this) {
+                                                                                              if (publishesDisposable != null) {
+                                                                                                  publishesDisposable.dispose();
+                                                                                                  publishesDisposable = null;
+                                                                                              }
+                                                                                              registerPublishCallback();
+                                                                                          }
+                                                                                      });
     }
 
     String getClientInformationString() {
@@ -142,10 +160,14 @@ public class HivemqMqttClient implements JoynrMqttClient {
 
     @Override
     public synchronized void start() {
+        shuttingDown = false;
         logger.info("{}: Initializing HiveMQ MQTT client for address {}.",
                     clientInformation,
                     client.getConfig().getServerAddress());
+
         assert (!isReceiver || messagingSkeleton != null);
+        registerPublishCallback();
+
         if (!client.getConfig().getState().isConnected()) {
             while (!client.getConfig().getState().isConnected()) {
                 logger.info("{}: Attempting to connect client, clean session={} ...", clientInformation, cleanSession);
@@ -209,7 +231,28 @@ public class HivemqMqttClient implements JoynrMqttClient {
             } catch (Exception e) {
                 logger.error("{}: Exception thrown on disconnect.", clientInformation, e.getMessage());
             }
+            if (publishesDisposable != null) {
+                publishesDisposable.dispose();
+                publishesDisposable = null;
+            }
+            synchronized (subscriptions) {
+                disposeSubscriptions();
+                for (Disposable unsubscribeDisposable : unsubscribeDisposables) {
+                    unsubscribeDisposable.dispose();
+                }
+                obsoleteUnsubscribeDisposableCount.set(0);
+            }
         }
+    }
+
+    /**
+     * Dispose reactivex subscriptions for MQTT subscriptions to release internal threads and unblock shutdown.
+     */
+    private void disposeSubscriptions() {
+        for (Disposable subscriptionDisposable : subscriptionDisposables.values()) {
+            subscriptionDisposable.dispose();
+        }
+        subscriptionDisposables.clear();
     }
 
     @Override
@@ -304,50 +347,80 @@ public class HivemqMqttClient implements JoynrMqttClient {
     @Override
     public void subscribe(String topic) {
         assert (isReceiver);
-        logger.info("{}: Subscribing to topic: {}", clientInformation, topic);
-        Mqtt5Subscription subscription = subscriptions.computeIfAbsent(topic,
-                                                                       (t) -> Mqtt5Subscription.builder()
-                                                                                               .topicFilter(t)
-                                                                                               .qos(MqttQos.AT_LEAST_ONCE) // TODO make configurable
-                                                                                               .build());
-        doSubscribe(subscription);
+        synchronized (subscriptions) {
+            logger.info("{}: Subscribing to topic: {}", clientInformation, topic);
+            Mqtt5Subscription subscription = subscriptions.computeIfAbsent(topic,
+                                                                           (t) -> Mqtt5Subscription.builder()
+                                                                                                   .topicFilter(t)
+                                                                                                   .qos(MqttQos.AT_LEAST_ONCE) // TODO make configurable
+                                                                                                   .build());
+
+            if (isShutdown()) {
+                return;
+            }
+            doSubscribe(subscription, topic);
+        }
     }
 
-    private void doSubscribe(Mqtt5Subscription subscription) {
+    private void doSubscribe(Mqtt5Subscription subscription, String topic) {
         Mqtt5Subscribe subscribe = Mqtt5Subscribe.builder().addSubscription(subscription).build();
-        client.subscribeStream(subscribe)
-              .doOnSingle(mqtt5SubAck -> logger.debug("{}: Subscribed to topic: {}, result: {}",
-                                                      clientInformation,
-                                                      subscription,
-                                                      mqtt5SubAck))
-              .subscribe(mqtt5Publish -> {
-                  /* handled in general publish callback */ },
-                         throwable -> logger.error("{}: Error encountered for subscription {}.",
-                                                   clientInformation,
-                                                   subscription,
-                                                   throwable));
+        Disposable disposable = client.subscribeStream(subscribe)
+                                      .doOnSingle(mqtt5SubAck -> logger.debug("{}: Subscribed to topic: {}, result: {}",
+                                                                              clientInformation,
+                                                                              subscription,
+                                                                              mqtt5SubAck))
+                                      .subscribe(mqtt5Publish -> {
+                                          /* handled in general publish callback */ },
+                                                 throwable -> logger.error("{}: Error encountered for subscription {}.",
+                                                                           clientInformation,
+                                                                           subscription,
+                                                                           throwable));
+        subscriptionDisposables.put(topic, disposable);
     }
 
     public void resubscribe() {
         logger.debug("{}: Resubscribe triggered.", clientInformation);
-        subscriptions.forEach((topic, subscription) -> {
-            logger.info("{}: Resubscribing to topic: {}", clientInformation, topic);
-            doSubscribe(subscription);
-        });
+        synchronized (subscriptions) {
+            disposeSubscriptions();
+            subscriptions.forEach((topic, subscription) -> {
+                logger.info("{}: Resubscribing to topic: {}", clientInformation, topic);
+                doSubscribe(subscription, topic);
+            });
+        }
     }
 
     @Override
     public void unsubscribe(String topic) {
         assert (isReceiver);
-        logger.info("{}: Unsubscribing from topic: {}", clientInformation, topic);
-        subscriptions.remove(topic);
         Mqtt5Unsubscribe unsubscribe = Mqtt5Unsubscribe.builder().addTopicFilter(topic).build();
-        client.unsubscribe(unsubscribe)
-              .subscribe((unused) -> logger.debug("{}: Unsubscribed from topic: {}", clientInformation, topic),
-                         throwable -> logger.error("{}: Unable to unsubscribe from topic: {}",
-                                                   clientInformation,
-                                                   topic,
-                                                   throwable));
+        synchronized (subscriptions) {
+            logger.info("{}: Unsubscribing from topic: {}", clientInformation, topic);
+            subscriptions.remove(topic);
+            AtomicBoolean callbackCalled = new AtomicBoolean(false);
+            Disposable unsubscribeDisposable = client.unsubscribe(unsubscribe).subscribe((unused) -> {
+                logger.debug("{}: Unsubscribed from topic: {}", clientInformation, topic);
+                if (callbackCalled.compareAndSet(false, true)) {
+                    obsoleteUnsubscribeDisposableCount.getAndIncrement();
+                }
+            }, throwable -> {
+                logger.error("{}: Unable to unsubscribe from topic: {}", clientInformation, topic, throwable);
+                if (callbackCalled.compareAndSet(false, true)) {
+                    obsoleteUnsubscribeDisposableCount.getAndIncrement();
+                }
+            });
+            unsubscribeDisposables.add(unsubscribeDisposable);
+            Disposable disposable = subscriptionDisposables.remove(topic);
+            if (disposable != null) {
+                disposable.dispose();
+            }
+            while (obsoleteUnsubscribeDisposableCount.get() > 0) {
+                unsubscribeDisposable = unsubscribeDisposables.remove(0);
+                if (unsubscribeDisposable != null) {
+                    unsubscribeDisposable.dispose();
+                }
+                obsoleteUnsubscribeDisposableCount.decrementAndGet();
+            }
+        }
     }
 
     @Override
