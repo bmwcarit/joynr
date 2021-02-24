ff18ec00b67e1d84e6126989710f83736abfa8b6
Thomas Reuter <thomas.reuter@bertrandt.com>
2021-02-24 17:57:08 +0100

    [Java,JEE] Unit tests for refactored MQTT shared subscriptions skeletons

---
+++ java/messaging/mqtt/joynr-mqtt-client/src/test/java/io/joynr/messaging/mqtt/MqttMessagingSkeletonProviderTest.java
@@ -0,0 +1,75 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2021 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.joynr.messaging.mqtt;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import io.joynr.messaging.IMessagingSkeletonFactory;
+import io.joynr.messaging.routing.MessageRouter;
+import joynr.system.RoutingTypes.MqttAddress;
+
+public class MqttMessagingSkeletonProviderTest {
+
+    private MqttMessagingSkeletonProvider subject;
+
+    @Mock
+    private MessageRouter mockMessageRouter;
+
+    @Mock
+    private MqttClientFactory mockMqttClientFactory;
+
+    @Mock
+    private MqttTopicPrefixProvider mockMqttTopicPrefixProvider;
+
+    private void createProvider(boolean enableSharedSubscriptions) {
+        subject = new MqttMessagingSkeletonProvider(new String[0],
+                                                    enableSharedSubscriptions,
+                                                    new MqttAddress(),
+                                                    42,
+                                                    false,
+                                                    32,
+                                                    23,
+                                                    new MqttAddress(),
+                                                    mockMessageRouter,
+                                                    mockMqttClientFactory,
+                                                    "",
+                                                    mockMqttTopicPrefixProvider,
+                                                    null,
+                                                    null,
+                                                    null,
+                                                    null);
+    }
+
+    @Test
+    public void createsJeeSpecificFactoryIfSharedSubscriptionsEnabled() {
+        createProvider(true);
+        IMessagingSkeletonFactory result = subject.get();
+        assertTrue(SharedSubscriptionsMqttMessagingSkeletonFactory.class.isInstance(result));
+    }
+
+    @Test
+    public void createsDefaultFactoryIfSharedSubscriptionsDisabled() {
+        createProvider(false);
+        IMessagingSkeletonFactory result = subject.get();
+        assertTrue(MqttMessagingSkeletonFactory.class.isInstance(result));
+    }
+}
