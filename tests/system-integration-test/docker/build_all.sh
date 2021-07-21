95e2fe7a5a327022ec83b9a024ab22ef664c6876
Guido Ostkamp <Guido.Ostkamp@partner.bmw.de>
2021-07-21 16:16:00 +0200

    [SIT, C++] Added jee-db image to build_all.sh

--- tests/system-integration-test/docker/build_all.sh
+++ tests/system-integration-test/docker/build_all.sh
@@ -3,7 +3,7 @@
 # build onboard image first because it builds joynr
 # which is required by the other build scripts
 #for BUILD_DIR in onboard joynr-backend-jee-1 joynr-backend-jee-2 sit-jee-app sit-jee-stateless-consumer sit-controller
-for BUILD_DIR in joynr-backend-jee-1 joynr-backend-jee-2 sit-jee-app sit-jee-stateless-consumer sit-controller
+for BUILD_DIR in joynr-backend-jee-1 joynr-backend-jee-2 joynr-backend-jee-db sit-jee-app sit-jee-stateless-consumer sit-controller
 do
 	pushd $BUILD_DIR
 	./build_docker_image.sh
