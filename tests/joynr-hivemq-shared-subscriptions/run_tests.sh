f436d8981ab735d87bfe930da9dda6aa5b78d9ef
Abdu Almamou <AbdAlbasset.Almamou@partner.bmwgroup.com>
2021-07-27 12:53:27 +0200

    [DOCKER] Abort log dump in SST if it does not finish in time

--- tests/joynr-hivemq-shared-subscriptions/run_tests.sh
+++ tests/joynr-hivemq-shared-subscriptions/run_tests.sh
@@ -12,11 +12,17 @@ then
   exit 1
 fi
 
-echo "wait 6 minutes, then log the result of the docker containers"
+echo "Wait 6 minutes, then log the result of the docker containers"
 echo "Started: $(date)"
 sleep 360
 
-docker-compose logs --no-color -t > sst-full.log
+echo "Logging results. Started: $(date). Timeout = 240s"
+
+# timeout runs the docker-compose logs command for 240s, and if it is not terminated,
+# it will kill it after ten seconds
+timeout -k 10s 240s docker-compose logs --no-color -t > sst-full.log
+
+echo "Logged: $(du -sh sst-full.log)"
 
 echo "stop all containers"
 docker-compose stop
