6d403e76b006e2d4975e499344142714a91bfca5
Thomas Reuter <thomas.reuter@partner.bmw.de>
2021-08-30 19:54:07 +0200

    [Docker] Add database docker image for GCD

---
+++ docker/joynr-gcd-db/init.sql
@@ -0,0 +1,50 @@
+CREATE SEQUENCE public.hibernate_sequence
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE TABLE IF NOT EXISTS public.customparameters
+(
+    id bigint NOT NULL,
+    name character varying(255) COLLATE pg_catalog."default",
+    value character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT customparameters_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.discovery_entries
+(
+    gbid character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    participantid character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    address text COLLATE pg_catalog."default",
+    clustercontrollerid character varying(255) COLLATE pg_catalog."default",
+    domain character varying(255) COLLATE pg_catalog."default",
+    expirydatems bigint,
+    interfacename character varying(255) COLLATE pg_catalog."default",
+    lastseendatems bigint,
+    majorversion integer,
+    minorversion integer,
+    priority bigint,
+    scope integer,
+    supportsonchangesubscriptions boolean,
+    publickeyid character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT discovery_entries_pkey PRIMARY KEY (gbid, participantid)
+);
+
+CREATE TABLE IF NOT EXISTS public.discovery_entries_customparameters
+(
+    globaldiscoveryentrypersisted_gbid character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    globaldiscoveryentrypersisted_participantid character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    customparameterlist_id bigint NOT NULL,
+    CONSTRAINT uk_a64uvuch6vcmhp01lp34qcsmm UNIQUE (customparameterlist_id),
+    CONSTRAINT fkd0oj1nnnhhycfc3lhtxw43xqv FOREIGN KEY (customparameterlist_id)
+        REFERENCES public.customparameters (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fkk7pg6nowtmpmce2rr8cld5lcb FOREIGN KEY (globaldiscoveryentrypersisted_gbid, globaldiscoveryentrypersisted_participantid)
+        REFERENCES public.discovery_entries (gbid, participantid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
