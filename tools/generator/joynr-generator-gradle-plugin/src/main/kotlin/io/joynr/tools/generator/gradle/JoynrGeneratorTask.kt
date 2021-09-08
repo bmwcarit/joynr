f980ca4b040dd9b597fc95e9230a91c75b7fbc22
Diogo Junior <Diogo.Junior@ctw.bmwgroup.com>
2021-09-08 15:56:55 +0100

    [Gradle Generator] add gradle 7.0 plugin support
    Gradle 7.0 force plugin's tasks to indicate if variables are internal or
    input/output files/directories in order to improve cache mechanism.
    The handler that the joynr generator uses is an internal var and that's
    what this commit does.

--- tools/generator/joynr-generator-gradle-plugin/src/main/kotlin/io/joynr/tools/generator/gradle/JoynrGeneratorTask.kt
+++ tools/generator/joynr-generator-gradle-plugin/src/main/kotlin/io/joynr/tools/generator/gradle/JoynrGeneratorTask.kt
@@ -21,10 +21,12 @@ package io.joynr.tools.generator.gradle
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.Internal
 
 
 open class JoynrGeneratorTask : DefaultTask() {
 
+    @Internal
     var joynrGeneratorHandler: JoynrGeneratorHandler? = null
 
     @TaskAction
