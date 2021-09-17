ffb639b36c19ef3323b9d6606ce58d698be9d046
Abdu Almamou <AbdAlbasset.Almamou@partner.bmwgroup.com>
2021-09-17 10:39:40 +0200

    [Docker] Use Java GCD in standalone-pt

--- tests/standalone-pt/docker/build_all.sh
+++ tests/standalone-pt/docker/build_all.sh
@@ -4,7 +4,12 @@ set -Eeuxo pipefail
 # build onboard image first because it builds joynr
 # which is required by the other build scripts
 
-for BUILD_DIR in joynr-hivemq joynr-consumer joynr-infra joynr-infra-db joynr-jee-provider 
+# build joynr backend images
+cd ../../../docker/ && ./build_backend.sh
+cd -
+
+# build the other images
+for BUILD_DIR in joynr-hivemq joynr-consumer joynr-jee-provider
 do
     pushd $BUILD_DIR
     ./build_docker_image.sh
--- tests/standalone-pt/docker/joynr-infra-db/start-me-up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-asadmin --user admin --passwordfile=/opt/payara41/pwdfile start-database --jvmoptions="-Dderby.storage.useDefaultFilePermissions=true"
-asadmin --user admin --passwordfile=/opt/payara41/pwdfile start-domain --verbose
-
-echo "end of start-me-up of joynr-infra-db"
