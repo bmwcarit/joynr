0bac5739e2d78100d606cab5851ba49c99dee69f
Abdu Almamou <AbdAlbasset.Almamou@partner.bmwgroup.com>
2021-09-17 12:52:26 +0200

    [Docker] Use Java GCD in inter-language-test

--- tests/inter-language-test/docker/joynr-backend-jee/start-me-up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-asadmin --user admin --passwordfile=/opt/payara41/pwdfile start-database --jvmoptions="-Dderby.storage.useDefaultFilePermissions=true"
-asadmin --user admin --passwordfile=/opt/payara41/pwdfile start-domain --verbose
