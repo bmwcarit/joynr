71e8e28f0ec4c00fab6494d11173b2afd2299ef5
Guido Ostkamp <Guido.Ostkamp@partner.bmw.de>
2021-09-20 15:14:02 +0200

    [Docker] Updated joynr-base to use Java 11 as default
    * Configure Java 11 as default via alternatives,
      so that java, javac etc. refer to Java 11 instead
      of Java 8.
    
    * Configure Maven to use Java 11 when called by joynr
      user. Since the Maven wrapper scripts do not honor
      settings set by alternatives (see above) this must
      be done via manual override if using the OS package
      version of Maven.

--- docker/joynr-base/scripts/boot2user.sh
+++ docker/joynr-base/scripts/boot2user.sh
@@ -11,6 +11,14 @@ export DEV_UID=${DEV_UID:=1000}
 
 useradd -u $DEV_UID -m $USER
 chown -R $USER:$USER /home/$USER
+# override required for using jdk11 with maven
+# otherwise jdk8 will still be taken despite
+# alternatives has already been configured for java11
+# since the set_jvm function in /usr/share/java-utils/java-functions
+# invoked by shell script /usr/bin/mvn does not respect alternatives
+cat > /home/$USER/.mavenrc << EOF
+export JAVA_HOME=/usr/lib/jvm/java-openjdk
+EOF
 
 #add user to kvm group and change /dev/kvm to joynr user so that it can 
 #run android virtual devices inside the container
