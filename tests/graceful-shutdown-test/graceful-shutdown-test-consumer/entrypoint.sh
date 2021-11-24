843ba8ddb155d6831df42d07bac373a52cdf778b
Thomas Reuter <thomas.reuter@partner.bmw.de>
2021-11-24 21:15:47 +0100

    [Java] Fix graceful-shutdown-test

--- tests/graceful-shutdown-test/graceful-shutdown-test-consumer/entrypoint.sh
+++ tests/graceful-shutdown-test/graceful-shutdown-test-consumer/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while [ -z "$(curl -v http://provider:8080/control/ping 2>&1 | grep '200 OK')" ]; do
+while [ -z "$(curl --noproxy provider -v http://provider:8080/control/ping 2>&1 | grep '200 OK')" ]; do
     echo "Waiting for provider to become available ..."
     sleep 5
 done
@@ -11,9 +11,9 @@ java -cp /app.jar io.joynr.tests.gracefulshutdown.Bootstrap &
 CONSUMER_PID=$!
 
 sleep 20
-curl http://provider:8080/control/prepareForShutdown
+curl --noproxy provider http://provider:8080/control/prepareForShutdown
 sleep 20
-FAILURE_REPORT="$(curl http://secondlevel:8080/control/failureReport)"
+FAILURE_REPORT="$(curl --noproxy secondlevel http://secondlevel:8080/control/failureReport)"
 echo "Second level failure report: ${FAILURE_REPORT}"
 
 echo "Stopping consumer ..."
--- tests/graceful-shutdown-test/graceful-shutdown-test-second-level-provider/entrypoint.sh
+++ tests/graceful-shutdown-test/graceful-shutdown-test-second-level-provider/entrypoint.sh
@@ -2,8 +2,8 @@
 
 echo "Check that joynrbackend is running ..."
 
-while [ -z "$(curl -v http://joynrbackend:8080/health 2>&1 | grep '200 OK')" ]; do
-    echo "Waiting for joynr backend to become available ..."
+while [ -z "$(echo "\n" | curl --noproxy joynr-gcd -v telnet://joynr-gcd:9998 2>&1 | grep 'OK')" ]; do
+    echo "Waiting for joynr backend / joynr-gcd to become available ..."
     sleep 5
 done
 
