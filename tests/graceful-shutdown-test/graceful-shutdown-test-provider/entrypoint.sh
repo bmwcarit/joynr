85c5baebf8bbed91d825c076860ba1354f91e0c3
Thomas Reuter <thomas.reuter@partner.bmw.de>
2021-11-24 21:15:47 +0100

    [Java] Fix graceful-shutdown-test

--- tests/graceful-shutdown-test/graceful-shutdown-test-provider/entrypoint.sh
+++ tests/graceful-shutdown-test/graceful-shutdown-test-provider/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while [ -z "$(curl -v http://secondlevel:8080/control/ping 2>&1 | grep '200 OK')" ]; do
+while [ -z "$(curl --noproxy secondlevel -v http://secondlevel:8080/control/ping 2>&1 | grep '200 OK')" ]; do
     echo "Waiting for second level provider to become available ..."
     sleep 5
 done
