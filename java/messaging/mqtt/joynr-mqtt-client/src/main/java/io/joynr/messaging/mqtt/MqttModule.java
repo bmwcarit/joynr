a829fd41c416459e543df08a53622a6444c389af
Guido Ostkamp <Guido.Ostkamp@partner.bmw.de>
2022-02-08 12:21:49 +0100

    [Java] Introduced new property MqttModule.PROPERTY_KEY_MQTT_DISABLE_HOSTNAME_VERIFICATION
    * Added boolean property
      MqttModule.PROPERTY_KEY_MQTT_DISABLE_HOSTNAME_VERIFICATION
      with key 'joynr.messaging.mqtt.disablehostnameverification'
    
    * Added documentation in JavaSettings.md
    
    * The property can be used to disable the hostname verification present in
      recent Java versions in order to bypass certificate issues when
      the actual used hostname is not part of the hostnames specified
      in the broker certificate (e.g. wrt. SAN list).

--- java/messaging/mqtt/joynr-mqtt-client/src/main/java/io/joynr/messaging/mqtt/MqttModule.java
+++ java/messaging/mqtt/joynr-mqtt-client/src/main/java/io/joynr/messaging/mqtt/MqttModule.java
@@ -75,6 +75,7 @@ public class MqttModule extends AbstractModule {
     public static final String PROPERTY_KEY_MQTT_SEPARATE_CONNECTIONS = "joynr.messaging.mqtt.separateconnections";
     public static final String PROPERTY_KEY_MQTT_USERNAME = "joynr.messaging.mqtt.username";
     public static final String PROPERTY_KEY_MQTT_PASSWORD = "joynr.messaging.mqtt.password";
+    public static final String PROPERTY_KEY_MQTT_DISABLE_HOSTNAME_VERIFICATION = "joynr.messaging.mqtt.disablehostnameverification";
 
     /**
      * Use this key to activate shared subscription support by setting the property's value to <code>true</code>. Shared
