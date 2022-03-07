abc7ca568d114db2a5c11ab09bb800e382ef7568
Guido Ostkamp <Guido.Ostkamp@partner.bmw.de>
2022-03-07 17:12:16 +0100

    [C++] Support using gtest/gmock from thirdparty directory
    * Use gtest/gmock from thirdparty directory if
      USE_PLATFORM_GTEST_GMOCK=OFF
    
    * In this case the library targets gtestLib and gmockLib are
      added and configured. The AddExternalProject statements produce the
      required static library import targets as BUILD_BYPRODUCTS in order
      to work with ninja.
    
    * Additionally then dependencies of JoynrMocks to gtestLib and gmockLib
      are added. These depend itself on googletest and googlemock
      respectively.

--- cpp/CMake/AddGtestGmock.cmake
+++ cpp/CMake/AddGtestGmock.cmake
@@ -15,18 +15,34 @@ if (USE_PLATFORM_GTEST_GMOCK)
 else (USE_PLATFORM_GTEST_GMOCK)
     ### clone googletest git repository ###########################################################
 
+    # Version is provided in the top level CMakeList.txt in googletest-repo;
+    # however since we do not use it, but build from subdirectory CMakeList.txt files
+    # we have to provide this ourselves
+    set(GOOGLETEST_VERSION 1.11.0)
+    set(DISABLE_PTHREAD_CMAKE_ARG OFF)
+
+    # force detection of compiler
+    # this is required if this file is included from e.g. inter-language-test
+    # since otherwise no compiler is available for the cmake invoked through the
+    # AddExternalProject specification.
+    include(CMakeDetermineCCompiler)
+
+    get_property(EP_PREFIX DIRECTORY PROPERTY EP_PREFIX)
+
+    # Note that CMAKE_CURRENT_LIST_DIR contains the directory where this file is stored when it
+    # gets processed by a cmake include command (contrary to the location of the file that contains
+    # the include command or the current working directory at the time where cmake or make is called).
+    # This info is used to determine a relative path to the thirdparty directory where googletest
+    # is stored.
+
     # this repository contains both googletest and googlemock
     AddExternalProject(
         googletest-repo
         DOWNLOAD_DIR "googletest-repo"
-        TIMEOUT 10
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG ddb8012e # there is no version tag on github; this is to make sure that a working version is used
+        URL "file://${CMAKE_CURRENT_LIST_DIR}/../../thirdparty/testing/googletest"
         CONFIGURE_COMMAND "" # Disable configuring
         BUILD_COMMAND "" # Disable building
     )
-
-    get_property(EP_PREFIX DIRECTORY PROPERTY EP_PREFIX)
 
     ### Add google test ###########################################################
 
@@ -35,10 +51,11 @@ else (USE_PLATFORM_GTEST_GMOCK)
         googletest
         DOWNLOAD_COMMAND "" # already downloaded by googletest-repo
         SOURCE_DIR "${EP_PREFIX}/src/googletest-repo/googletest"
-        BUILD_BYPRODUCTS "${EP_PREFIX}/src/googletest-build/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}"
+        BUILD_BYPRODUCTS "${EP_PREFIX}/src/googletest-build/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}"
         CMAKE_ARGS -Dgtest_force_shared_crt=ON
                    -Dgtest_disable_pthreads=${DISABLE_PTHREAD_CMAKE_ARG}
                    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+                   -DGOOGLETEST_VERSION=${GOOGLETEST_VERSION}
     )
 
     ExternalProject_Get_Property(googletest binary_dir)
@@ -53,7 +70,13 @@ else (USE_PLATFORM_GTEST_GMOCK)
     set(googletest_source_dir ${source_dir})
     message(STATUS "variable googletest_source_dir=${googletest_source_dir}")
     set(GTEST_INCLUDE_DIRS ${googletest_source_dir}/include)
-    set(GTEST_LIBRARIES ${googletest_binary_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(GTEST_LIBRARIES ${googletest_binary_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+    add_library(gtestLib STATIC IMPORTED)
+    add_dependencies(gtestLib googletest)
+    set_target_properties(gtestLib
+        PROPERTIES IMPORTED_LOCATION ${GTEST_LIBRARIES}
+    )
 
     ### Add google mock ###########################################################
 
@@ -62,10 +85,11 @@ else (USE_PLATFORM_GTEST_GMOCK)
         googlemock
         DOWNLOAD_COMMAND "" # already downloaded by googletest-repo
         SOURCE_DIR "${EP_PREFIX}/src/googletest-repo/googlemock"
-        BUILD_BYPRODUCTS "${EP_PREFIX}/src/googlemock-build/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}"
+        BUILD_BYPRODUCTS "${EP_PREFIX}/src/googlemock-build/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}"
         CMAKE_ARGS -Dgtest_force_shared_crt=ON
                    -Dgtest_disable_pthreads=${DISABLE_PTHREAD_CMAKE_ARG}
                    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+                   -DGOOGLETEST_VERSION=${GOOGLETEST_VERSION}
     )
 
     ExternalProject_Get_Property(googlemock binary_dir)
@@ -80,7 +104,17 @@ else (USE_PLATFORM_GTEST_GMOCK)
     set(googlemock_source_dir ${source_dir})
     message(STATUS "variable googlemock_source_dir=${googlemock_source_dir}")
     set(GMOCK_INCLUDE_DIRS ${googlemock_source_dir}/include)
-    set(GMOCK_LIBRARIES ${googlemock_binary_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(GMOCK_LIBRARIES ${googlemock_binary_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+    add_library(gmockLib STATIC IMPORTED)
+    add_dependencies(gmockLib googlemock)
+    set_target_properties(gmockLib
+        PROPERTIES IMPORTED_LOCATION ${GMOCK_LIBRARIES}
+    )
+
+    # required to get gtest_discover_tests
+    include(GoogleTest REQUIRED)
+
 endif(USE_PLATFORM_GTEST_GMOCK)
 
 function(RegisterToCtest TARGET ${ARGN})
