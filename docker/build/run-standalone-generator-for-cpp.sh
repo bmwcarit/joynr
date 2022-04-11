5cc8eec633053c72917adc59b1e44794f9799251
Dumitru Suschevici <Dumitru.Suschevici@partner.bmw.de>
2022-04-11 17:03:40 +0200

    [CI] Generate tests in c++ code generation script

--- docker/build/run-standalone-generator-for-cpp.sh
+++ docker/build/run-standalone-generator-for-cpp.sh
@@ -6,3 +6,20 @@ java -jar $GENERATOR \
      -modelpath /src/basemodel/src/main/franca \
      -outputPath /src/cpp/libjoynr/basemodel/generated \
      -generationLanguage cpp
+
+java -jar $GENERATOR \
+     -modelpath /src/basemodel/src/test/franca \
+     -outputPath /src/cpp/tests/gen \
+     -generationLanguage cpp
+
+java -jar $GENERATOR \
+     -modelpath /src/basemodel/src/test/franca-with-version \
+     -outputPath /src/cpp/tests/gen-with-version \
+     -generationLanguage cpp \
+     -addVersionTo comment
+
+java -jar $GENERATOR \
+     -modelpath /src/basemodel/src/test/franca-unversioned \
+     -outputPath /src/cpp/tests/gen-with-version \
+     -generationLanguage cpp \
+     -addVersionTo comment
