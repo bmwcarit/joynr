fc839b3f986600a5664f1ffe5173f20704d1c012
Abdu Almamou <AbdAlbasset.Almamou@partner.bmwgroup.com>
2022-04-28 10:25:28 +0200

    [C++] Fix unstable/flaky test in End2EndBroadcastTest
    * The time given for a successful subscription was too short, 500 ms.
    * In average it needs around 800 ms, we extended the time to 1000 ms.

--- cpp/tests/systemintegration-tests/End2EndBroadcastTestBase.cpp
+++ cpp/tests/systemintegration-tests/End2EndBroadcastTestBase.cpp
@@ -329,7 +329,7 @@ protected:
     {
         // wait some time so that MQTT subscribe/unsubscribe can be
         // executed by MQTT client and MQTT broker
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 };
 
