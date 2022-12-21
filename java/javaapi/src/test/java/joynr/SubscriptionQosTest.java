de25d1670f205a7b4f1bf2383d681383e06ca40c
Thomas Reuter <thomas.reuter@partner.bmw.de>
2022-12-21 15:49:38 +0100

    [Java] Replace wildcard import in SubscriptionQosTest

--- java/javaapi/src/test/java/joynr/SubscriptionQosTest.java
+++ java/javaapi/src/test/java/joynr/SubscriptionQosTest.java
@@ -18,7 +18,8 @@
  */
 package joynr;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
